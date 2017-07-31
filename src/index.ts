c6e90a8ce6fbd7429c96d3eaceaa381d6176bb5f
Steve Kite <korshakov.stepan@gmail.com>
2017-08-01 01:24:55 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import  * as e from 'express';
+// import * as bodyParser from 'body-parser';
+// import { graphqlExpress } from 'apollo-server-express';
+
+// var express = e()
+
+console.warn("123123");
+
+while (true) {
+    //
+}
