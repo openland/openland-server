5a73ef483564f076c4736763fabe851c171187a1
Steve Kite <korshakov.stepan@gmail.com>
2017-08-02 02:17:50 +0300

    Change default port and enable cors

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,20 @@
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import * as bodyParser from "body-parser";
 import * as express from "express";
-import * as Schema from './schema'
+import * as Schema from './schema';
+import * as cors from 'cors';
 
 export default async function () {
     const app = express();
 
     // Routes
+    app.use(cors())
     app.use("/graphql", bodyParser.json(), graphqlExpress({ schema: Schema.Schema }));
     app.use('/sandbox', graphiqlExpress({ endpointURL: '/graphql' }));
 
     // Starting Api
     var port = process.env.PORT
-    var dport = 3000
+    var dport = 9000
     if (port != undefined && port != "") {
         dport = parseInt(process.env.PORT as string)
     }
