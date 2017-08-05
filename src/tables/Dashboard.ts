8f90ef5c26b18fb13582582677fb86594b6e96b4
Steve Kite <korshakov.stepan@gmail.com>
2017-08-06 01:36:26 +0300

    Altering database instead of recreation

--- src/tables/Dashboard.ts
+++ src/tables/Dashboard.ts
@@ -4,5 +4,6 @@ import * as sequelize from 'sequelize'
 export const Dashboard = connection.define('dashboard', {
     id: { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
     title: { type: sequelize.STRING },
-    description: { type: sequelize.STRING }
+    kind: { type: sequelize.STRING, allowNull: true },
+    description: { type: sequelize.STRING },
 })
