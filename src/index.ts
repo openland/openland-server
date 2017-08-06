8cd5c987844b3c8b527b9d3bb0f82c5d0808f280
Steve Kite <korshakov.stepan@gmail.com>
2017-08-06 23:32:52 +0300

    Separate production and development mode

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import * as api from './api'
 
 async function init() {
   try {
-    console.info("Connecting to database")
-    await db.connection.sync({force: true})
+    if (process.env.NODE_ENV == "development") {
+      console.info("Connecting to database in DEVELOPMENT mode")
+      await db.connection.sync({ force: true })
+    } else {
+      console.info("Connecting to database in RELEASE mode")
+      await db.connection.sync()
+    }
     console.info("Starting API endpoint")
     await api.default()
   } catch (e) {
--- src/tables/Vote.ts
+++ src/tables/Vote.ts
@@ -1,14 +1,15 @@
-import { connection } from '../connector';
-import * as sequelize from 'sequelize'
+import { DataTypes, Sequelize, Instance } from 'sequelize'
 
 export interface VoteAttributes {
     id?: number;
     slug?: string;
 }
 
-export interface Vote extends sequelize.Instance<VoteAttributes>, VoteAttributes { }
+export interface Vote extends Instance<VoteAttributes>, VoteAttributes { }
 
-export const VoteTable = connection.define<Vote, VoteAttributes>('vote', {
-    id: { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    slug: { type: sequelize.STRING }
-}, { indexes: [{ unique: true, fields: ['id', 'slug'] }] })
+export const VoteTable = function (sequelize: Sequelize, dataTypes: DataTypes) {
+    sequelize.define<Vote, VoteAttributes>('vote', {
+        id: { type: dataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        slug: { type: dataTypes.STRING }
+    }, { indexes: [{ unique: true, fields: ['id', 'slug'] }] })
+}
