9cf431ff2f515ce164203c85327b1b8d473d58e4
Steve Kite <korshakov.stepan@gmail.com>
2017-08-12 02:26:23 +0300

    City Admin API

--- src/models/City.ts
+++ src/models/City.ts
@@ -5,9 +5,23 @@ export const Schema = `
         id: ID!
         name: String!
     }
+
+    type AdminCity {
+        id: ID!
+        name: String!
+        activated: Boolean!
+    }
+
     extend type Query {
         city(id: ID!): City
         cities: [City]
+
+        adminCities: [AdminCity]
+    }
+
+    extend type Mutation {
+        adminCreateCity(id: ID!, name: String!): AdminCity
+        adminAlterCity(id: ID!, name: String, activated: Boolean): AdminCity
     }
 `
 
@@ -26,6 +40,16 @@ export const Resolver = {
                 }
             })
         },
+        adminCities: async function () {
+            return await DB.City.findAll().map((city: { id: number, name: string, slug: string, activated: boolean }) => {
+                return {
+                    _dbid: city.id,
+                    id: city.slug,
+                    name: city.name,
+                    activated: city.activated
+                }
+            })
+        },
         city: async function (_: any, args: { id: string }) {
             var res = await DB.City.find({
                 where: {
@@ -41,6 +65,38 @@ export const Resolver = {
                 }
             } else {
                 return null
+            }
+        }
+    },
+    Mutation: {
+        adminCreateCity: async function (_: any, args: { id: string, name: string }) {
+            var res = await DB.City.create({
+                slug: args.id,
+                name: args.name
+            });
+            return {
+                id: res.slug,
+                name: res.name,
+                activated: res.activated
+            }
+        },
+        adminAlterCity: async function (_: any, args: { id: string, name?: string, activated?: boolean }) {
+            var res = (await DB.City.findOne({
+                where: {
+                    slug: args.id
+                }
+            }))!!
+            if (args.name != null) {
+                res.name = args.name
+            }
+            if (args.activated != null) {
+                res.activated = args.activated
+            }
+            res.save()
+            return {
+                id: res.slug,
+                name: res.name,
+                activated: res.activated
             }
         }
     }
