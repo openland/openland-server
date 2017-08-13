ba95fa114583da48b0c70786ae1a99a0cd49836b
Steve Kite <korshakov.stepan@gmail.com>
2017-08-13 06:48:06 +0300

    Restored sample data generation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import * as db from './connector'
 import * as api from './api'
-
+import * as sample from './sample';
 async function init() {
   try {
     if (process.env.NODE_ENV == "development") {
       console.info("Connecting to database in DEVELOPMENT mode")
       await db.connection.getQueryInterface().dropAllTables()
       await db.migrate()
+      await sample.createEmptyData()
     } else {
       console.info("Connecting to database in RELEASE mode")
       await db.migrate()
---
+++ src/sample.ts
@@ -0,0 +1,33 @@
+import { DB } from './tables/'
+
+export async function createEmptyData() {
+
+
+    await DB.User.create({
+        authId: "facebook|10213268338843701",
+        firstName: "Stepan",
+        lastName: "Korshakov",
+        email: "korshakov.stepan@gmail.com",
+        picture: "https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/12799449_10208337398773281_8543476314381451147_n.jpg?oh=f5e1fb63405ecf5dc1f88950fdcb4257&oe=5A2F17E5"
+    })
+
+    var sf = await DB.Account.create({
+        slug: "sf",
+        name: "San Francisco Housing",
+        activated: true
+    })
+    await DB.Account.create({
+        slug: "nyc",
+        name: "New York",
+        activated: true
+    })
+
+    await DB.DataSet.create({
+        name: 'Housing Element 2014',
+        description: 'Complete 200+ pages report that have all information about housing research in SF government',
+        link: 'http://208.121.200.84/ftp/files/plans-and-programs/planning-for-the-city/housing-element/2014HousingElement-AllParts_ADOPTED_web.pdf',
+        account: sf.id,
+        kind: 'dataset',
+        activated: true
+    })
+}
