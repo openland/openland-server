3e7d7da52f4a192bb71562801771f9b19c48aeff
Steve Kite <korshakov.stepan@gmail.com>
2017-11-23 22:54:59 +0300

    Added permits table

---
+++ src/tables/Permit.ts
@@ -0,0 +1,31 @@
+import { connection } from '../connector';
+import * as sequelize from 'sequelize'
+
+export interface PermitAttributes {
+    id?: number;
+    account?: number;
+    address?: string;
+    permitStatus?: "filled" | "issued" | "completed" | "expired";
+    permitCreated?: Date;
+    permitIssued?: Date;
+    permitCompleted?: Date;
+    permitExpired?: Date;
+}
+
+export interface Permit extends sequelize.Instance<PermitAttributes>, PermitAttributes { }
+
+export const PermitTable = connection.define<Permit, PermitAttributes>('permits', {
+    id: { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    account: {
+        type: sequelize.INTEGER, references: {
+            model: 'accounts',
+            key: 'id',
+        }
+    },
+    address: { type: sequelize.STRING, allowNull: true },
+    permitStatus: { type: sequelize.ENUM('filled', 'issued', 'completed', 'expired'), allowNull: true },
+    permitCreated: { type: sequelize.DATEONLY, allowNull: true },
+    permitIssued: { type: sequelize.DATEONLY, allowNull: true },
+    permitCompleted: { type: sequelize.DATEONLY, allowNull: true },
+    permitExpired: { type: sequelize.DATEONLY, allowNull: true },
+})
