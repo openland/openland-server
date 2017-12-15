9814123a02c4727f870386a626a7aab7f468c9d5
Steve Kite <korshakov.stepan@gmail.com>
2017-12-16 00:44:10 +0300

    Adding more indexes

---
+++ src/tables/migrations/20171216004008-AddIndexesForAssociations.ts
@@ -0,0 +1,11 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+export async function up(queryInterface: QueryInterface, sequelize: DataTypes) {
+    await queryInterface.addIndex('permit_street_numbers', ['permitId'])
+    await queryInterface.addIndex('permit_street_numbers', ['streetNumberId'])
+    await queryInterface.addIndex('permit_events', ['permitId'])
+}
+
+export async function down(queryInterface: QueryInterface, sequelize: DataTypes) {
+
+}
