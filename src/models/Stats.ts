adc1b7e761747a0495255b966836ca02c7a38fa1
Steve Kite <korshakov.stepan@gmail.com>
2018-01-17 13:35:42 -0800

    Total organization calculation

--- src/models/Stats.ts
+++ src/models/Stats.ts
@@ -6,6 +6,7 @@ export const Schema = `
         totalProjects: Int!
         totalDevelopers: Int!
         totalConstructors: Int!
+        totalOrganizations: Int!
         totalPermits: Int!        
     }
     
@@ -18,10 +19,11 @@ export const Resolver = {
     Query: {
         globalStats: async function (_: any, args: {}, context: CallContext) {
             return {
-                totalProjects: DB.BuidlingProject.count({where: {account: context.accountId}}),
-                totalDevelopers: DB.Developer.count({where: {account: context.accountId, isDeveloper: true}}),
-                totalConstructors: DB.Developer.count({where: {account: context.accountId, isConstructor: true}}),
-                totalPermits: DB.Permit.count({where: {account: context.accountId}}),
+                totalProjects: DB.BuidlingProject.count({ where: { account: context.accountId } }),
+                totalDevelopers: DB.Developer.count({ where: { account: context.accountId, isDeveloper: true } }),
+                totalConstructors: DB.Developer.count({ where: { account: context.accountId, isConstructor: true } }),
+                totalOrganizations: DB.Developer.count({ where: { account: context.accountId } }),
+                totalPermits: DB.Permit.count({ where: { account: context.accountId } }),
             };
         }
     }
