3cd12ef1ca59867dda9892626721c2fc0806d902
Steve Kite <steve@statecraft.one>
2018-01-28 00:04:38 -0800

    New area-based project resolving

--- src/models/BuildingProject.ts
+++ src/models/BuildingProject.ts
@@ -76,6 +76,12 @@ export const Schema = `
         buildingProject(slug: String!): BuildingProject!
     }
 
+    extend type Area {
+        buildingProjects(filter: String, minUnits: Int, year: String, first: Int!, after: String): BuildingProjectConnection!
+        buildingProjectsStats: BuildingProjectStats!
+        buildingProject(slug: String!): BuildingProject!
+    }
+
     input BuildingProjectInput {
         id: ID!
         name: String
@@ -90,6 +96,135 @@ export const Schema = `
         permits: [ID!]
     }
 `;
+
+let buildingProjectsStats = async (areaId: number) => {
+    let projectsQuery = new SelectBuilder(DB.BuidlingProject)
+        .whereEq('account', areaId);
+    let projectsTracked = projectsQuery
+        .count();
+    let projectsVerified = projectsQuery
+        .whereEq('verified', true)
+        .count();
+
+    let year2017NewUnits = projectsQuery
+        .whereEq('extrasYearEnd', '2017')
+        .sum('\"proposedUnits" - "existingUnits\"');
+    let year2017NewUnitsVerified = projectsQuery
+        .whereEq('extrasYearEnd', '2017')
+        .whereEq('verified', true)
+        .sum('\"proposedUnits" - "existingUnits\"');
+    let year2018NewUnits = projectsQuery
+        .whereEq('extrasYearEnd', '2018')
+        .sum('\"proposedUnits" - "existingUnits\"');
+    let year2018NewUnitsVerified = projectsQuery
+        .whereEq('extrasYearEnd', '2018')
+        .whereEq('verified', true)
+        .sum('\"proposedUnits" - "existingUnits\"');
+
+    let allProjects = (await DB.BuidlingProject.findAll({
+        where: {
+            account: areaId
+        },
+        include: [{
+            model: DB.Permit,
+            as: 'permits',
+            include: [{
+                model: DB.StreetNumber,
+                as: 'streetNumbers',
+                include: [{
+                    model: DB.Street,
+                    as: 'street',
+                }]
+            }]
+        }]
+    })).filter((v) =>
+        (v.permits!!.find((p) => p.permitCreated !== null && p.permitIssued !== null))
+        && (v.existingUnits != null)
+        && (v.proposedUnits != null)
+        && ((v.proposedUnits - v.existingUnits) >= 10));
+
+    let fastestProject = allProjects[0];
+    let slowestProject = allProjects[0];
+    let fastestDuration = dateDiff(new Date(fastestProject.permits!![0].permitCreated!!), new Date(fastestProject.permits!![0].permitIssued!!));
+    let slowestDuration = dateDiff(new Date(fastestProject.permits!![0].permitCreated!!), new Date(fastestProject.permits!![0].permitIssued!!));
+
+    for (let proj of allProjects) {
+        for (let p of proj.permits!!) {
+            if (p.permitCreated && p.permitIssued) {
+                let duration = dateDiff(new Date(p.permitCreated!!), new Date(p.permitIssued!!));
+                if (duration < fastestDuration) {
+                    fastestDuration = duration;
+                    fastestProject = proj;
+                }
+                if (duration > slowestDuration) {
+                    slowestDuration = duration;
+                    slowestProject = proj;
+                }
+            }
+        }
+    }
+
+    return {
+        projectsTracked: projectsTracked,
+        projectsVerified: projectsVerified,
+        year2017NewUnits: year2017NewUnits,
+        year2017NewUnitsVerified: year2017NewUnitsVerified,
+        year2018NewUnits: year2018NewUnits,
+        year2018NewUnitsVerified: year2018NewUnitsVerified,
+        fastestApprovalProject: fastestProject,
+        slowestApprovalProject: slowestProject
+    };
+};
+
+let buildingProjects = async function (areaId: number, args: { first: number, minUnits?: number, year?: string, filter?: string, after?: string }) {
+    let builder = new SelectBuilder(DB.BuidlingProject)
+        .whereEq('account', areaId)
+        .filterField('name')
+        .filterField('extrasAddress')
+        .filterField('extrasAddressSecondary')
+        .limit(args.first)
+        .after(args.after)
+        .filter(args.filter)
+        .whereEq('account', areaId)
+        .orderByRaw('"proposedUnits"-"existingUnits"', 'DESC NULLS LAST');
+    // .postProcessor((src) => src.sort((a, b) => {
+    //     if (a.proposedUnits != null && b.proposedUnits != null && a.existingUnits != null && b.existingUnits != null) {
+    //         return (b.proposedUnits!! - b.existingUnits!!) - (a.proposedUnits!! - a.existingUnits!!);
+    //     } else if (a.proposedUnits != null && a.existingUnits != null) {
+    //         return -1;
+    //     } else if (b.proposedUnits != null && b.existingUnits != null) {
+    //         return 1;
+    //     } else {
+    //         return b.id!! - a.id!!;
+    //     }
+    // }));
+    if (args.minUnits) {
+        builder = builder.where('"proposedUnits"-"existingUnits" >= ' + args.minUnits);
+    }
+    if (args.year && args.year !== 'all') {
+        builder = builder.whereEq('extrasYearEnd', args.year);
+    }
+    let verified = builder.whereEq('verified', true);
+
+    return {
+        ...(await builder.findAll()),
+        stats: {
+            newUnits: builder.sum('\"proposedUnits"-"existingUnits\"'),
+            newUnitsVerified: verified.sum('\"proposedUnits"-"existingUnits\"'),
+            totalProjects: builder.count(),
+            totalProjectsVerified: verified.count()
+        }
+    };
+};
+
+let buildingProject = function (areaId: number, args: { slug: number }) {
+    return DB.BuidlingProject.findOne({
+        where: {
+            account: areaId,
+            projectId: args.slug
+        }
+    });
+};
 
 export const Resolver = {
     BuildingProject: {
@@ -154,131 +289,24 @@ export const Resolver = {
     },
     Query: {
         buildingProjectsStats: async function (_: any, args: {}, context: CallContext) {
-
-            let projectsQuery = new SelectBuilder(DB.BuidlingProject)
-                .whereEq('account', context.accountId);
-            let projectsTracked = projectsQuery
-                .count();
-            let projectsVerified = projectsQuery
-                .whereEq('verified', true)
-                .count();
-
-            let year2017NewUnits = projectsQuery
-                .whereEq('extrasYearEnd', '2017')
-                .sum('\"proposedUnits" - "existingUnits\"');
-            let year2017NewUnitsVerified = projectsQuery
-                .whereEq('extrasYearEnd', '2017')
-                .whereEq('verified', true)
-                .sum('\"proposedUnits" - "existingUnits\"');
-            let year2018NewUnits = projectsQuery
-                .whereEq('extrasYearEnd', '2018')
-                .sum('\"proposedUnits" - "existingUnits\"');
-            let year2018NewUnitsVerified = projectsQuery
-                .whereEq('extrasYearEnd', '2018')
-                .whereEq('verified', true)
-                .sum('\"proposedUnits" - "existingUnits\"');
-
-            let allProjects = (await DB.BuidlingProject.findAll({
-                where: {
-                    account: context.accountId
-                },
-                include: [{
-                    model: DB.Permit,
-                    as: 'permits',
-                    include: [{
-                        model: DB.StreetNumber,
-                        as: 'streetNumbers',
-                        include: [{
-                            model: DB.Street,
-                            as: 'street',
-                        }]
-                    }]
-                }]
-            })).filter((v) =>
-                (v.permits!!.find((p) => p.permitCreated !== null && p.permitIssued !== null))
-                && (v.existingUnits != null)
-                && (v.proposedUnits != null)
-                && ((v.proposedUnits - v.existingUnits) >= 10));
-
-            let fastestProject = allProjects[0];
-            let slowestProject = allProjects[0];
-            let fastestDuration = dateDiff(new Date(fastestProject.permits!![0].permitCreated!!), new Date(fastestProject.permits!![0].permitIssued!!));
-            let slowestDuration = dateDiff(new Date(fastestProject.permits!![0].permitCreated!!), new Date(fastestProject.permits!![0].permitIssued!!));
-
-            for (let proj of allProjects) {
-                for (let p of proj.permits!!) {
-                    if (p.permitCreated && p.permitIssued) {
-                        let duration = dateDiff(new Date(p.permitCreated!!), new Date(p.permitIssued!!));
-                        if (duration < fastestDuration) {
-                            fastestDuration = duration;
-                            fastestProject = proj;
-                        }
-                        if (duration > slowestDuration) {
-                            slowestDuration = duration;
-                            slowestProject = proj;
-                        }
-                    }
-                }
-            }
-
-            return {
-                projectsTracked: projectsTracked,
-                projectsVerified: projectsVerified,
-                year2017NewUnits: year2017NewUnits,
-                year2017NewUnitsVerified: year2017NewUnitsVerified,
-                year2018NewUnits: year2018NewUnits,
-                year2018NewUnitsVerified: year2018NewUnitsVerified,
-                fastestApprovalProject: fastestProject,
-                slowestApprovalProject: slowestProject
-            };
+            return buildingProjectsStats(context.accountId);
         },
         buildingProjects: async function (_: any, args: { first: number, minUnits?: number, year?: string, filter?: string, after?: string }, context: CallContext) {
-            let builder = new SelectBuilder(DB.BuidlingProject)
-                .whereEq('account', context.accountId)
-                .filterField('name')
-                .filterField('extrasAddress')
-                .filterField('extrasAddressSecondary')
-                .limit(args.first)
-                .after(args.after)
-                .filter(args.filter)
-                .whereEq('account', context.accountId)
-                .orderByRaw('"proposedUnits"-"existingUnits"', 'DESC NULLS LAST');
-            // .postProcessor((src) => src.sort((a, b) => {
-            //     if (a.proposedUnits != null && b.proposedUnits != null && a.existingUnits != null && b.existingUnits != null) {
-            //         return (b.proposedUnits!! - b.existingUnits!!) - (a.proposedUnits!! - a.existingUnits!!);
-            //     } else if (a.proposedUnits != null && a.existingUnits != null) {
-            //         return -1;
-            //     } else if (b.proposedUnits != null && b.existingUnits != null) {
-            //         return 1;
-            //     } else {
-            //         return b.id!! - a.id!!;
-            //     }
-            // }));
-            if (args.minUnits) {
-                builder = builder.where('"proposedUnits"-"existingUnits" >= ' + args.minUnits);
-            }
-            if (args.year && args.year !== 'all') {
-                builder = builder.whereEq('extrasYearEnd', args.year);
-            }
-            let verified = builder.whereEq('verified', true);
-
-            return {
-                ...(await builder.findAll()),
-                stats: {
-                    newUnits: builder.sum('\"proposedUnits"-"existingUnits\"'),
-                    newUnitsVerified: verified.sum('\"proposedUnits"-"existingUnits\"'),
-                    totalProjects: builder.count(),
-                    totalProjectsVerified: verified.count()
-                }
-            };
+            return buildingProjects(context.accountId, args);
         },
         buildingProject: function (_: any, args: { slug: number }, call: CallContext) {
-            return DB.BuidlingProject.findOne({
-                where: {
-                    account: call.accountId,
-                    projectId: args.slug
-                }
-            });
+            return buildingProject(call.accountId, args);
         }
     },
+    Area: {
+        buildingProjectsStats: async function (area: { id: number }) {
+            return buildingProjectsStats(area.id);
+        },
+        buildingProjects: async function (area: { id: number }, args: { first: number, minUnits?: number, year?: string, filter?: string, after?: string }) {
+            return buildingProjects(area.id, args);
+        },
+        buildingProject: function (area: { id: number }, args: { slug: number }) {
+            return buildingProject(area.id, args);
+        }
+    }
 };
