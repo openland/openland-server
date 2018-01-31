13ad64ab6912527562e3fe534976a63f24896e59
Steve Kite <steve@statecraft.one>
2018-01-31 01:29:55 -0800

    Faster account resolving

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,7 +1,7 @@
 import { DB, Account } from '../tables';
 import { CallContext } from './CallContext';
 import { User } from '../tables';
-
+import * as DataLoader from 'dataloader';
 export const Schema = `
     type Account {
         id: ID!
@@ -102,16 +102,24 @@ export async function resolveAccountId(domain: string) {
     return res.id!!;
 }
 
+let dataLoader = new DataLoader<number, Account | null>(async (accountIds) => {
+    let promises = accountIds.map((v) => DB.Account.findOne({
+        where: {
+            id: v,
+            activated: true
+        }
+    }));
+    let res: (Account | null)[] = [];
+    for (let p of promises) {
+        res.push(await p);
+    }
+    return res;
+});
+
 export const Resolver = {
     Query: {
         account: async function (_: any, args: {}, context: CallContext) {
-            let account = await DB.Account.findOne({
-                where: {
-                    id: context.accountId,
-                    activated: true
-                }
-            })!!;
-            return convertAccount(account, context);
+            return convertAccount(await dataLoader.load(context.accountId), context);
         }
     }
 };
