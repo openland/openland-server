d41910c8338ab6c850b799eacd27fe8175ac38b2
Steve Kite <steve@statecraft.one>
2018-03-15 18:48:24 -0700

    More fields to Deals

--- src/api/Deals.ts
+++ src/api/Deals.ts
@@ -3,10 +3,26 @@ import { DB } from '../tables';
 import { Deal } from '../tables/Deal';
 import { IDs } from './utils/IDs';
 
+interface DealInput {
+    title: string;
+    status?: 'ACTIVE' | 'CLOSED' | 'ON_HOLD';
+    statusDescription?: string;
+    statusDate?: string;
+
+    location?: string;
+    address?: string;
+}
+
 export const Resolver = {
     Deal: {
         id: (src: Deal) => IDs.Deal.serialize(src.id!!),
         title: (src: Deal) => src.title,
+        status: (src: Deal) => src.status,
+        statusDescription: (src: Deal) => src.statusDescription,
+        statusDate: (src: Deal) => src.statusDate,
+
+        location: (src: Deal) => src.location,
+        address: (src: Deal) => src.address,
     },
     Query: {
         deals: withAccount((args, uid, org) => {
@@ -14,10 +30,14 @@ export const Resolver = {
         })
     },
     Mutation: {
-        dealAdd: withAccount<{ input: { title: string } }>((args, uid, org) => {
-            console.warn(org);
+        dealAdd: withAccount<{ input: DealInput }>((args, uid, org) => {
             return DB.Deal.create({
                 title: args.input.title,
+                status: args.input.status,
+                statusDescription: args.input.statusDescription,
+                statusDate: args.input.statusDate,
+                location: args.input.location,
+                address: args.input.address,
                 organizationId: org
             });
         })
--- src/tables/Deal.ts
+++ src/tables/Deal.ts
@@ -6,6 +6,13 @@ export interface DealAttributes {
     id?: number;
     title?: string;
 
+    status?: string | null;
+    statusDescription?: string | null;
+    statusDate?: string | null;
+
+    address?: string | null;
+    location?: string | null;
+
     organizationId?: number | null;
     organization?: Organization | null;
 }
@@ -15,7 +22,14 @@ export interface Deal extends sequelize.Instance<DealAttributes>, DealAttributes
 
 export const DealTable = connection.define<Deal, DealAttributes>('deal', {
     id: { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    title: { type: sequelize.STRING, allowNull: false }
+    title: { type: sequelize.STRING, allowNull: false },
+
+    status: { type: sequelize.STRING, allowNull: true },
+    statusDescription: { type: sequelize.STRING, allowNull: true },
+    statusDate: { type: sequelize.DATE, allowNull: true },
+
+    address: { type: sequelize.STRING, allowNull: true },
+    location: { type: sequelize.STRING, allowNull: true },
 });
 
 DealTable.belongsTo(OrganizationTable, { as: 'organization', foreignKey: { allowNull: false } });
