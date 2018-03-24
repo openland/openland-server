ded225462ba747c08e620a58e80cab4594538db1
Steve Kite <steve@statecraft.one>
2018-03-23 21:41:11 -0700

    Better handlers for authentication

--- src/handlers/authV2.ts
+++ src/handlers/authV2.ts
@@ -35,12 +35,18 @@ export const JWTChecker = jwt({
 //
 
 export const TokenChecker = async function (req: express.Request, response: express.Response, next: express.NextFunction) {
-    let accessToken = req.headers['x-openland-token'];
-    if (accessToken) {
-        let uid = await Repos.Tokens.fetchUserByToken(accessToken as string);
-        if (uid !== null) {
-            req.user = { id: uid };
+    try {
+        let accessToken = req.headers['x-openland-token'];
+        if (accessToken) {
+            let uid = await Repos.Tokens.fetchUserByToken(accessToken as string);
+            if (uid !== null) {
+                req.user = { id: uid };
+            }
         }
+    } catch (e) {
+        console.warn(e);
+        response.status(500).send({ error: 'Internal server error' });
+        return;
     }
     next();
 };
@@ -50,56 +56,76 @@ export const TokenChecker = async function (req: express.Request, response: expr
 //
 
 export const Authenticator = async function (req: express.Request, response: express.Response) {
-
-    //
-    // Fetching Profile Info
-    //
-    let accessToken = req.headers['x-openland-access-token'];
-    let res = await fetch('https://auth.openland.com/userinfo', {
-        headers: {
-            authorization: 'Bearer ' + accessToken
-        }
-    });
-    let profile = await res.json<Profile>();
-
-    //
-    // Get Or Create User
-    //
-    let uid = await DB.tx(async () => {
-        let userKey = req.user.sub;
-        let user = await DB.User.find({
-            where: {
-                authId: userKey
+    try {
+        console.warn('auth1');
+        //
+        // Fetching Profile Info
+        //
+        let accessToken = req.headers['x-openland-access-token'];
+        let res = await fetch('https://auth.openland.com/userinfo', {
+            headers: {
+                authorization: 'Bearer ' + accessToken
             }
         });
-        if (user != null) {
-            await DB.User.update({
-                firstName: profile.given_name,
-                lastName: profile.family_name,
-                email: profile.email,
-                picture: profile.picture
-            }, {
-                    where: {
-                        authId: userKey
-                    },
-                });
-            return user.id!!;
-        } else {
-            return (await DB.User.create({
-                authId: userKey,
-                firstName: profile.given_name,
-                lastName: profile.family_name,
-                email: profile.email,
-                picture: profile.picture
-            })).id!!;
+        let profile = await res.json<Profile>();
+
+        let firstName = 'No Name';
+        let lastName = '';
+        if (profile.nickname) {
+            firstName = profile.nickname;
         }
-    });
+        if (profile.given_name) {
+            firstName = profile.given_name;
+        }
+        if (profile.family_name) {
+            lastName = profile.family_name;
+        }
 
-    //
-    // Create New Token
-    //
+        console.warn('auth2');
+        console.warn(profile);
 
-    let token = await Repos.Tokens.createToken(uid);
+        //
+        // Get Or Create User
+        //
+        let uid = await DB.tx(async () => {
+            let userKey = req.user.sub;
+            let user = await DB.User.find({
+                where: {
+                    authId: userKey
+                }
+            });
+            if (user != null) {
+                await DB.User.update({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: profile.email,
+                    picture: profile.picture
+                }, {
+                        where: {
+                            authId: userKey
+                        },
+                    });
+                return user.id!!;
+            } else {
+                return (await DB.User.create({
+                    authId: userKey,
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: profile.email,
+                    picture: profile.picture
+                })).id!!;
+            }
+        });
 
-    response.json({ ok: true, token: token });
+        //
+        // Create New Token
+        //
+
+        let token = await Repos.Tokens.createToken(uid);
+
+        response.json({ ok: true, token: token });
+    } catch (e) {
+        console.warn(e);
+        response.status(500).send({ error: 'Internal server error' });
+    }
 };
