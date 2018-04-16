ad94aee47cc90fc9b44677d646ef3a396b9d61c4
Steve Kite <steve@statecraft.one>
2018-04-16 14:55:21 -0700

    Added BISWeb service

--- src/api/Services.ts
+++ src/api/Services.ts
@@ -5,6 +5,9 @@ export const Resolvers = {
     Query: {
         servicesNYCProperties: withAny<{ borough: number, block: number, lot: number }>(async (args) => {
             return Services.NYCProperties.fetchPropertyInformation(args.borough, args.block, args.lot);
+        }),
+        servicesNYCBisWeb: withAny<{ borough: number, block: number }>(async (args) => {
+            return Services.NYCBisWeb.fetchBlock(args.borough, args.block);
         })
     }
 };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,7 @@
 import { NYCProperties } from './NYCProperties';
+import { NYCBISWEB } from './NYCBISWEB';
 
 export const Services = {
-    NYCProperties: new NYCProperties()
+    NYCProperties: new NYCProperties(),
+    NYCBisWeb: new NYCBISWEB()
 };
