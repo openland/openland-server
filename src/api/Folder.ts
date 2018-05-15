829b0a33ceb23c6bfb1697c375a40477cf782e1b
Steve Kite <steve@statecraft.one>
2018-05-15 14:26:38 +0300

    Return actual list of folders

--- src/api/Folder.ts
+++ src/api/Folder.ts
@@ -1,15 +1,54 @@
 import { withAccount } from './utils/Resolvers';
+import { Folder } from '../tables/Folder';
+import { IDs } from './utils/IDs';
+import { DB } from '../tables';
 
 export const Resolver = {
+    Folder: {
+        id: (src: Folder | 'favorites' | 'all') => {
+            switch (src) {
+                case 'favorites':
+                    return 'favorites';
+                case 'all':
+                    return 'all';
+                default:
+                    return IDs.Folder.serialize(src.id!!);
+            }
+        },
+        name: (src: Folder | 'favorites' | 'all') => {
+            switch (src) {
+                case 'favorites':
+                    return 'Favorites';
+                case 'all':
+                    return 'All Parcels';
+                default:
+                    return src.name;
+            }
+        },
+        special: (src: Folder | 'favorites' | 'all') => {
+            switch (src) {
+                case 'favorites':
+                    return 'FAVORITES';
+                case 'all':
+                    return 'ALL';
+                default:
+                    return null;
+            }
+        },
+    },
     Query: {
         alphaFolders: withAccount(async (args, uid, orgId) => {
-            // let readers = (await DB.ReaderState.findAll());
-            // return readers.map((v) => ({
-            //     id: IDs.DebugReader.serialize(v.id!!),
-            //     title: normalizeCapitalized(v.key!!.replace('_', ' ')),
-            //     remaining: v.remaining
-            // }));
-            return [];
+            let res: (Folder | 'favorites' | 'all')[] = [];
+            res.push('all');
+            res.push('favorites');
+            let folders = await DB.Folder.findAll({
+                where: {
+                    organizationId: orgId
+                }
+            });
+            folders = folders.sort((a, b) => a.name!!.localeCompare(b.name!!));
+            res = [...res, ...folders];
+            return res;
         })
     }
 };
--- src/api/utils/IDs.ts
+++ src/api/utils/IDs.ts
@@ -13,4 +13,5 @@ export const IDs = {
     FeatureFlag: new ID('FeatureFlag'),
     Opportunities: new ID('Op'),
     DebugReader: new ID('dr'),
+    Folder: new ID('f'),
 };
