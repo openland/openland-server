2747afbf8076858b93eb277cee6168d65a7386fa
Steve Kite <steve@statecraft.one>
2018-05-31 02:19:48 +0300

    Fix delay in SampleWorker

--- src/workers/SampleWorker.ts
+++ src/workers/SampleWorker.ts
@@ -4,7 +4,7 @@ import { delay } from '../utils/timer';
 export function createSampleWorker() {
     let queue = new WorkQueue<{ someArgument: number }, { multiplied: number }>('sampleTask');
     queue.addWorker(async (item) => {
-        delay(5000);
+        await delay(5000);
         return { multiplied: item.someArgument * 2 };
     });
     return queue;
