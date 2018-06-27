cb509067453f91a7a058cfbafa8ae794e4b538f2
altox <xeroxaltox@gmail.com>
2018-06-27 18:16:08 +0300

    cosmetic fix

--- src/errors/InvalidInputError.ts
+++ src/errors/InvalidInputError.ts
@@ -1,7 +1,7 @@
 export class InvalidInputError extends Error {
-    fields: { key: string, message: string }[];
-    constructor(fields: { key: string, message: string }[]) {
+    constructor(
+        public fields: { key: string, message: string }[]
+    ) {
         super(fields[0].message);
-        this.fields = fields;
     }
 }
