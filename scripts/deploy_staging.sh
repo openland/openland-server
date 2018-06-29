c0b25a34c470090a4d298147a7657271e19f6622
Steve Kite <steve@statecraft.one>
2018-06-29 18:20:04 +0300

    Fix deployment

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-./kubectl set image deployment/statecraft-server-staging statecraft-server-staging=index.docker.io/statecraft/statecraft-server:${1}
+./kubectl set image deployment/statecraft-server-staging statecraft-server-staging=index.docker.io/openland/server:${1}
 ./kubectl rollout status deployments statecraft-server-staging || true
