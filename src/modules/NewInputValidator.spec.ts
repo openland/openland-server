b22f558c3904ae32d7705faa9aab91d5d73006b9
Steve Kite <steve@statecraft.one>
2018-07-01 23:14:57 +0300

    Add more tests

--- src/modules/NewInputValidator.spec.ts
+++ src/modules/NewInputValidator.spec.ts
@@ -2,10 +2,17 @@ import { validate, stringNotEmpty } from './NewInputValidator';
 
 describe('New Input Validator', () => {
     it('should validate strings', async () => {
-        await validate({ name: stringNotEmpty(), }, { name: 'something' });
-        expect(validate({ name: stringNotEmpty(), }, { name: '' })).rejects.toThrow();
-        expect(validate({ name: stringNotEmpty(), }, { name: null })).rejects.toThrow();
-        expect(validate({ name: stringNotEmpty(), }, { name: undefined })).rejects.toThrow();
-        expect(validate({ name: stringNotEmpty(), }, {  })).rejects.toThrow();
+        expect(validate({ name: stringNotEmpty() }, { name: 'something' })).resolves.toBe(undefined);
+        expect(validate({ name: stringNotEmpty() }, { name: '' })).rejects.toThrow();
+        expect(validate({ name: stringNotEmpty() }, { name: null })).rejects.toThrow();
+        expect(validate({ name: stringNotEmpty() }, { name: undefined })).rejects.toThrow();
+        expect(validate({ name: stringNotEmpty() }, {})).rejects.toThrow();
+    });
+    it('should validate structs', async () => {
+        expect(validate({ something: { name: stringNotEmpty() } }, { something: { name: 'something' } })).resolves.toBe(undefined);
+        expect(validate({ something: { name: stringNotEmpty() } }, { something: { name: '' } })).rejects.toThrow();
+    });
+    it('should ignore unknown fields', async () => {
+        expect(validate({ something: { name: stringNotEmpty() } }, { something: { name: 'something', some: 'str' } })).resolves.toBe(undefined);
     });
 });
