7eeef6bd90446628669023aa2739bc4336888e53
Steve Kite <steve@statecraft.one>
2018-07-02 00:28:46 +0300

    Fixing compilation

--- src/tests/express.spec.ts
+++ src/tests/express.spec.ts
@@ -1,11 +1,9 @@
-import express from 'express';
 import prepareApp from './utils/prepareApp';
 import { initApi } from '../init/initApi';
 
-var server: express.Express;
 beforeAll(async () => {
     await prepareApp();
-    server = await initApi(true);
+    await initApi(true);
 });
 
 describe('API Server', () => {
