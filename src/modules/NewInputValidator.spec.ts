05965d91385ce7a2707e01b9d3cf1058be5e1985
Steve Kite <steve@statecraft.one>
2018-07-01 23:02:24 +0300

    Fixing tests

--- src/modules/NewInputValidator.spec.ts
+++ src/modules/NewInputValidator.spec.ts
@@ -3,6 +3,9 @@ import { validate, stringNotEmpty } from './NewInputValidator';
 describe('New Input Validator', () => {
     it('should validate strings', async () => {
         await validate({ name: stringNotEmpty(), }, { name: 'something' });
-        expect(() => validate({ name: stringNotEmpty(), }, { name: '' })).toThrow();
+        expect(validate({ name: stringNotEmpty(), }, { name: '' })).rejects.toThrow();
+        expect(validate({ name: stringNotEmpty(), }, { name: null })).rejects.toThrow();
+        expect(validate({ name: stringNotEmpty(), }, { name: undefined })).rejects.toThrow();
+        expect(validate({ name: stringNotEmpty(), }, {  })).rejects.toThrow();
     });
 });
