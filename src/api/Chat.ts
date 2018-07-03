49097f399f151f6357faeefd25ff53e1f110d3e0
Steve Kite <steve@statecraft.one>
2018-07-03 04:57:08 +0300

    Added simple chat subscription

--- src/api/Chat.ts
+++ src/api/Chat.ts
@@ -4,6 +4,10 @@ import { Conversation } from '../tables/Conversation';
 import { DB } from '../tables';
 import { withPermission, withAny, withAccount } from './utils/Resolvers';
 import { validate, stringNotEmpty } from '../modules/NewInputValidator';
+import { Pubsub } from '../modules/pubsub';
+import { delayBreakable } from '../utils/timer';
+
+let pubsub = new Pubsub<{ messageId: number }>();
 
 export const Resolver = {
     ConversationMessage: {
@@ -43,14 +47,46 @@ export const Resolver = {
                 title: args.title
             });
         }),
-        alphaSendMessage: withAccount<{ conversationId: string, message: string }>((args, uid) => {
+        alphaSendMessage: withAccount<{ conversationId: string, message: string }>(async (args, uid) => {
             validate({ message: stringNotEmpty() }, args);
             let conversationId = IDs.Conversation.parse(args.conversationId);
-            return DB.ConversationMessage.create({
+            let res = await DB.ConversationMessage.create({
                 message: args.message,
                 conversationId: conversationId,
                 userId: uid
             });
+            pubsub.publish('chat_' + conversationId, { messageId: res.id });
+            return res;
         })
+    },
+    Subscription: {
+        alphaChatSubscribe: {
+            resolve: async (msg: number) => {
+                console.warn(msg);
+                return DB.ConversationMessage.findById(msg);
+            },
+            subscribe: async function* (_: any, args: { conversationId: string }) {
+                let conversationId = IDs.Conversation.parse(args.conversationId);
+                let lastMessageId: number = -1;
+                let breakable: (() => void) | null = null;
+                pubsub.subscribe('chat_' + conversationId, (msg) => {
+                    lastMessageId = msg.messageId;
+                    if (breakable) {
+                        breakable();
+                        breakable = null;
+                    }
+                });
+                while (true) {
+                    let delay = delayBreakable(1000);
+                    breakable = delay.resolver;
+                    await delay.promise;
+                    if (lastMessageId !== -1) {
+                        let msg = lastMessageId;
+                        lastMessageId = -1;
+                        yield msg;
+                    }
+                }
+            }
+        }
     }
 };
