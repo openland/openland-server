2fd73001b56f94896066da80cec48deeb96040c1
kor_ka <gleb.putintsev@openland.com>
2018-07-05 01:54:01 +0300

    damn you Sequelize

--- src/repositories/OrganizationRepository.ts
+++ src/repositories/OrganizationRepository.ts
@@ -1,11 +1,12 @@
 import { OrganizationMember } from '../tables/OrganizationMember';
 import { DB } from '../tables';
+import sequelize from 'sequelize';
 
 export class OrganizationRepository {
     async getOrganizationMembers(orgId: number): Promise<OrganizationMember[]> {
         return await DB.OrganizationMember.findAll({
             where: { orgId },
-            order: ['user.userProfile.name'],
+            order: [sequelize.literal('user.userProfile.name')],
             include: [{
                 model: DB.User,
                 as: 'user',
