8a491cdd9f0d75c9a3801eab6dbf0679a5b400c8
kor_ka <gleb.putintsev@openland.com>
2018-07-12 21:56:18 +0300

    fix activate via invites

--- src/api/Account.ts
+++ src/api/Account.ts
@@ -176,12 +176,12 @@ export const Resolver = {
                     let user = await DB.User.find({ where: { id: uid }, transaction: tx });
                     if (user) {
                         user.status = 'ACTIVATED';
-                        await user.save({ transaction: tx });
+                        await user.save();
                     }
                 }
 
                 if (invite.isOneTime === true) {
-                    await invite.destroy();
+                    await invite.destroy({ transaction: tx });
                 }
 
                 return 'ok';
