9392fc03dd49b292964d32a77bc4dcad3faa986f
altox <xeroxaltox@gmail.com>
2018-07-12 18:25:31 +0300

    fix migration

--- src/tables/migrations/20180712130056-AddUserStatus.ts
+++ src/tables/migrations/20180712130056-AddUserStatus.ts
@@ -1,6 +1,6 @@
 import { QueryInterface, DataTypes } from 'sequelize';
-import { DB } from '..';
-import { Repos } from '../../repositories';
+// import { DB } from '..';
+// import { Repos } from '../../repositories';
 
 export async function up(queryInterface: QueryInterface, sequelize: DataTypes) {
 
@@ -14,16 +14,30 @@ export async function up(queryInterface: QueryInterface, sequelize: DataTypes) {
         allowNull: false
     });
 
-    let users = await DB.User.all();
-    for (let user of users) {
-        let orgs = await Repos.Users.fetchUserAccounts(user.id!!);
-        for (let orgid of orgs) {
-            let org = await DB.Organization.find({ where: { id: orgid } });
-            if (org && org.status === 'ACTIVATED') {
-                user.status = 'ACTIVATED';
+    let users = await queryInterface.sequelize.query('select * from "users";');
+
+    for (let user of users[0]) {
+        let orgs = await queryInterface.sequelize.query(
+            'select "organizations".* from "organization_members" inner join "organizations" on "organization_members"."orgId" = "organizations".id where "organization_members"."userId" = :userId;',
+            {
+                replacements: {
+                    userId: user.id
+                }
+            }
+        );
+
+        for (let org of orgs[0]) {
+            if (org.status === 'ACTIVATED') {
+                await queryInterface.sequelize.query(
+                    'update "users" set "status" = \'ACTIVATED\' where id=:userId;',
+                    {
+                        replacements: {
+                            userId: user.id
+                        }
+                    }
+                );
                 break;
             }
         }
-        user.save();
     }
 }
