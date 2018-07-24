a8d1fc4e0674726e1e47f9c350b0f250780fa2d4
Steve Kite <steve@statecraft.one>
2018-07-24 23:08:32 +0300

    Add iOS push notificaitons

--- src/init/initConfig.ts
+++ src/init/initConfig.ts
@@ -3,6 +3,7 @@ import WebPush from 'web-push';
 
 class AppConfig {
     webPush: { private: string, public: string } | undefined;
+    apple: { teamId: string, key: string, keyId: string, bundles: string[] }[] | undefined;
 }
 
 export const AppConfiuguration = new AppConfig();
@@ -12,11 +13,19 @@ interface CertsConfig {
         private: string,
         public: string;
     } | undefined | null;
+    'apple': {
+        teams: {
+            key: string;
+            'key-id': string;
+            'team-id': string;
+            bundles: string[];
+        }[] | undefined | null
+    } | undefined | null;
 }
 
 if (process.env.PUSH_CERTS_PATH) {
     let config = JSON.parse(fs.readFileSync(process.env.PUSH_CERTS_PATH, 'utf-8')) as CertsConfig;
-    
+
     // Loading Web Push
     if (config['web-push']) {
         AppConfiuguration.webPush = { private: config['web-push']!!.private, public: config['web-push']!!.public };
@@ -25,5 +34,9 @@ if (process.env.PUSH_CERTS_PATH) {
             AppConfiuguration.webPush.public,
             AppConfiuguration.webPush.private
         );
+    }
+    if (config.apple && config.apple.teams && config.apple.teams.length > 0) {
+        let t = config.apple!!.teams!!;
+        AppConfiuguration.apple = t.map((v) => ({ teamId: v['team-id'], keyId: v['key-id'], key: v.key, bundles: v.bundles }));
     }
 }
--- src/workers/PushWorker.ts
+++ src/workers/PushWorker.ts
@@ -2,10 +2,13 @@ import { WorkQueue } from '../modules/workerQueue';
 import { DB } from '../tables';
 import WebPush from 'web-push';
 import { AppConfiuguration } from '../init/initConfig';
+import APN from 'apn';
+
+let providers = new Map<string, APN.Provider>();
 
 export function createPushWorker() {
     let queue = new WorkQueue<{ uid: number, title: string, body: string, picture: string | null, }, { result: string }>('push_sender');
-    if (AppConfiuguration.webPush) {
+    if (AppConfiuguration.webPush || AppConfiuguration.apple) {
         console.log('Starting push worker');
         queue.addWorker(async (args, lock) => {
             let registrations = await DB.UserPushRegistration.findAll({
@@ -22,6 +25,37 @@ export function createPushWorker() {
                             body: args.body,
                             picture: args.picture
                         }));
+                    } catch (e) {
+                        // Fast ignore for push notifications
+                        console.warn(e);
+                    }
+                } else if (reg.pushType === 'ios' && AppConfiuguration.apple) {
+                    try {
+                        let endpoint = JSON.parse(reg.pushEndpoint);
+                        let bundleId = endpoint.bundleId as string;
+                        let token = endpoint.token as string;
+                        let isSandbox = endpoint.sandbox as string;
+                        // Ignore production for now
+                        if (!isSandbox) {
+                            continue;
+                        }
+                        let team = AppConfiuguration.apple.find((v) => v.bundles.indexOf(bundleId) > 0);
+                        if (team) {
+                            if (!providers.has(team.teamId)) {
+                                providers.set(team.teamId, new APN.Provider({
+                                    token: {
+                                        key: Buffer.from(team.key, 'base64'),
+                                        keyId: team.keyId,
+                                        teamId: team.teamId
+                                    }
+                                }));
+                            }
+                            var not = new APN.Notification();
+                            not.expiry = Math.floor(Date.now() / 1000) + 3600;
+                            not.alert = { title: args.title, body: args.body };
+                            not.topic = bundleId;
+                            await (providers.get(team.teamId)!!).send(not, token);
+                        }
                     } catch (e) {
                         // Fast ignore for push notifications
                         console.warn(e);
