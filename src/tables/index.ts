543c13b35f8919512d73bd4ffb0a35a9515c6763
altox <xeroxaltox@gmail.com>
2018-08-31 00:34:59 +0300

    db silent flag

--- src/tables/index.ts
+++ src/tables/index.ts
@@ -76,7 +76,7 @@ import { ConversationChannelMembersTable } from './ConversationChannelMembers';
 import { ChannelInviteTable } from './ChannelInvite';
 
 const SILENT_TX_ACTUALLY_SILENT = false;
-export const DB_SILENT = SILENT_TX_ACTUALLY_SILENT;
+export const DB_SILENT = !SILENT_TX_ACTUALLY_SILENT;
 
 export const DB = {
     User: UserTable,
