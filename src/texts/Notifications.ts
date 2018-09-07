b6157c61b89341a91513f7614a3407c9cbde40e1
altox <xeroxaltox@gmail.com>
2018-09-07 15:27:52 +0300

    "at" -> @

--- src/texts/Notifications.ts
+++ src/texts/Notifications.ts
@@ -4,5 +4,5 @@ export const Notifications = {
     NEW_MESSAGE_ANONYMOUS: 'New message',
     FILE_ATTACH: '<file>',
     IMAGE_ATTACH: '<image>',
-    GROUP_PUSH_TITLE: templated<{senderName: string, chatTitle: string}>('{{senderName}} at {{chatTitle}}')
+    GROUP_PUSH_TITLE: templated<{senderName: string, chatTitle: string}>('{{senderName}}@{{chatTitle}}')
 };
