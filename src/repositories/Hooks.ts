d3011ef905bcc1c7ca837c616be35cda30611a3e
altox <xeroxaltox@gmail.com>
2018-09-07 15:59:01 +0300

    change some texts

--- src/repositories/Hooks.ts
+++ src/repositories/Hooks.ts
@@ -1,16 +1,19 @@
 import { Transaction } from 'sequelize';
 import { Emails } from '../services/Emails';
 import { Repos } from '.';
+import { DB } from '../tables';
 
 export const Hooks = {
     onOrganizstionCreated: async (uid: number, oid: number, tx: Transaction) => {
         let chat = await Repos.Chats.loadOrganizationalChat(oid, oid, tx);
-        await Repos.Chats.sendMessage(tx, chat.id, uid, { message: 'Joined organization', isService: true, isMuted: true });
+        let profile = await DB.UserProfile.find({ where: { userId: uid }, transaction: tx });
+        await Repos.Chats.sendMessage(tx, chat.id, uid, { message: `${profile!.firstName} has joined organization`, isService: true, isMuted: true });
     },
     onUserJoined: async (uid: number, oid: number, tx: Transaction) => {
         await Emails.sendMemberJoinedEmails(oid, uid, tx);
         let chat = await Repos.Chats.loadOrganizationalChat(oid, oid, tx);
-        await Repos.Chats.sendMessage(tx, chat.id, uid, { message: 'Joined organization', isService: true, isMuted: true });
+        let profile = await DB.UserProfile.find({ where: { userId: uid }, transaction: tx });
+        await Repos.Chats.sendMessage(tx, chat.id, uid, { message: `${profile!.firstName} has joined organization`, isService: true, isMuted: true });
     },
     onUserRemoved: async (uid: number, oid: number, tx: Transaction) => {
         //
