106cbec11f2ee4193a71fb154b414eef576df53f
altox <xeroxaltox@gmail.com>
2018-09-07 15:54:52 +0300

    fix wrong push titles caused by bug in templates rendering

--- src/texts/util.ts
+++ src/texts/util.ts
@@ -1,10 +1,11 @@
 export function templated<T extends { [key: string]: string } = any>(template: string) {
     return (vars: T) => {
+        let rendered = '' + template;
 
         for (let key of Object.keys(vars)) {
-            template = template.replace(new RegExp('{{' + key + '}}', 'g'), vars[key]);
+            rendered = rendered.replace(new RegExp('{{' + key + '}}', 'g'), vars[key]);
         }
 
-        return template;
+        return rendered;
     };
 }
