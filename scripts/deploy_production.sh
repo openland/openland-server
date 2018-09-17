e91070f9d28938b951a054574c77273d0c6f20b3
Steve Kite <steve@statecraft.one>
2018-09-18 00:43:59 +0300

    Add Schema Upload

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -2,3 +2,4 @@
 set -e
 ./kubectl set image deployment/statecraft-server statecraft-server=index.docker.io/openland/server:${1}
 ./kubectl rollout status deployments statecraft-server || true
+apollo schema:publish --key="service:openland-production:qrYALC8Flb8hv-SGm4grug" --endpoint="https://api.openland.com/api"
