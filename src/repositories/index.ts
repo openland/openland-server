1f4db75eb5e2cf00e59d3f88d19ecda73819dac7
altox <xeroxaltox@gmail.com>
2018-09-18 16:33:38 +0300

    phone repo

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -11,6 +11,7 @@ import { FoldersRepository } from './FoldersRepository';
 import { OrganizationRepository } from './OrganizationRepository';
 import { ChatsRepository } from './ChatRepository';
 import InvitesRepository from './InvitesRepository';
+import { PhoneRepository } from './PhoneRepository';
 
 export const Repos = {
     Area: new AreaRepository(),
@@ -25,5 +26,6 @@ export const Repos = {
     Folders: new FoldersRepository(),
     Organizations: new OrganizationRepository(),
     Chats: new ChatsRepository(),
-    Invites: new InvitesRepository()
+    Invites: new InvitesRepository(),
+    Phones: new PhoneRepository()
 };
