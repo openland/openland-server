9ee7f1ea4795fc96d42cc20f80cf863a9f151ad8
altox <xeroxaltox@gmail.com>
2018-10-01 18:51:33 +0300

    added transactions to auth methods

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -58,187 +58,193 @@ export function withAudit(handler: (req: express.Request, response: express.Resp
 }
 
 export async function sendCode(req: express.Request, response: express.Response) {
-    let {
-        email,
-        phone,
-        session
-    } = req.body;
+    return await DB.txLight(async (tx) => {
+        let {
+            email,
+            phone,
+            session
+        } = req.body;
 
-    console.log('auth_sendCode', JSON.stringify(req.body));
+        console.log('auth_sendCode', JSON.stringify(req.body));
 
-    let authSession: AuthSession;
+        let authSession: AuthSession;
 
-    if (session) {
-        let existing = await DB.AuthSession.findOne({ where: { sessionSalt: session } });
+        if (session) {
+            let existing = await DB.AuthSession.findOne({where: {sessionSalt: session}, transaction: tx});
 
-        // No session found
-        if (!existing) {
-            sendError(response, 2);
-            return;
-        }
-        authSession = existing;
-    }
-
-    if (!email && !phone) {
-        sendError(response, 0);
-        return;
-    }
-
-    let code = randomString(5);
-
-    if (email) {
-        let isTest = isTestEmail(email);
-
-        if (!isTest) {
-            await Emails.sendDebugEmail(email, 'Your code: ' + code);
-        } else {
-            code = testEmailCode(email);
+            // No session found
+            if (!existing) {
+                sendError(response, 2);
+                return;
+            }
+            authSession = existing;
         }
 
-        if (!authSession!) {
-            authSession = await DB.AuthSession.create({
-                sessionSalt: base64.encodeBuffer(randomBytes(64)),
-                code,
-                codeExpires: new Date(Date.now() + 1000 * 60 * 5), // 5 minutes
-                extras: {
-                    email
-                }
-            });
-        } else {
-            await authSession!.update({ code });
-        }
-
-        response.json({ ok: true, session: authSession!.sessionSalt });
-    } else if (phone) {
-        if (!Repos.Phones.checkPhone(phone)) {
+        if (!email && !phone) {
             sendError(response, 0);
             return;
         }
 
-        let isTest = isTestPhone(phone);
+        let code = randomString(5);
 
-        if (!isTest) {
-            await Services.TeleSign.sendSMS(phone, 'Your code: ' + code);
-        } else {
-            code = getTestPhoneCode(phone);
+        if (email) {
+            let isTest = isTestEmail(email);
+
+            if (!isTest) {
+                await Emails.sendDebugEmail(email, 'Your code: ' + code);
+            } else {
+                code = testEmailCode(email);
+            }
+
+            if (!authSession!) {
+                authSession = await DB.AuthSession.create({
+                    sessionSalt: base64.encodeBuffer(randomBytes(64)),
+                    code,
+                    codeExpires: new Date(Date.now() + 1000 * 60 * 5), // 5 minutes
+                    extras: {
+                        email
+                    }
+                }, {transaction: tx});
+            } else {
+                await authSession!.update({code});
+            }
+
+            response.json({ok: true, session: authSession!.sessionSalt});
+        } else if (phone) {
+            if (!Repos.Phones.checkPhone(phone)) {
+                sendError(response, 0);
+                return;
+            }
+
+            let isTest = isTestPhone(phone);
+
+            if (!isTest) {
+                await Services.TeleSign.sendSMS(phone, 'Your code: ' + code);
+            } else {
+                code = getTestPhoneCode(phone);
+            }
+
+            if (!authSession!) {
+                authSession = await DB.AuthSession.create({
+                    sessionSalt: base64.encodeBuffer(randomBytes(64)),
+                    code,
+                    codeExpires: new Date(Date.now() + 1000 * 60 * 5), // 5 minutes
+                    extras: {
+                        phone
+                    }
+                }, {transaction: tx});
+            } else {
+                await authSession!.update({code});
+            }
+
+            response.json({ok: true, session: authSession!.sessionSalt});
         }
-
-        if (!authSession!) {
-            authSession = await DB.AuthSession.create({
-                sessionSalt: base64.encodeBuffer(randomBytes(64)),
-                code,
-                codeExpires: new Date(Date.now() + 1000 * 60 * 5), // 5 minutes
-                extras: {
-                    phone
-                }
-            });
-        } else {
-            await authSession!.update({ code });
-        }
-
-        response.json({ ok: true, session: authSession!.sessionSalt });
-    }
+    };
 }
 
 export async function checkCode(req: express.Request, response: express.Response) {
-    let {
-        session,
-        code
-    } = req.body;
+    return await DB.txLight(async (tx) => {
+        let {
+            session,
+            code
+        } = req.body;
 
-    if (!session && !code) {
-        sendError(response, 0);
-        return;
-    }
+        if (!session && !code) {
+            sendError(response, 0);
+            return;
+        }
 
-    let authSession = await DB.AuthSession.findOne({ where: { sessionSalt: session } });
+        let authSession = await DB.AuthSession.findOne({where: {sessionSalt: session}, transaction: tx});
 
-    // No session found
-    if (!authSession) {
-        sendError(response, 2);
-        return;
-    }
+        // No session found
+        if (!authSession) {
+            sendError(response, 2);
+            return;
+        }
 
-    // Code expired
-    if (new Date() > authSession.codeExpires!) {
-        sendError(response, 3);
-        return;
-    }
+        // Code expired
+        if (new Date() > authSession.codeExpires!) {
+            sendError(response, 3);
+            return;
+        }
 
-    // Wrong code
-    if (authSession.code! !== code) {
-        sendError(response, 4);
-        return;
-    }
+        // Wrong code
+        if (authSession.code! !== code) {
+            sendError(response, 4);
+            return;
+        }
 
-    authSession.extras!.authToken = base64.encodeBuffer(randomBytes(64));
-    (authSession as any).changed('extras', true);
-    await authSession.save();
+        authSession.extras!.authToken = base64.encodeBuffer(randomBytes(64));
+        (authSession as any).changed('extras', true);
+        await authSession.save();
 
-    response.json({ ok: true, authToken: authSession.extras!.authToken });
+        response.json({ok: true, authToken: authSession.extras!.authToken});
+    };
 }
 
 export async function getAccessToken(req: express.Request, response: express.Response) {
-    let {
-        session,
-        authToken
-    } = req.body;
+    return await DB.txLight(async (tx) => {
+        let {
+            session,
+            authToken
+        } = req.body;
 
-    if (!session && !authToken) {
-        sendError(response, 0);
-        return;
-    }
-
-    let authSession = await DB.AuthSession.findOne({ where: { sessionSalt: session, extras: { authToken } } });
-
-    // No session found
-    if (!authSession) {
-        sendError(response, 2);
-        return;
-    }
-
-    if (authSession.extras!.email) {
-        let existing = await DB.User.findOne({ where: { email: authSession.extras!.email as any } });
-
-        if (existing) {
-            let token = await Repos.Tokens.createToken(existing.id!);
-            response.json({ ok: true, accessToken: token });
-            await authSession.destroy();
-            return;
-        } else {
-            let user = await DB.User.create({
-                authId: 'email|' + authSession.extras!.email,
-                email: '',
-            });
-            let token = await Repos.Tokens.createToken(user.id!);
-            response.json({ ok: true, accessToken: token });
-            await authSession.destroy();
+        if (!session && !authToken) {
+            sendError(response, 0);
             return;
         }
-    } else if (authSession.extras!.phone) {
-        let existing = await DB.Phone.findOne({ where: { phone: authSession.extras!.phone as any } });
 
-        if (existing) {
-            let token = await Repos.Tokens.createToken(existing.userId!);
-            response.json({ ok: true, accessToken: token });
-            await authSession.destroy();
-            return;
-        } else {
-            let user = await DB.User.create({
-                authId: 'phone|' + authSession.extras!.phone,
-                email: '',
-            });
-            await DB.Phone.create({
-                phone: authSession.extras!.phone as any,
-                status: 'VERIFIED',
-                userId: user.id
-            });
-            let token = await Repos.Tokens.createToken(user.id!);
-            response.json({ ok: true, accessToken: token });
-            await authSession.destroy();
+        let authSession = await DB.AuthSession.findOne({ where: { sessionSalt: session, extras: { authToken } }, transaction: tx });
+
+        // No session found
+        if (!authSession) {
+            sendError(response, 2);
             return;
         }
-    } else {
-        sendError(response, 1);
-    }
+
+        if (authSession.extras!.email) {
+            let existing = await DB.User.findOne({ where: { email: authSession.extras!.email as any }, transaction: tx });
+
+            if (existing) {
+                let token = await Repos.Tokens.createToken(existing.id!);
+                response.json({ ok: true, accessToken: token });
+                await authSession.destroy();
+                return;
+            } else {
+                let user = await DB.User.create({
+                    authId: 'email|' + authSession.extras!.email,
+                    email: '',
+                }, { transaction: tx });
+                let token = await Repos.Tokens.createToken(user.id!);
+                response.json({ ok: true, accessToken: token });
+                await authSession.destroy();
+                return;
+            }
+        } else if (authSession.extras!.phone) {
+            let existing = await DB.Phone.findOne({ where: { phone: authSession.extras!.phone as any }, transaction: tx });
+
+            if (existing) {
+                let token = await Repos.Tokens.createToken(existing.userId!);
+                response.json({ ok: true, accessToken: token });
+                await authSession.destroy();
+                return;
+            } else {
+                let user = await DB.User.create({
+                    authId: 'phone|' + authSession.extras!.phone,
+                    email: '',
+                }, {transaction: tx});
+                await DB.Phone.create({
+                    phone: authSession.extras!.phone as any,
+                    status: 'VERIFIED',
+                    userId: user.id
+                }, {transaction: tx});
+                let token = await Repos.Tokens.createToken(user.id!);
+                response.json({ ok: true, accessToken: token });
+                await authSession.destroy();
+                return;
+            }
+        } else {
+            sendError(response, 1);
+        }
+    });
 }
