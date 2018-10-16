9ac9dfe8159f05d8f0b0215034b2a27059679633
Steve Kite <steve@statecraft.one>
2018-10-16 17:05:46 +0300

    Add honeycomb integration

--- server/src/main/kotlin/com/openland/server/tables/Organization.kt
+++ server/src/main/kotlin/com/openland/server/tables/Organization.kt
@@ -4,5 +4,7 @@ import org.jetbrains.exposed.sql.Table
 
 object Organizations : Table("organizations") {
     val id = integer("id").autoIncrement().primaryKey()
-    val name = varchar("name",256)
+    val name = varchar("name", 255)
+    val website = varchar("website", 255).nullable()
+    val photo = varchar("website", 255).nullable()
 }
