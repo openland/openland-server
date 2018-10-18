864e4ab03853632f68e3e3e89198a85190d6ac2d
altox <xeroxaltox@gmail.com>
2018-10-18 20:04:24 +0300

    add log

--- packages/openland-server/repositories/SuperRepository.ts
+++ packages/openland-server/repositories/SuperRepository.ts
@@ -49,6 +49,8 @@ export class SuperRepository {
                     }],
                     transaction: tx
                 });
+                console.log('activateOrganization', members.map(m => m.id));
+
                 for (let m of members) {
                     m.user.status = 'ACTIVATED';
                     m.user.save({ transaction: tx });
