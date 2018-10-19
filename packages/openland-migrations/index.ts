e2a4933e5d9e01169657ffb046040f66a716fff1
altox <xeroxaltox@gmail.com>
2018-10-19 19:25:08 +0300

    online export

--- packages/openland-migrations/index.ts
+++ packages/openland-migrations/index.ts
@@ -1,7 +1,10 @@
 import { tokenExport } from './exportTokens';
+import { onlineExport } from './exportOnlines';
 
 const tokenExporter = tokenExport();
+const onlineExporter = onlineExport();
 
 export function performMigrations() {
     tokenExporter.start();
+    onlineExporter.start();
 }
