e134cd7d3abad506ab59ea94862ed91794dadb6e
Steve Kite <steve@statecraft.one>
2018-10-20 05:24:40 +0300

    Display silencing of deployment errors

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 ./kubectl set image deployment/statecraft-server statecraft-server=index.docker.io/openland/server:${1}
-./kubectl rollout status deployments statecraft-server || true
+./kubectl rollout status deployments statecraft-server
 # apollo schema:publish --key="service:openland-production:qrYALC8Flb8hv-SGm4grug" --endpoint="https://api.openland.com/api"
--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 ./kubectl set image deployment/statecraft-server-staging statecraft-server-staging=index.docker.io/openland/server:${1}
-./kubectl rollout status deployments statecraft-server-staging || true
+./kubectl rollout status deployments statecraft-server-staging
