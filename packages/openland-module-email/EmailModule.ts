00f1fcaa15a631ebfbb6a80590cd7b7c79b005ea
Steve Kite <steve@statecraft.one>
2018-10-22 16:03:52 +0300

    Workers reorganization

--- packages/openland-module-email/EmailModule.ts
+++ packages/openland-module-email/EmailModule.ts
@@ -1,12 +1,8 @@
 import { createEmailWorker } from './workers/EmailWorker';
-import { serverRoleEnabled } from 'openland-utils/serverRoleEnabled';
-import { startEmailNotificationWorker } from './workers/EmailNotificationWorker';
 
 export class EmailModule {
     readonly Worker = createEmailWorker();
     start = () => {
-        if (serverRoleEnabled('email_notifications')) {
-            startEmailNotificationWorker();
-        }
+        // Nothing to do
     }
 }
