6726e32e1050a05ede3a43ed64f20eba9ae93187
Steve Kite <steve@statecraft.one>
2018-10-26 12:46:12 +0300

    More push logging

--- packages/openland-server/api/Push.ts
+++ packages/openland-server/api/Push.ts
@@ -1,6 +1,10 @@
 import { CallContext } from './utils/CallContext';
 import { AppConfiuguration } from '../init/initConfig';
 import { Modules } from 'openland-modules/Modules';
+import { withLogContext } from 'openland-log/withLogContext';
+import { createLogger } from 'openland-log/createLogger';
+
+const pushLog = createLogger('push');
 
 export const Resolvers = {
     Query: {
@@ -20,21 +24,24 @@ export const Resolvers = {
             if (!context.uid || !context.tid) {
                 throw Error('Unable to register push for non-registered user');
             }
-            if (args.type === 'IOS') {
-                let parsed = JSON.parse(args.endpoint);
-                await Modules.Push.registerPushApple(context.uid!, context.tid!, parsed.endpoint, parsed.bundleId, parsed.sandbox);
-                return 'ok';
-            }
-            if (args.type === 'ANDROID') {
-                let parsed = JSON.parse(args.endpoint);
-                await Modules.Push.registerPushAndroid(context.uid!, context.tid!, parsed.endpoint, parsed.bundleId, parsed.sandbox);
-                return 'ok';
-            }
-            if (args.type === 'WEB_PUSH') {
-                await Modules.Push.registerPushWeb(context.uid!, context.tid!, args.endpoint);
-                return 'ok';
-            }
-            throw Error('Unknown type: ' + args.type);
+            return await withLogContext('push', async () => {
+                pushLog.log('Received push token: ' + JSON.stringify(args.endpoint));
+                if (args.type === 'IOS') {
+                    let parsed = JSON.parse(args.endpoint);
+                    await Modules.Push.registerPushApple(context.uid!, context.tid!, parsed.endpoint, parsed.bundleId, parsed.sandbox);
+                    return 'ok';
+                }
+                if (args.type === 'ANDROID') {
+                    let parsed = JSON.parse(args.endpoint);
+                    await Modules.Push.registerPushAndroid(context.uid!, context.tid!, parsed.endpoint, parsed.bundleId, parsed.sandbox);
+                    return 'ok';
+                }
+                if (args.type === 'WEB_PUSH') {
+                    await Modules.Push.registerPushWeb(context.uid!, context.tid!, args.endpoint);
+                    return 'ok';
+                }
+                throw Error('Unknown type: ' + args.type);
+            });
         }
     }
 };
