527888d653bf92f3c62f1fff605c5121462069eb
kor_ka <gleb.putintsev@openland.com>
2018-10-26 17:08:54 +0300

    try fix conversation events - disable DeliveryWorker

--- packages/openland-module-messaging/workers/DeliveryWorker.ts
+++ packages/openland-module-messaging/workers/DeliveryWorker.ts
@@ -1,12 +1,12 @@
-import { UpdateReader } from 'openland-server/modules/updateReader';
-import { DB } from 'openland-server/tables';
+// import { UpdateReader } from 'openland-server/modules/updateReader';
+// import { DB } from 'openland-server/tables';
 
 export function createDeliveryWorker() {
-    let reader = new UpdateReader('message-delivery-2', 1, DB.ConversationEvent);
-    reader.processor(async (items) => {
-        for (let i of items) {
-            console.log(i.eventType);
-        }
-    });
-    reader.start();
+    // let reader = new UpdateReader('message-delivery-2', 1, DB.ConversationEvent);
+    // reader.processor(async (items) => {
+    //     for (let i of items) {
+    //         console.log(i.eventType);
+    //     }
+    // });
+    // reader.start();
 }
