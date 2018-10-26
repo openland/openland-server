fbbf93ca1480a98a20d07dabbcc0abcd4ed29ff5
Steve Kite <steve@statecraft.one>
2018-10-26 06:15:53 +0300

    Remove bad entity id fixer

--- packages/foundation-orm/FDoctor.ts
+++ packages/foundation-orm/FDoctor.ts
@@ -5,7 +5,7 @@ import { createLogger } from 'openland-log/createLogger';
 import { FConnection } from './FConnection';
 import { FKeyEncoding } from './utils/FKeyEncoding';
 
-const log = createLogger('doctor');
+// const log = createLogger('doctor');
 
 export const FDoctor = {
 
@@ -30,18 +30,18 @@ export const FDoctor = {
     },
 
     async doctorEntityIds(entity: FEntityFactory<any>) {
-        await inTx(async () => {
-            log.log('Trying to fix entity ids');
-            let all = await entity.findAllWithIds();
-            log.log('Found ' + all.length + ' entities');
-            for (let i of all) {
-                let id = entity.extractId(i.key);
-                (i.item as any).rawId = i.key;
-                (i.item as any)._value = { ...(i.item as any)._value, ...id, };
-                (i.item as any)._valueInitial = { ...(i.item as any)._valueInitial, ...id };
-                (i.item as any).markDirty();
-                log.log('Updated entity ' + (i.item as any)._value);
-            }
-        });
+        // await inTx(async () => {
+        //     log.log('Trying to fix entity ids');
+        //     let all = await entity.findAllWithIds();
+        //     log.log('Found ' + all.length + ' entities');
+        //     for (let i of all) {
+        //         let id = entity.extractId(i.key);
+        //         (i.item as any).rawId = i.key;
+        //         (i.item as any)._value = { ...(i.item as any)._value, ...id, };
+        //         (i.item as any)._valueInitial = { ...(i.item as any)._valueInitial, ...id };
+        //         (i.item as any).markDirty();
+        //         log.log('Updated entity ' + (i.item as any)._value);
+        //     }
+        // });
     }
 };
