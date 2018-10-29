d7ffa79aced3e5ec40a42092f97e7a60094c1257
Steve Kite <steve@statecraft.one>
2018-10-29 06:42:49 +0300

    Do not try to search for empty strings

--- packages/openland-module-users/search/UserSearch.ts
+++ packages/openland-module-users/search/UserSearch.ts
@@ -2,23 +2,28 @@ import { Modules } from 'openland-modules/Modules';
 
 export class UserSearch {
     async searchForUsers(query: string, options?: { uid?: number, limit?: number }) {
-        let hits = await Modules.Search.elastic.client.search({
-            index: 'user_profile',
-            type: 'user_profile',
-            size: options && options.limit ? options.limit : 20,
-            body: {
-                query: {
-                    bool: {
-                        should: [
-                            { match_phrase_prefix: { firstName: { query: query } } },
-                            { match_phrase_prefix: { lastName: { query: query } } },
-                            { match: { email: { query: query } } },
-                        ]
+        let normalized = query.trim();
+        if (normalized.length > 0) {
+            let hits = await Modules.Search.elastic.client.search({
+                index: 'user_profile',
+                type: 'user_profile',
+                size: options && options.limit ? options.limit : 20,
+                body: {
+                    query: {
+                        bool: {
+                            should: [
+                                { match_phrase_prefix: { firstName: { query: query } } },
+                                { match_phrase_prefix: { lastName: { query: query } } },
+                                { match: { email: { query: query } } },
+                            ]
+                        }
                     }
                 }
-            }
-        });
-        let uids = hits.hits.hits.map((v) => parseInt(v._id, 10));
-        return uids;
+            });
+            let uids = hits.hits.hits.map((v) => parseInt(v._id, 10));
+            return uids;
+        } else {
+            return [];
+        }
     }
 }
