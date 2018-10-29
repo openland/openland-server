8c53b1ffc5284b96b8c01d90016e36b08208a7b3
kor_ka <gleb.putintsev@openland.com>
2018-10-29 12:51:37 +0300

    searchForUsers: search empty query, match_phrase_prefix

--- packages/openland-module-users/search/UserSearch.ts
+++ packages/openland-module-users/search/UserSearch.ts
@@ -5,65 +5,59 @@ import { Repos } from 'openland-server/repositories';
 export class UserSearch {
     async searchForUsers(query: string, options?: { uid?: number, limit?: number }) {
         let normalized = query.trim();
-        if (normalized.length > 0) {
 
-            let mainQuery: any = {
-                bool: {
-                    should: [
-                        { match_phrase_prefix: { firstName: { query: query } } },
-                        { match_phrase_prefix: { lastName: { query: query } } },
-                        { match: { email: { query: query } } },
-                    ],
-                    must_not: options && options.uid ? [
-                        { match: { _id: options.uid } }
-                    ] : [],
-                }
-            };
+        let mainQuery: any = {
+            bool: {
+                should: normalized.length > 0 ? [
+                    { match_phrase_prefix: { search: query } }
+                ] : [],
+                must_not: options && options.uid ? [
+                    { match: { _id: options.uid } }
+                ] : [],
+            }
+        };
 
-            if (options && options.uid) {
-                let profile = await FDB.UserProfile.findById(options.uid);
-                let organizations = await Repos.Users.fetchUserAccounts(options.uid);
-                let functions: any[] = [];
+        if (options && options.uid) {
+            let profile = await FDB.UserProfile.findById(options.uid);
+            let organizations = await Repos.Users.fetchUserAccounts(options.uid);
+            let functions: any[] = [];
 
-                // Huge boost if primary organization same
-                if (profile && profile.primaryOrganization) {
+            // Huge boost if primary organization same
+            if (profile && profile.primaryOrganization) {
+                functions.push({
+                    filter: { match: { primaryOrganization: profile.primaryOrganization } },
+                    weight: 8
+                });
+            }
+
+            // Boost if have common organizations (more common organizations - more boost)
+            if (organizations.length > 0) {
+                for (let o of organizations) {
                     functions.push({
-                        filter: { match: { primaryOrganization: profile.primaryOrganization } },
-                        weight: 8
+                        filter: { match: { organizations: o } },
+                        weight: 2
                     });
-                }
-
-                // Boost if have common organizations (more common organizations - more boost)
-                if (organizations.length > 0) {
-                    for (let o of organizations) {
-                        functions.push({
-                            filter: { match: { organizations: o } },
-                            weight: 2
-                        });
-                    }
-                }
-                
-                if (functions.length > 0) {
-                    mainQuery = {
-                        function_score: {
-                            query: mainQuery,
-                            functions: functions,
-                            boost_mode: 'multiply'
-                        }
-                    };
                 }
             }
 
-            let hits = await Modules.Search.elastic.client.search({
-                index: 'user_profile',
-                type: 'user_profile',
-                size: options && options.limit ? options.limit : 20,
-                body: { query: mainQuery }
-            });
-            let uids = hits.hits.hits.map((v) => parseInt(v._id, 10));
-            return uids;
-        } else {
-            return [];
+            if (functions.length > 0) {
+                mainQuery = {
+                    function_score: {
+                        query: mainQuery,
+                        functions: functions,
+                        boost_mode: 'multiply'
+                    }
+                };
+            }
         }
+
+        let hits = await Modules.Search.elastic.client.search({
+            index: 'user_profile',
+            type: 'user_profile',
+            size: options && options.limit ? options.limit : 20,
+            body: { query: mainQuery }
+        });
+        let uids = hits.hits.hits.map((v) => parseInt(v._id, 10));
+        return uids;
     }
 }
