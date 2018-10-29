d294fedb04d39910f68bd3bc77d4b68d53aa00da
Steve Kite <steve@statecraft.one>
2018-10-29 09:17:01 +0300

    Indexing of organization membership

--- packages/openland-module-users/workers/userProfileIndexer.ts
+++ packages/openland-module-users/workers/userProfileIndexer.ts
@@ -1,11 +1,15 @@
 import { FDB } from 'openland-module-db/FDB';
 import { declareSearchIndexer } from 'openland-module-search/declareSearchIndexer';
 import { Modules } from 'openland-modules/Modules';
+import { Repos } from 'openland-server/repositories';
 
 export function userProfileIndexer() {
-    declareSearchIndexer('user-profile-index', 5, 'user_profile', FDB.UserProfile.createByUpdatedAtStream(50))
+    declareSearchIndexer('user-profile-index', 6, 'user_profile', FDB.UserProfile.createByUpdatedAtStream(50))
         .withProperties({
             primaryOrganization: {
+                type: 'keyword'
+            },
+            organizations: {
                 type: 'keyword'
             },
             firstName: {
@@ -35,6 +39,7 @@ export function userProfileIndexer() {
         })
         .start(async (item) => {
             let shortName = await Modules.Shortnames.findUserShortname(item.id);
+            let orgs = await Repos.Users.fetchUserAccounts(item.id);
 
             let searchData: (string | undefined | null)[] = [];
             searchData.push(item.firstName);
@@ -45,13 +50,14 @@ export function userProfileIndexer() {
             return {
                 id: item.id!!,
                 doc: {
-                    primaryOrganization: item.primaryOrganization,
                     firstName: item.firstName,
                     lastName: item.lastName,
                     name: (item.firstName || '') + (item.lastName || ''),
                     shortName: shortName ? shortName.shortname : undefined,
                     userId: item.id,
                     search: searchData.join(' '),
+                    primaryOrganization: item.primaryOrganization,
+                    organizations: orgs,
                     createdAt: (item as any).createdAt,
                     updatedAt: (item as any).updatedAt,
                 }
