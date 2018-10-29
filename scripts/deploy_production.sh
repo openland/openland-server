62a4397d2e15f5b38d9bd46d41b14d8c07a79b84
Steve Kite <steve@statecraft.one>
2018-10-29 15:26:30 +0300

    Multi-role deployments

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 ./kubectl set image deployment/statecraft-server statecraft-server=index.docker.io/openland/server:${1}
+./kubectl set image deployment/openland-server-workers openland-server-workers=index.docker.io/openland/server:${1}
 ./kubectl rollout status deployments statecraft-server
-# apollo schema:publish --key="service:openland-production:qrYALC8Flb8hv-SGm4grug" --endpoint="https://api.openland.com/api"
+./kubectl rollout status deployments openland-server-workers
