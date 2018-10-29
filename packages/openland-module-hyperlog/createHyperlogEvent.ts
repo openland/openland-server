8cf8a373ae459cd198b24d294d43745f0fcbe8fd
Steve Kite <steve@statecraft.one>
2018-10-29 06:27:36 +0300

    Wrap logger

--- packages/openland-module-hyperlog/createHyperlogEvent.ts
+++ packages/openland-module-hyperlog/createHyperlogEvent.ts
@@ -1,13 +1,20 @@
 import { FDB } from 'openland-module-db/FDB';
+import { inTx } from 'foundation-orm/inTx';
 
 export function createHyperlogger<T>(type: string) {
     return {
         event: async (event: T) => {
-            await FDB.HyperLog.create(await FDB.HyperLog.connection.nextRandomId(), {
-                type: type,
-                date: Date.now(),
-                body: event
-            });
+            try {
+                await inTx(async () => {
+                    await FDB.HyperLog.create(await FDB.HyperLog.connection.nextRandomId(), {
+                        type: type,
+                        date: Date.now(),
+                        body: event
+                    });
+                });
+            } catch (e) {
+                console.warn(e);
+            }
         }
     };
 }
