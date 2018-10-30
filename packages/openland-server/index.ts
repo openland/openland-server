74550fc4da250757ea145c4af7a1b0d36c16ee79
Steve Kite <steve@statecraft.one>
2018-10-30 13:05:56 +0300

    Disable blocked-at

--- packages/openland-server/index.ts
+++ packages/openland-server/index.ts
@@ -1,8 +1,8 @@
 require('module-alias/register');
 
-require('blocked-at')((time: number, stack: string) => {
-    console.log(`Blocked for ${time} ms, operation started here:`, stack);
-});
+// require('blocked-at')((time: number, stack: string) => {
+//     console.log(`Blocked for ${time} ms, operation started here:`, stack);
+// });
 
 // AsyncIterator polyfil
 import { serverRoleEnabled } from '../openland-utils/serverRoleEnabled';
