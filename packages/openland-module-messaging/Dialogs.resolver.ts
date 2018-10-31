334be5196c83e3aabdc932171557f9ab251330b1
Steve Kite <steve@statecraft.one>
2018-10-31 20:06:23 +0300

    Move dialogs search, loading and subscription resolvers to separate file

--- packages/openland-module-messaging/Dialogs.resolver.ts
+++ packages/openland-module-messaging/Dialogs.resolver.ts
@@ -1,6 +1,24 @@
 import { CallContext } from 'openland-server/api/utils/CallContext';
+import { DB } from 'openland-server/tables';
+import { Repos } from 'openland-server/repositories';
+import { withUser } from 'openland-server/api/utils/Resolvers';
+import { FDB } from 'openland-module-db/FDB';
 
 export default {
+    Query: {
+        alphaChats: withUser<{ first: number, after?: string | null, seq?: number }>(async (args, uid) => {
+            let global = await FDB.UserMessagingState.findById(uid);
+            let seq = global ? global.seq : 0;
+            let conversations = await FDB.UserDialog
+                .rangeFromUserWithCursor(uid, args.first, args.after ? args.after : undefined, true);
+            return {
+                conversations: conversations.items.map((v) => DB.Conversation.findById(v.cid)),
+                seq: seq,
+                next: conversations.cursor,
+                counter: uid
+            };
+        }),
+    },
     Subscription: {
         dialogUpdates: {
             resolve: async (msg: any) => {
@@ -9,6 +27,46 @@ export default {
             subscribe: async function (_: any, args: { fromSeq?: number }, context: CallContext) {
                 //
             }
-        }
+        },
+        alphaSubscribeEvents: {
+            resolve: async (msg: any) => {
+                return msg;
+            },
+            subscribe: async function (_: any, args: { fromSeq?: number }, context: CallContext) {
+                let ended = false;
+                return {
+                    ...(async function* func() {
+                        let lastKnownSeq = args.fromSeq;
+                        while (!ended) {
+                            if (lastKnownSeq !== undefined) {
+                                let events = await DB.ConversationUserEvents.findAll({
+                                    where: {
+                                        userId: context.uid,
+                                        seq: {
+                                            $gt: lastKnownSeq
+                                        }
+                                    },
+                                    order: [['seq', 'asc']]
+                                });
+                                for (let r of events) {
+                                    yield r;
+                                }
+                                if (events.length > 0) {
+                                    lastKnownSeq = events[events.length - 1].seq;
+                                }
+                            }
+                            let res = await Repos.Chats.userReader.loadNext(context.uid!!, lastKnownSeq ? lastKnownSeq : null);
+                            if (!lastKnownSeq) {
+                                lastKnownSeq = res - 1;
+                            }
+                        }
+                    })(),
+                    return: async () => {
+                        ended = true;
+                        return 'ok';
+                    }
+                };
+            }
+        },
     }
 };
---
+++ packages/openland-module-messaging/DialogsSearch.resolver.ts
@@ -0,0 +1,165 @@
+import { FDB } from 'openland-module-db/FDB';
+import { withAccount } from 'openland-server/api/utils/Resolvers';
+import { DB } from 'openland-server/tables';
+import { Modules } from 'openland-modules/Modules';
+import { ConversationMessage } from 'openland-server/tables/ConversationMessage';
+
+export default {
+    Query: {
+        alphaChatTextSearch: withAccount<{ query: string }>(async (args, uid, oid) => {
+
+            // GROUPS / CHANNELS has titles we can search 
+            let searchableConversations = (await FDB.UserDialog.allFromUser(uid)).map((v) => v.cid);
+            let sequelize = DB.connection;
+            let groupsChannels = await DB.Conversation.findAll({
+                where: {
+                    type: {
+                        $in: ['group', 'channel']
+                    },
+                    title: {
+                        $ilike: '%' + args.query.toLowerCase() + '%'
+                    },
+                    id: {
+                        $in: searchableConversations
+                    }
+                }
+            });
+
+            // PERSONAL - search users first, then matching conversations with current user
+            let userIds = await Modules.Users.searchForUsers(args.query, { uid, limit: 50 });
+
+            let personal = await DB.Conversation.findAll({
+                where: [
+                    sequelize.and(
+                        {
+                            type: 'private'
+                        },
+                        sequelize.or(
+                            {
+                                member1Id: uid,
+                                member2Id: {
+                                    $in: userIds
+                                }
+                            },
+                            {
+                                member2Id: uid,
+                                member1Id: {
+                                    $in: userIds
+                                }
+                            }
+                        )
+                    )
+                ]
+            });
+
+            // SHARED search org1 matching name, org2 current and vice versa
+            let orgs1 = await DB.Conversation.findAll({
+                include: [
+                    {
+                        model: DB.Organization,
+                        as: 'organization1',
+                        required: true,
+                        where: {
+                            name: {
+                                $ilike: '%' + args.query.toLowerCase() + '%'
+                            }
+                        }
+                    },
+                    {
+                        model: DB.Organization,
+                        as: 'organization2',
+                        required: true,
+                        where: {
+                            id: oid
+                        }
+                    }
+                ]
+            });
+            let orgs2 = await DB.Conversation.findAll({
+                include: [
+                    {
+                        model: DB.Organization,
+                        as: 'organization2',
+                        required: true,
+                        where: {
+                            name: {
+                                $ilike: '%' + args.query.toLowerCase() + '%'
+                            }
+                        }
+                    },
+                    {
+                        model: DB.Organization,
+                        as: 'organization1',
+                        required: true,
+                        where: {
+                            id: oid
+                        }
+                    }
+                ]
+            });
+            // ORG INNER CHATS
+            let userAsMember = await DB.OrganizationMember.findAll({
+                where: {
+                    userId: uid
+                }
+            });
+            let orgsIds = userAsMember.map(m => m.orgId);
+            let orgsInner = await DB.Conversation.findAll({
+                include: [
+                    {
+                        model: DB.Organization,
+                        as: 'organization1',
+                        required: true,
+                        where: {
+                            id: {
+                                $in: orgsIds,
+                            },
+                            name: {
+                                $ilike: '%' + args.query.toLowerCase() + '%'
+                            }
+                        }
+                    },
+                    {
+                        model: DB.Organization,
+                        as: 'organization2',
+                        required: true,
+                        where: {
+                            id: {
+                                $in: orgsIds,
+                            },
+                            name: {
+                                $ilike: '%' + args.query.toLowerCase() + '%'
+                            }
+                        }
+                    },
+                ]
+            });
+
+            let res = [...personal, ...groupsChannels, ...orgs1, ...orgs2, ...orgsInner];
+            res = res.reduce(
+                (p, x) => {
+                    if (!p.find(c => c.id === x.id)) {
+                        p.push(x);
+                    }
+                    return p;
+                },
+                [] as any[]
+            );
+            let messages = new Map<number, ConversationMessage | null>();
+            for (let c of res) {
+                messages.set(c.id, await DB.ConversationMessage.find({
+                    where: { conversationId: c.id },
+                    order: [['id', 'DESC']]
+                }));
+            }
+            res = res.filter(c => messages.get(c.id))
+                .sort((a, b) => {
+                    let lastMessageA = messages.get(a.id);
+                    let lastMessageB = messages.get(b.id);
+                    return (lastMessageB ? new Date((lastMessageB as any).createdAt).getTime() : 0) - (lastMessageA ? new Date((lastMessageA as any).createdAt).getTime() : 0);
+                });
+            return res;
+
+        }),
+    }
+};
