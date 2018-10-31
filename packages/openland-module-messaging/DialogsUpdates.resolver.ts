972eda332717d8ee76026cfef23381e8cff90e92
Steve Kite <steve@statecraft.one>
2018-10-31 21:29:36 +0300

    Fix resolver name

--- packages/openland-module-messaging/DialogsUpdates.resolver.ts
+++ packages/openland-module-messaging/DialogsUpdates.resolver.ts
@@ -1,5 +1,7 @@
 import { CallContext } from 'openland-server/api/utils/CallContext';
 import { ConversationUserEvents } from 'openland-server/tables/ConversationUserEvents';
+import { DB } from 'openland-server/tables';
+import { Repos } from 'openland-server/repositories';
 
 export default {
     /* 
@@ -72,12 +74,50 @@ export default {
      * Subscription
      */
     Subscription: {
-        dialogUpdates: {
+        dialogsUpdates: {
             resolve: async (msg: any) => {
                 return msg;
             },
             subscribe: async function (_: any, args: { fromState?: string }, context: CallContext) {
-                //
+                let ended = false;
+                let startSeq = args.fromState ? parseInt(args.fromState.substr(2), 10) : undefined;
+                return {
+                    ...(async function* func() {
+                        let lastKnownSeq = startSeq;
+                        while (!ended) {
+                            if (lastKnownSeq !== undefined) {
+                                let events = await DB.ConversationUserEvents.findAll({
+                                    where: {
+                                        userId: context.uid,
+                                        seq: {
+                                            $gt: lastKnownSeq
+                                        }
+                                    },
+                                    order: [['seq', 'asc']]
+                                });
+                                // events.filter((v)=>{
+                                //     v.eventType !== ''
+                                // })
+                                if (events.length > 1) {
+                                    yield events;
+                                } else if (events.length === 1) {
+                                    yield events[0];
+                                }
+                                if (events.length > 0) {
+                                    lastKnownSeq = events[events.length - 1].seq;
+                                }
+                            }
+                            let res = await Repos.Chats.userReader.loadNext(context.uid!!, lastKnownSeq ? lastKnownSeq : null);
+                            if (!lastKnownSeq) {
+                                lastKnownSeq = res - 1;
+                            }
+                        }
+                    })(),
+                    return: async () => {
+                        ended = true;
+                        return 'ok';
+                    }
+                };
             }
         },
     }
