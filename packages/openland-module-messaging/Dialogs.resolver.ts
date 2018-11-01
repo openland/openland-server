f4e296d3462d54e78d3d7b1202287df634b421b8
Steve Kite <steve@statecraft.one>
2018-11-01 03:08:22 +0300

    Faster dialog list loading

--- packages/openland-module-messaging/Dialogs.resolver.ts
+++ packages/openland-module-messaging/Dialogs.resolver.ts
@@ -11,8 +11,19 @@ export default {
             let seq = global ? global.seq : 0;
             let conversations = await FDB.UserDialog
                 .rangeFromUserWithCursor(uid, args.first, args.after ? args.after : undefined, true);
+            let convs = await DB.Conversation.findAll({
+                where: {
+                    id: {
+                        $in: conversations.items.map((v) => v.cid)
+                    }
+                }
+            });
+            let res: any[] = [];
+            for (let c of conversations.items) {
+                res.push(convs.find((v) => v.id === c.cid));
+            }
             return {
-                conversations: conversations.items.map((v) => DB.Conversation.findById(v.cid)),
+                conversations: res,
                 seq: seq,
                 next: conversations.cursor,
                 counter: uid
