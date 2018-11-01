f490566dd7c18321b54b62c67341e016a00b4c45
Steve Kite <steve@statecraft.one>
2018-11-01 06:52:09 +0300

    Disable redis for development

--- scripts/set_dev_config.sh
+++ scripts/set_dev_config.sh
@@ -2,7 +2,7 @@ set -e
 export NODE_ENV="development" 
 export DATABASE_USER=$(id -un)
 export ELASTIC_ENDPOINT="http://localhost:9200/"
-export REDIS_HOST="localhost" 
+# export REDIS_HOST="localhost" 
 export GOOGLE_APPLICATION_CREDENTIALS="`pwd`/scripts/google-cloud-dev.json"
 export FILES_BUCKET="files.datamakesperfect.com"
 export WEB_PUSH_PUBLIC="BOvxoJt3pHvDl9XDfwtcYPEtKxu7-K1Ztfxh7AyYyNSZa65rsBlt--8d72Y5X7_5kwQWlbaY30J82Olt_g-R7oE"
