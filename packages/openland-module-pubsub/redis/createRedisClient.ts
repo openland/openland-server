882490d4a9c282a5b495d1f9e1e423b330fddf06
Steve Kite <steve@statecraft.one>
2018-11-01 06:46:12 +0300

    Trying to fix redis

--- packages/openland-module-pubsub/redis/createRedisClient.ts
+++ packages/openland-module-pubsub/redis/createRedisClient.ts
@@ -1,6 +1,6 @@
 import * as Redis from 'handy-redis';
 
-export const isRedisConfigured = process.env.REDIS_HOST !== undefined;
+export const isRedisConfigured = process.env.REDIS_HOST !== undefined && process.env.DISABLE_CACHE !== 'true';
 
 export function createRedisClient() {
     let port = process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT as string, 10) : 6379;
