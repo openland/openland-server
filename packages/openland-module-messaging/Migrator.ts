5409c09b9821894f431402ff0f659d9d31d7b66f
Steve Kite <steve@statecraft.one>
2018-11-02 17:54:14 +0300

    Removed ConversationGroupMembers

--- packages/openland-module-messaging/Migrator.ts
+++ packages/openland-module-messaging/Migrator.ts
@@ -1,37 +1,3 @@
-import { UpdateReader } from 'openland-server/modules/updateReader';
-import { DB } from 'openland-server/tables';
-import { inTx } from 'foundation-orm/inTx';
-import { FDB } from 'openland-module-db/FDB';
-
 export function startMigrator() {
-    let reader = new UpdateReader('export-members', 3, DB.ConversationGroupMembers);
-    reader.processor(async (items) => {
-        for (let i of items) {
-
-            // FDB.Conversation.findById(i.id);
-            await inTx(async () => {
-                let p = await FDB.RoomParticipant.findById(i.conversationId, i.userId);
-                if (!p) {
-                    await FDB.RoomParticipant.create(i.conversationId, i.userId, {
-                        role:
-                            (i.role === 'admin')
-                                ? 'admin'
-                                : (i.role === 'creator')
-                                    ? 'owner'
-                                    : 'member',
-                        invitedBy: i.invitedById,
-                        status:
-                            (i.status === 'requested')
-                                ? 'requested'
-                                : (i.status === 'member')
-                                    ? 'joined'
-                                    : 'kicked'
-                    });
-                } else {
-                    p.markDirty();
-                }
-            });
-        }
-    });
-    reader.start();
+    // No migrations
 }
