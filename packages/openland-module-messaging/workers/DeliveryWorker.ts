d2ac92313588efda2f438152b987cebf3b5e9d09
Steve Kite <steve@statecraft.one>
2018-11-03 20:50:26 +0300

    Add tracing to delivery worker

--- packages/openland-module-messaging/workers/DeliveryWorker.ts
+++ packages/openland-module-messaging/workers/DeliveryWorker.ts
@@ -5,57 +5,67 @@ import { Modules } from 'openland-modules/Modules';
 import { Repos } from 'openland-server/repositories';
 import { inTx } from 'foundation-orm/inTx';
 import { FDB } from 'openland-module-db/FDB';
+import { createTracer } from 'openland-log/createTracer';
+import { withTracing } from 'openland-log/withTracing';
+import { trace } from 'openland-log/trace';
+
+const tracer = createTracer('message-delivery');
 
 export function createDeliveryWorker() {
     let queue = new WorkQueue<{ messageId: number }, { result: string }>('conversation_message_delivery');
     if (serverRoleEnabled('delivery')) {
         queue.addWorker(async (item) => {
-            await inTx(async () => {
-                let message = (await FDB.Message.findById(item.messageId))!;
-                let conversationId = message.cid;
-                let conv = (await DB.Conversation.findById(conversationId))!;
-                let uid = message.uid;
-                let members = await Repos.Chats.getConversationMembersFast(conversationId, conv);
 
-                // Cancel Typings
-                await Modules.Typings.cancelTyping(uid, conversationId, members);
+            await withTracing(tracer, 'delivery', async () => {
+                await inTx(async () => {
+                    let message = (await FDB.Message.findById(item.messageId))!;
+                    let conversationId = message.cid;
+                    let conv = (await DB.Conversation.findById(conversationId))!;
+                    let uid = message.uid;
+                    let members = await Repos.Chats.getConversationMembersFast(conversationId, conv);
 
-                // Notify augmentation worker
-                await Modules.Messaging.AugmentationWorker.pushWork({ messageId: item.messageId });
+                    // Cancel Typings
+                    await Modules.Typings.cancelTyping(uid, conversationId, members);
 
-                // Deliver messages
-                if (members.length > 0) {
-                    for (let m of members) {
-                        let existing = await Modules.Messaging.repo.getUserDialogState(m, conversationId);
-                        let existingGlobal = await Modules.Messaging.repo.getUserMessagingState(m);
+                    // Notify augmentation worker
+                    await Modules.Messaging.AugmentationWorker.pushWork({ messageId: item.messageId });
 
-                        // Write user's chat state
-                        if (m !== uid) {
-                            existing.unread++;
+                    // Deliver messages
+                    if (members.length > 0) {
+                        for (let m of members) {
+                            await trace(tracer, 'member', async () => {
+                                let existing = await Modules.Messaging.repo.getUserDialogState(m, conversationId);
+                                let existingGlobal = await Modules.Messaging.repo.getUserMessagingState(m);
+
+                                // Write user's chat state
+                                if (m !== uid) {
+                                    existing.unread++;
+                                }
+
+                                // Update dialog date
+                                existing.date = message.createdAt;
+
+                                // Update global counters
+                                if (m !== uid) {
+                                    existingGlobal.unread++;
+                                }
+                                existingGlobal.seq++;
+
+                                // Write User Event
+                                await FDB.UserDialogEvent.create(m, existingGlobal.seq, {
+                                    kind: 'message_received',
+                                    cid: conversationId,
+                                    mid: message.id,
+                                    allUnread: existingGlobal.unread,
+                                    unread: existing.unread
+                                });
+
+                                // Send counter push notification to iOS
+                                await Modules.Push.sendCounterPush(m, conversationId, existingGlobal.unread);
+                            });
                         }
-
-                        // Update dialog date
-                        existing.date = message.createdAt;
-
-                        // Update global counters
-                        if (m !== uid) {
-                            existingGlobal.unread++;
-                        }
-                        existingGlobal.seq++;
-
-                        // Write User Event
-                        await FDB.UserDialogEvent.create(m, existingGlobal.seq, {
-                            kind: 'message_received',
-                            cid: conversationId,
-                            mid: message.id,
-                            allUnread: existingGlobal.unread,
-                            unread: existing.unread
-                        });
-
-                        // Send counter push notification to iOS
-                        await Modules.Push.sendCounterPush(m, conversationId, existingGlobal.unread);
                     }
-                }
+                });
             });
 
             return { result: 'ok' };
