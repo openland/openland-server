5abe1074ac42a40c0f67aabf5c299d61fe22ad06
Steve Kite <steve@statecraft.one>
2018-11-04 12:01:34 +0300

    Yet another counter fixer

--- packages/openland-module-db/migrations.ts
+++ packages/openland-module-db/migrations.ts
@@ -154,6 +154,16 @@ migrations.push({
     }
 });
 
+migrations.push({
+    key: '12-fix-counters',
+    migration: async () => {
+        let users = (await DB.User.findAll());
+        for (let u of users) {
+            await Modules.Messaging.fixer.fixForUser(u.id!);
+        }
+    }
+});
+
 export function startMigrationsWorker() {
     if (serverRoleEnabled('workers')) {
         staticWorker({ name: 'foundation-migrator' }, async () => {
