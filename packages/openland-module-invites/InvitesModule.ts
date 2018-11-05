c59ffdadeed7af03e0f5fead2e29c9cd5e1b497d
kor_ka <gleb.putintsev@openland.com>
2018-11-05 18:37:32 +0300

    invites migrator

--- packages/openland-module-invites/InvitesModule.ts
+++ packages/openland-module-invites/InvitesModule.ts
@@ -1,11 +1,13 @@
 import { FDB } from 'openland-module-db/FDB';
 import { InviteRepository } from './repositories/InviteRepository';
+import { startMigrator } from 'openland-module-invites/Migrator';
 
 export class InvitesModule {
     readonly repo = new InviteRepository(FDB);
 
     start = () => {
-        // Do nothing
+        startMigrator();
+
     }
 
     async getInviteLinkKey(uid: number) {
