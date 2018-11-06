4922ffa687b44fefe7f906da781957bfc661c7a8
altox <xeroxaltox@gmail.com>
2018-11-06 18:24:57 +0300

    API Schema test now crashes on warnings

--- packages/openland-server/api/Schema.spec.ts
+++ packages/openland-server/api/Schema.spec.ts
@@ -20,6 +20,9 @@ let resolvers = buildResolvers(__dirname + '/../../');
 
 describe('GQLSchema', () => {
     it('should be valid', () => {
+        const spy = jest.spyOn(global.console, 'warn');
+        const spy2 = jest.spyOn(global.console, 'error');
+
         makeExecutableSchema({
             typeDefs: schema,
             resolvers: merge(
@@ -39,5 +42,8 @@ describe('GQLSchema', () => {
             ),
             schemaDirectives: Directives
         });
+
+        expect(spy).not.toHaveBeenCalled();
+        expect(spy2).not.toHaveBeenCalled();
     });
 });
