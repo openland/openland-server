7de2a294b414a04b9ff9828d9d723d4fdf342670
kor_ka <gleb.putintsev@openland.com>
2018-11-06 14:02:35 +0300

    fix conversations migrator channel ownerId

--- packages/openland-module-messaging/Dialogs.resolver.ts
+++ packages/openland-module-messaging/Dialogs.resolver.ts
@@ -1,5 +1,6 @@
 import { withUser } from 'openland-server/api/utils/Resolvers';
 import { FDB } from 'openland-module-db/FDB';
+import { DB } from 'openland-server/tables';
 
 export default {
     Query: {
@@ -8,8 +9,19 @@ export default {
             let seq = global ? global.seq : 0;
             let conversations = await FDB.UserDialog
                 .rangeFromUserWithCursor(uid, args.first, args.after ? args.after : undefined, true);
-            let res = await Promise.all(conversations.items.map((v) => FDB.Conversation.findById(v.cid)));
+            // let res = await Promise.all(conversations.items.map((v) => FDB.Conversation.findById(v.cid)));
             // console.log(res);
+            let convs = await DB.Conversation.findAll({
+                where: {
+                    id: {
+                        $in: conversations.items.map((v) => v.cid)
+                    }
+                }
+            });
+            let res: any[] = [];
+            for (let c of conversations.items) {
+                res.push(convs.find((v) => v.id === c.cid));
+            }
             return {
                 conversations: res,
                 seq: seq,
