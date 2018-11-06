13db196e8b3bbcc825aed042f1e36eb6cef8c2e6
Steve Kite <steve@statecraft.one>
2018-11-06 17:03:07 +0300

    Revert resolver

--- packages/openland-module-messaging/Dialogs.resolver.ts
+++ packages/openland-module-messaging/Dialogs.resolver.ts
@@ -1,6 +1,5 @@
 import { withUser } from 'openland-server/api/utils/Resolvers';
 import { FDB } from 'openland-module-db/FDB';
-import { DB } from 'openland-server/tables';
 
 export default {
     Query: {
@@ -9,19 +8,7 @@ export default {
             let seq = global ? global.seq : 0;
             let conversations = await FDB.UserDialog
                 .rangeFromUserWithCursor(uid, args.first, args.after ? args.after : undefined, true);
-            // let res = await Promise.all(conversations.items.map((v) => FDB.Conversation.findById(v.cid)));
-            // console.log(res);
-            let convs = await DB.Conversation.findAll({
-                where: {
-                    id: {
-                        $in: conversations.items.map((v) => v.cid)
-                    }
-                }
-            });
-            let res: any[] = [];
-            for (let c of conversations.items) {
-                res.push(convs.find((v) => v.id === c.cid));
-            }
+            let res = await Promise.all(conversations.items.map((v) => FDB.Conversation.findById(v.cid)));
             return {
                 conversations: res,
                 seq: seq,
