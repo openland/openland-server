9c75afe2b1fb2d7e2c8ab1f2542467d8ddaee77d
altox <xeroxaltox@gmail.com>
2018-11-08 00:40:19 +0300

    removed unnecessary log

--- packages/foundation-orm/FEntityFactory.ts
+++ packages/foundation-orm/FEntityFactory.ts
@@ -42,7 +42,6 @@ export abstract class FEntityFactory<T extends FEntity> {
     async findAll() {
         let res = await this.namespace.range(this.connection, []);
         res = res.filter((v) => !v.key.find((k) => k === '__indexes'));
-        console.log(res);
         return res.map((v) => this.doCreateEntity(v.item, false));
     }
 
