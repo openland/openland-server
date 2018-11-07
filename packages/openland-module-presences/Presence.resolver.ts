8692506035fd685a41edf1826569dd22cf986167
altox <xeroxaltox@gmail.com>
2018-11-08 00:04:50 +0300

    fix presence resolver

--- packages/openland-module-presences/Presence.resolver.ts
+++ packages/openland-module-presences/Presence.resolver.ts
@@ -3,61 +3,63 @@ import { withAny } from 'openland-server/api/utils/Resolvers';
 import { CallContext } from 'openland-server/api/utils/CallContext';
 
 export default {
-    presenceReportOnline: async (_: any, args: { timeout: number, platform?: string }, context: CallContext) => {
-        if (!context.uid) {
-            throw Error('Not authorized');
-        }
-        if (args.timeout <= 0) {
-            throw Error('Invalid input');
-        }
-        if (args.timeout > 5000) {
-            throw Error('Invalid input');
-        }
-        await Modules.Presence.setOnline(context.uid, context.tid!, args.timeout, args.platform || 'unknown');
-        return 'ok';
-    },
-    presenceReportOffline: withAny<{ platform?: string }>(async (args, ctx) => {
-        // TODO: Implement
-        return 'ok';
-    }),
-    
-    // Deprecated
-    alphaReportOnline: async (_: any, args: { timeout: number, platform?: string }, context: CallContext) => {
-        if (!context.uid) {
-            throw Error('Not authorized');
-        }
-        if (args.timeout <= 0) {
-            throw Error('Invalid input');
-        }
-        if (args.timeout > 5000) {
-            throw Error('Invalid input');
-        }
-        await Modules.Presence.setOnline(context.uid, context.tid!, args.timeout, args.platform || 'unknown');
-        return 'ok';
-    },
-    alphaReportOffline: withAny<{ platform?: string }>(async (args, ctx) => {
-        // TODO: Implement
-        return 'ok';
-    }),
+    Mutation: {
+        presenceReportOnline: async (_: any, args: { timeout: number, platform?: string }, context: CallContext) => {
+            if (!context.uid) {
+                throw Error('Not authorized');
+            }
+            if (args.timeout <= 0) {
+                throw Error('Invalid input');
+            }
+            if (args.timeout > 5000) {
+                throw Error('Invalid input');
+            }
+            await Modules.Presence.setOnline(context.uid, context.tid!, args.timeout, args.platform || 'unknown');
+            return 'ok';
+        },
+        presenceReportOffline: withAny<{ platform?: string }>(async (args, ctx) => {
+            // TODO: Implement
+            return 'ok';
+        }),
 
-    // TODO: Move to Push Module
-    alphaReportActive: async (_: any, args: { timeout: number, platform?: string }, context: CallContext) => {
-        if (!context.uid) {
-            throw Error('Not authorized');
-        }
-        if (args.timeout <= 0) {
-            throw Error('Invalid input');
-        }
-        if (args.timeout > 5000) {
-            throw Error('Invalid input');
-        }
+        // Deprecated
+        alphaReportOnline: async (_: any, args: { timeout: number, platform?: string }, context: CallContext) => {
+            if (!context.uid) {
+                throw Error('Not authorized');
+            }
+            if (args.timeout <= 0) {
+                throw Error('Invalid input');
+            }
+            if (args.timeout > 5000) {
+                throw Error('Invalid input');
+            }
+            await Modules.Presence.setOnline(context.uid, context.tid!, args.timeout, args.platform || 'unknown');
+            return 'ok';
+        },
+        alphaReportOffline: withAny<{ platform?: string }>(async (args, ctx) => {
+            // TODO: Implement
+            return 'ok';
+        }),
 
-        // FIXME
-        // let token = await DB.UserToken.findById(context.tid);
-        // token!.lastIp = context.ip;
-        // await token!.save();
+        // TODO: Move to Push Module
+        alphaReportActive: async (_: any, args: { timeout: number, platform?: string }, context: CallContext) => {
+            if (!context.uid) {
+                throw Error('Not authorized');
+            }
+            if (args.timeout <= 0) {
+                throw Error('Invalid input');
+            }
+            if (args.timeout > 5000) {
+                throw Error('Invalid input');
+            }
 
-        // await Repos.Users.markUserActive(context.uid, args.timeout, context.tid!!, args.platform);
-        return 'ok';
-    },
+            // FIXME
+            // let token = await DB.UserToken.findById(context.tid);
+            // token!.lastIp = context.ip;
+            // await token!.save();
+
+            // await Repos.Users.markUserActive(context.uid, args.timeout, context.tid!!, args.platform);
+            return 'ok';
+        },
+    }
 };
