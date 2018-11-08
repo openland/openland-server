5b81aafde9c2de7d53d4c472391ba6e4e7a3ff5c
kor_ka <gleb.putintsev@openland.com>
2018-11-08 17:23:39 +0300

    alphaChatTextSearch tracer

--- packages/openland-module-messaging/DialogsSearch.resolver.ts
+++ packages/openland-module-messaging/DialogsSearch.resolver.ts
@@ -2,55 +2,59 @@ import { FDB } from 'openland-module-db/FDB';
 import { withAccount } from 'openland-module-api/Resolvers';
 import { Modules } from 'openland-modules/Modules';
 import { Message } from 'openland-module-db/schema';
+import { createTracer } from 'openland-log/createTracer';
+import { withTracing } from 'openland-log/withTracing';
+
+const tracer = createTracer('chat-text-search');
 
 export default {
     Query: {
         alphaChatTextSearch: withAccount<{ query: string }>(async (args, uid, oid) => {
+            return await withTracing(tracer, 'chat-text-search', async () => {
+                // GROUPS / CHANNELS has titles we can search 
+                let searchableConversations = Promise.all((await FDB.UserDialog.allFromUser(uid)).map((v) => FDB.Conversation.findById(v.cid)));
 
-            // GROUPS / CHANNELS has titles we can search 
-            let searchableConversations = Promise.all((await FDB.UserDialog.allFromUser(uid)).map((v) => FDB.Conversation.findById(v.cid)));
+                let groupsChannels = (await Promise.all((await searchableConversations)
+                    .filter((v) => v!.kind === 'room')
+                    .map((v) => FDB.RoomProfile.findById(v!.id))))
+                    .filter((v) => v!.title.toLocaleLowerCase().indexOf(args.query.toLowerCase()) >= 0)
+                    .map((v) => v!);
 
-            let groupsChannels = (await Promise.all((await searchableConversations)
-                .filter((v) => v!.kind === 'room')
-                .map((v) => FDB.RoomProfile.findById(v!.id))))
-                .filter((v) => v!.title.toLocaleLowerCase().indexOf(args.query.toLowerCase()) >= 0)
-                .map((v) => v!);
+                // PERSONAL - search users first, then matching conversations with current user
+                let personal = Promise.all((
+                    await Modules.Users.searchForUsers(args.query, { uid, limit: 50 })).map((v) => Modules.Messaging.conv.resolvePrivateChat(uid, v)));
 
-            // PERSONAL - search users first, then matching conversations with current user
-            let personal = Promise.all((
-                await Modules.Users.searchForUsers(args.query, { uid, limit: 50 })).map((v) => Modules.Messaging.conv.resolvePrivateChat(uid, v)));
+                // Organizations chats
+                let orgsConvs = await Promise.all((await Modules.Orgs.findUserOrganizations(uid)).map(o => FDB.ConversationOrganization.findFromOrganization(o)));
+                let oganizationsConversations = (await Promise.all(orgsConvs.filter(oc => !!oc).map(oc => oc!).map(oc => FDB.Conversation.findById(oc.id)))).filter(oc => !!oc).map(oc => oc!);
 
-            // Organizations chats
-            let orgsConvs = await Promise.all((await Modules.Orgs.findUserOrganizations(uid)).map(o => FDB.ConversationOrganization.findFromOrganization(o)));
-            let oganizationsConversations = (await Promise.all(orgsConvs.filter(oc => !!oc).map(oc => oc!).map(oc => FDB.Conversation.findById(oc.id)))).filter(oc => !!oc).map(oc => oc!);
-
-            let res = [...await personal, ...await groupsChannels, ...oganizationsConversations];
-            res = res.reduce(
-                (p, x) => {
-                    if (!p.find(c => c.id === x.id)) {
-                        p.push(x);
+                let res = [...await personal, ...await groupsChannels, ...oganizationsConversations];
+                res = res.reduce(
+                    (p, x) => {
+                        if (!p.find(c => c.id === x.id)) {
+                            p.push(x);
+                        }
+                        return p;
+                    },
+                    [] as any[]
+                );
+                let messages = new Map<number, Message | null>();
+                for (let c of res) {
+                    let msg = await FDB.Message.rangeFromChat(c.id, 1, true);
+                    if (msg.length === 0) {
+                        messages.set(c.id, null);
+                    } else {
+                        messages.set(c.id, msg[0]);
                     }
-                    return p;
-                },
-                [] as any[]
-            );
-            let messages = new Map<number, Message | null>();
-            for (let c of res) {
-                let msg = await FDB.Message.rangeFromChat(c.id, 1, true);
-                if (msg.length === 0) {
-                    messages.set(c.id, null);
-                } else {
-                    messages.set(c.id, msg[0]);
                 }
-            }
-            res = res.filter(c => messages.get(c.id))
-                .sort((a, b) => {
-                    let lastMessageA = messages.get(a.id);
-                    let lastMessageB = messages.get(b.id);
-                    return (lastMessageB ? new Date((lastMessageB as any).createdAt).getTime() : 0) - (lastMessageA ? new Date((lastMessageA as any).createdAt).getTime() : 0);
-                });
-            return res;
-
+                res = res.filter(c => messages.get(c.id))
+                    .sort((a, b) => {
+                        let lastMessageA = messages.get(a.id);
+                        let lastMessageB = messages.get(b.id);
+                        return (lastMessageB ? new Date((lastMessageB as any).createdAt).getTime() : 0) - (lastMessageA ? new Date((lastMessageA as any).createdAt).getTime() : 0);
+                    });
+                return res;
+            });
         }),
     }
 };
