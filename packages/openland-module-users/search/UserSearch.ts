906760e23ab8db8201093b7784110c8000da5dc0
Steve Kite <steve@statecraft.one>
2018-11-13 17:28:16 +0300

    Add user search tracing

--- packages/openland-module-users/search/UserSearch.ts
+++ packages/openland-module-users/search/UserSearch.ts
@@ -1,62 +1,70 @@
 import { Modules } from 'openland-modules/Modules';
 import { FDB } from 'openland-module-db/FDB';
+import { createTracer } from 'openland-log/createTracer';
+import { withTracing } from 'openland-log/withTracing';
 
+const tracer = createTracer('room-search');
 export class UserSearch {
     async searchForUsers(query: string, options?: { uid?: number, limit?: number }) {
-        let normalized = query.trim();
+        return await withTracing(tracer, 'search', async () => {
 
-        let mainQuery: any = {
-            bool: {
-                should: normalized.length > 0 ? [
-                    { match_phrase_prefix: { search: query } }
-                ] : [],
-                must_not: options && options.uid ? [
-                    { match: { _id: options.uid } }
-                ] : [],
-            }
-        };
+            let normalized = query.trim();
 
-        if (options && options.uid) {
-            let profile = await FDB.UserProfile.findById(options.uid);
-            let organizations = await Modules.Orgs.findUserOrganizations(options.uid);
-            let functions: any[] = [];
+            let mainQuery: any = {
+                bool: {
+                    should: normalized.length > 0 ? [
+                        { match_phrase_prefix: { search: query } }
+                    ] : [],
+                    must_not: options && options.uid ? [
+                        { match: { _id: options.uid } }
+                    ] : [],
+                }
+            };
 
-            // Huge boost if primary organization same
-            if (profile && profile.primaryOrganization) {
-                functions.push({
-                    filter: { match: { primaryOrganization: profile.primaryOrganization } },
-                    weight: 8
-                });
-            }
+            if (options && options.uid) {
+                let profile = await FDB.UserProfile.findById(options.uid);
+                let organizations = await Modules.Orgs.findUserOrganizations(options.uid);
+                let functions: any[] = [];
 
-            // Boost if have common organizations (more common organizations - more boost)
-            if (organizations.length > 0) {
-                for (let o of organizations) {
+                // Huge boost if primary organization same
+                if (profile && profile.primaryOrganization) {
                     functions.push({
-                        filter: { match: { organizations: o } },
-                        weight: 2
+                        filter: { match: { primaryOrganization: profile.primaryOrganization } },
+                        weight: 8
                     });
                 }
-            }
 
-            if (functions.length > 0) {
-                mainQuery = {
-                    function_score: {
-                        query: mainQuery,
-                        functions: functions,
-                        boost_mode: 'multiply'
+                // Boost if have common organizations (more common organizations - more boost)
+                if (organizations.length > 0) {
+                    for (let o of organizations) {
+                        functions.push({
+                            filter: { match: { organizations: o } },
+                            weight: 2
+                        });
                     }
-                };
-            }
-        }
+                }
 
-        let hits = await Modules.Search.elastic.client.search({
-            index: 'user_profile',
-            type: 'user_profile',
-            size: options && options.limit ? options.limit : 20,
-            body: { query: mainQuery }
+                if (functions.length > 0) {
+                    mainQuery = {
+                        function_score: {
+                            query: mainQuery,
+                            functions: functions,
+                            boost_mode: 'multiply'
+                        }
+                    };
+                }
+            }
+            
+            return await withTracing(tracer, 'elastic', async () => {
+                let hits = await Modules.Search.elastic.client.search({
+                    index: 'user_profile',
+                    type: 'user_profile',
+                    size: options && options.limit ? options.limit : 20,
+                    body: { query: mainQuery }
+                });
+                let uids = hits.hits.hits.map((v) => parseInt(v._id, 10));
+                return uids;
+            });
         });
-        let uids = hits.hits.hits.map((v) => parseInt(v._id, 10));
-        return uids;
     }
 }
