c81864b8b01c4a74a1fa3ddbeba754af6445e2b7
Steve Kite <steve@statecraft.one>
2018-11-13 20:01:32 +0300

    Add cached logger tx

--- packages/foundation-orm/withCache.ts
+++ packages/foundation-orm/withCache.ts
@@ -1,5 +1,8 @@
 import { FCacheContext } from './FCacheContext';
+import { currentTime } from 'openland-utils/timer';
+import { createLogger } from 'openland-log/createLogger';
 
+const log = createLogger('tx-cache');
 export async function withCache<T>(callback: () => Promise<T>): Promise<T> {
     let ex = FCacheContext.context.value;
     if (ex) {
@@ -7,7 +10,12 @@ export async function withCache<T>(callback: () => Promise<T>): Promise<T> {
     }
 
     let cache = new FCacheContext();
+    let start = currentTime();
     return await FCacheContext.context.withContext(cache, async () => {
-        return await callback();
+        try {
+            return await callback();
+        } finally {
+            log.debug('full read tx time: ' + (currentTime() - start) + ' ms');
+        }
     });
 }
