132e64f8baa9e2702134b226cfccca6ec60ce7f6
Steve Kite <steve@statecraft.one>
2018-11-15 09:05:45 +0300

    gql tracing

--- packages/openland-module-api/schema/Schema.ts
+++ packages/openland-module-api/schema/Schema.ts
@@ -3,13 +3,15 @@ import { makeExecutableSchema } from 'graphql-tools';
 import * as Basics from './Date';
 
 import { Directives, IDScalars, injectIDScalars } from './Directives2';
-// import { GraphQLField, GraphQLFieldResolver } from 'graphql';
-// import { wrapAllResolvers } from '../Resolvers';
-// import { withLogContext } from '../../openland-log/withLogContext';
+import { GraphQLField, GraphQLFieldResolver } from 'graphql';
+import { wrapAllResolvers } from '../Resolvers';
+import { withLogContext } from '../../openland-log/withLogContext';
 // import { trace } from 'openland-log/trace';
 // import { gqlTracer } from 'openland-graphql/gqlTracer';
 import { buildSchema } from 'openland-graphql/buildSchema';
 import { buildResolvers } from 'openland-graphql/buildResolvers';
+import { AppContext } from 'openland-modules/AppContext';
+import { gqlTracer } from 'openland-graphql/gqlTracer';
 // import { withTracingSpan } from 'openland-log/withTracing';
 // import { withCache } from 'foundation-orm/withCache';
 
@@ -26,36 +28,22 @@ export const Schema = () => {
         ),
         schemaDirectives: Directives
     });
-    return executableSchema;
+    // return executableSchema;
 
-    // return wrapAllResolvers(executableSchema,
-    //     async (
-    //         field: GraphQLField<any, any>,
-    //         originalResolver: GraphQLFieldResolver<any, any, any>,
-    //         root: any,
-    //         args: any,
-    //         context: any,
-    //         info: any
-    //     ) => {
-    //         if (context.span) {
-    //             return await withTracingSpan(context.span, async () => {
-    //                 return await trace(gqlTracer, field.name, async () => {
-    //                     return await withLogContext(field.name, async () => {
-    //                         return await withCache(async () => {
-    //                             return await originalResolver(root, args, context, info);
-    //                         });
-    //                     });
-    //                 });
-    //             });
-    //         } else {
-    //             return await trace(gqlTracer, field.name, async () => {
-    //                 return await withLogContext(field.name, async () => {
-    //                     return await withCache(async () => {
-    //                         return await originalResolver(root, args, context, info);
-    //                     });
-    //                 });
-    //             });
-    //         }
-    //     }
-    // );
+    return wrapAllResolvers(executableSchema,
+        async (
+            field: GraphQLField<any, any>,
+            originalResolver: GraphQLFieldResolver<any, any, any>,
+            root: any,
+            args: any,
+            context: any,
+            info: any
+        ) => {
+            let ctx = (context as AppContext).ctx;
+            ctx = withLogContext(ctx, [field.name]);
+            return await gqlTracer.trace(ctx, field.name, async (ctx2) => {
+                return await originalResolver(root, args, new AppContext(ctx2), info);
+            });
+        }
+    );
 };
