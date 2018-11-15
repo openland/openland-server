18e7f28bc62db7d30edf8ac4002d1f96ab6fc107
Steve Kite <steve@statecraft.one>
2018-11-15 07:36:13 +0300

    Remove --runInBand

--- scripts/stress_test.sh
+++ scripts/stress_test.sh
@@ -1,4 +1,4 @@
-cat stress_test.http | vegeta attack -rate=1000/s -duration=10s | tee results.bin | vegeta report
+cat stress_test.http | vegeta attack -rate=1000/s -duration=30s | tee results.bin | vegeta report
 vegeta report -type=json results.bin > metrics.json
 cat results.bin | vegeta plot > plot.html
 cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
