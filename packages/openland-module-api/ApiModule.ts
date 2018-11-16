a7896470d6c93a559db738c1e0b4b8430e18812c
altox <xeroxaltox@gmail.com>
2018-11-16 21:06:15 +0300

    schema spec version checking

--- packages/openland-module-api/ApiModule.ts
+++ packages/openland-module-api/ApiModule.ts
@@ -2,12 +2,18 @@ import { serverRoleEnabled } from 'openland-utils/serverRoleEnabled';
 import { initApi } from './initApi';
 import { initHealthcheck } from './initHealthcheck';
 import { injectable } from 'inversify';
+import { GQL_SPEC_VERSION } from './schema/SchemaSpec';
+import { getSchemeVersion } from './schema/SchemaSpecGenerator';
+import { buildSchema } from '../openland-graphql/buildSchema';
 
 @injectable()
 export class ApiModule {
     start = async () => {
         console.log('start API Module');
         if (serverRoleEnabled('api')) {
+            if (GQL_SPEC_VERSION !== getSchemeVersion(buildSchema(__dirname + '/../'))) {
+                throw new Error('Schema version mismatch, did you forgot to run yarn schema:gen ?');
+            }
             await initApi(false);
         } else {
             if (!serverRoleEnabled('admin')) {
