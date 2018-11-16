a532844168568161a08d0214c40582f4e37a4bc0
Steve Kite <steve@statecraft.one>
2018-11-16 17:51:36 +0300

    Call subscriptions

--- packages/openland-module-calls/Calls.resolver.ts
+++ packages/openland-module-calls/Calls.resolver.ts
@@ -3,6 +3,8 @@ import { Modules } from 'openland-modules/Modules';
 import { IDs } from 'openland-module-api/IDs';
 import { ConferenceRoom, ConferenceParticipant } from 'openland-module-db/schema';
 import { Context } from 'openland-utils/Context';
+import { AppContext } from 'openland-modules/AppContext';
+import { FDB } from 'openland-module-db/FDB';
 
 export default {
     Conference: {
@@ -31,5 +33,31 @@ export default {
             await Modules.Calls.repo.conferenceJoin(ctx, IDs.Conversation.parse(args.id), uid, ctx.auth.tid!, 15000);
             return Modules.Calls.repo.findConference(ctx, IDs.Conversation.parse(args.id));
         })
+    },
+    Subscription: {
+        alphaConferenceWatch: {
+            resolve: async (msg: any) => {
+                return msg;
+            },
+            subscribe: async function (_: any, args: { id: string }, ctx: AppContext) {
+                let cid = IDs.Conversation.parse(args.id);
+                let ended = false;
+                return {
+                    ...(async function* func() {
+                        while (!ended) {
+                            let settings = await FDB.ConferenceRoom.findById(ctx, cid);
+                            yield settings;
+                            await new Promise((resolve) => FDB.ConferenceRoom.watch(ctx, cid, () => {
+                                resolve();
+                            }));
+                        }
+                    })(),
+                    return: async () => {
+                        ended = true;
+                        return 'ok';
+                    }
+                };
+            }
+        }
     }
 };
