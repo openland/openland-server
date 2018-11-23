1ec9641c7003c3e0d6970efde66992a70ec4fbd2
altox <xeroxaltox@gmail.com>
2018-11-23 16:50:47 +0300

    change log route version

--- packages/openland-module-push/safari/handlers.ts
+++ packages/openland-module-push/safari/handlers.ts
@@ -9,7 +9,7 @@ let log = createLogger('safari push');
 let ctx = createEmptyContext();
 
 export function initSafariPush(app: Express) {
-    app.post('/push/safari/v2/log', handleLog);
+    app.post('/push/safari/v1/log', handleLog);
     app.post('/push/safari/v2/devices/:deviceToken/registrations/:websitePushID', handleRegister);
     app.delete('/push/safari/v2/devices/:deviceToken/registrations/:websitePushID', handleDelete);
     app.get('/push/safari/v2/pushPackages/web.com.openland', handlePushPackage);
