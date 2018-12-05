f797771c46b8aa121e56d16564e3169e1d552f02
altox <xeroxaltox@gmail.com>
2018-12-05 17:54:29 +0300

    alphaEditPostMessage

--- packages/openland-module-messaging/resolvers/Chat.resolver.ts
+++ packages/openland-module-messaging/resolvers/Chat.resolver.ts
@@ -721,7 +721,9 @@ export default {
             return await Modules.Messaging.deleteMessage(ctx, messageId, uid);
         }),
 
+        //
         // Message Posts
+        //
         alphaSendPostMessage: withUser(async (parent, args, uid) => {
             let ctx = withLogContext(parent, 'send-post-message');
             let conversationId = IDs.Conversation.parse(args.conversationId);
@@ -752,6 +754,37 @@ export default {
                 repeatKey: args.repeatKey,
                 type: 'POST'
             });
+        }),
+        alphaEditPostMessage: withUser(async (parent, args, uid) => {
+            let ctx = withLogContext(parent, 'send-post-message');
+            let messageId = IDs.Conversation.parse(args.messageId);
+
+            let attachments: MessageAttachment[] = [];
+
+            if (args.attachments) {
+                for (let file of args.attachments) {
+                    let fileMetadata: JsonMap | null = null;
+                    let filePreview: string | null = null;
+
+                    let fileInfo = await Modules.Media.saveFile(file);
+                    fileMetadata = fileInfo as any;
+
+                    if (fileInfo.isImage) {
+                        filePreview = await Modules.Media.fetchLowResPreview(file);
+                    }
+
+                    attachments.push({ fileId: file, filePreview, fileMetadata: fileMetadata || null });
+                }
+            }
+
+            return Modules.Messaging.editMessage(ctx, messageId, uid!, {
+                title: args.title,
+                message: args.text,
+                attachments: attachments,
+                buttons: args.buttons,
+                repeatKey: args.repeatKey,
+                type: 'POST'
+            }, true);
         }),
 
         //
