2f9d206bbf343878b3c7fab66effc26381bc5fbd
Steve Kite <steve@statecraft.one>
2019-01-04 14:31:34 +0300

    fix(server): Fix bindings

--- packages/openland-module-feed/FeedModule.container.ts
+++ packages/openland-module-feed/FeedModule.container.ts
@@ -1,6 +1,8 @@
 import { container } from 'openland-modules/Modules.container';
 import { FeedModule } from './FeedModule';
+import { FeedRepository } from './repositories/FeedRepository';
 
 export function loadFeedModule() {
     container.bind(FeedModule).toSelf().inSingletonScope();
+    container.bind(FeedRepository).toSelf().inSingletonScope();
 }
