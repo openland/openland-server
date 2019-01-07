deb211535cb25044bf79336550b89d660e73ecb3
altox <xeroxaltox@gmail.com>
2019-01-07 19:17:28 +0300

    feat(api): ignore draft api files

--- packages/openland-graphql/buildSchema.ts
+++ packages/openland-graphql/buildSchema.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import { findAllFiles } from './utils/findAllFiles';
 
 export function buildSchema(rootPath: string) {
-    let schema = findAllFiles(rootPath, (src) => src.endsWith('.graphql'))
+    let schema = findAllFiles(rootPath, (src) => src.endsWith('.graphql')  && !src.endsWith('.draft.graphql'))
         .map((f) => fs.readFileSync(f, 'utf-8'))
         .sort()
         .join('\n');
