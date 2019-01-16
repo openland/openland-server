3c68155ffd984ed0f0372d5823cab0206557441b
Steve Kite <steve@statecraft.one>
2019-01-16 18:55:46 +0300

    feat(track): Implement tracking export

--- packages/openland-module-hyperlog/workers/declareAmplitudeIndexer.ts
+++ packages/openland-module-hyperlog/workers/declareAmplitudeIndexer.ts
@@ -1,12 +1,39 @@
-// import { updateReader } from 'openland-module-workers/updateReader';
-// import { FDB } from 'openland-module-db/FDB';
-// import { createEmptyContext } from 'openland-utils/Context';
+import { updateReader } from 'openland-module-workers/updateReader';
+import { FDB } from 'openland-module-db/FDB';
+import { createEmptyContext } from 'openland-utils/Context';
+import request, { Response } from 'request';
 
 export function declareAmplitudeIndexer() {
     if (process.env.AMPLITUDE_KEY) {
-        // TODO: Implement
-        // updateReader('amplitude-indexer', 1, FDB.HyperLog.createUserEventsStream(createEmptyContext(), 50), async (items) => {
-            
-        // });
+        let apiKey = process.env.AMPLITUDE_KEY;
+        updateReader('amplitude-indexer', 1, FDB.HyperLog.createUserEventsStream(createEmptyContext(), 50), async (items) => {
+            for (let i of items) {
+                let event = i.body as { id: string, name: string, args: any, uid?: number, tid?: string, did: string };
+                var eventData = {
+                    user_id: event.uid,
+                    device_id: event.did,
+                    event_type: event.name,
+                    event_properties:  event.args
+                };
+
+                await new Promise((resolve, reject) => {
+                    request.post({
+                        url: 'https://api.amplitude.com/httpapi',
+                        form: {
+                            api_key: apiKey,
+                            event: JSON.stringify(eventData)
+                        }
+                    }, function (err: any, response: Response, body: any) {
+                        if (err) {
+                            reject(err);
+                        } else if (response.statusCode !== 200) {
+                            reject(Error('Amplitude status ' + response.statusCode + ': "' + body + '"'));
+                        } else {
+                            resolve();
+                        }
+                    });
+                });
+            }
+        });
     }
 }
