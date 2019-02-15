0706ba659dc6f147866701fe2525040a116be946
altox <xeroxaltox@gmail.com>
2019-02-15 21:24:37 +0300

    fix(search): fix profile indexer

--- packages/openland-module-users/workers/userProfileIndexer.ts
+++ packages/openland-module-users/workers/userProfileIndexer.ts
@@ -4,7 +4,7 @@ import { Modules } from 'openland-modules/Modules';
 import { createEmptyContext } from 'openland-utils/Context';
 
 export function userProfileIndexer() {
-    declareSearchIndexer('user-profile-index', 13, 'user_profile', FDB.UserIndexingQueue.createUpdatedStream(createEmptyContext(), 50))
+    declareSearchIndexer('user-profile-index', 14, 'user_profile', FDB.UserIndexingQueue.createUpdatedStream(createEmptyContext(), 50))
         .withProperties({
             primaryOrganization: {
                 type: 'keyword'
@@ -45,7 +45,12 @@ export function userProfileIndexer() {
         })
         .start(async (item) => {
             let ctx = createEmptyContext();
-            let profile = (await FDB.UserProfile.findById(ctx, item.id))!;
+            let profile = (await FDB.UserProfile.findById(ctx, item.id));
+
+            if (!profile) {
+                return null;
+            }
+
             let shortName = await Modules.Shortnames.findUserShortname(ctx, item.id);
             let orgs = await Modules.Orgs.findUserOrganizations(ctx, item.id);
 
