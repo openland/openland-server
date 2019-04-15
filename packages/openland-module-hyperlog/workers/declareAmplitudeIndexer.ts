5959c055cea99dd02026c4c88f88957b3cdd93a7
altox <xeroxaltox@gmail.com>
2019-04-15 17:48:07 +0300

    feat(analytics): cohort user property

--- packages/openland-module-hyperlog/workers/declareAmplitudeIndexer.ts
+++ packages/openland-module-hyperlog/workers/declareAmplitudeIndexer.ts
@@ -2,53 +2,84 @@ import { updateReader } from 'openland-module-workers/updateReader';
 import { FDB } from 'openland-module-db/FDB';
 import { createEmptyContext } from 'openland-utils/Context';
 import request, { Response } from 'request';
+import { inTx } from '../../foundation-orm/inTx';
 
 const AMPLITUDE_TEST_KEY = '158a2c0e7619751c92cdf1943462a44e';
+const AMPLITUDE_KEY = process.env.AMPLITUDE_KEY;
+
+const saveEvents = async (events: any[], isProd: boolean) => {
+    await new Promise((resolve, reject) => {
+        request.post({
+            url: 'https://api.amplitude.com/httpapi',
+            form: {
+                api_key: isProd ? AMPLITUDE_KEY : AMPLITUDE_TEST_KEY,
+                event: JSON.stringify(events)
+            }
+        }, function (err: any, response: Response, body: any) {
+            if (err) {
+                console.warn(err);
+                reject(err);
+            } else if (response.statusCode !== 200) {
+                console.warn(response);
+                reject(Error('Amplitude status ' + response.statusCode + ': "' + body + '"'));
+            } else {
+                console.log('Export successful...');
+                resolve();
+            }
+        });
+    });
+};
 
 export function declareAmplitudeIndexer() {
     if (process.env.AMPLITUDE_KEY) {
-        let apiKey = process.env.AMPLITUDE_KEY;
         updateReader('amplitude-indexer', 2, FDB.HyperLog.createUserEventsStream(createEmptyContext(), 50), async (items) => {
-            const saveEvents = async (events: any[], isProd: boolean) => {
-                await new Promise((resolve, reject) => {
-                    request.post({
-                        url: 'https://api.amplitude.com/httpapi',
-                        form: {
-                            api_key: isProd ? apiKey : AMPLITUDE_TEST_KEY,
-                            event: JSON.stringify(events)
-                        }
-                    }, function (err: any, response: Response, body: any) {
-                        if (err) {
-                            console.warn(err);
-                            reject(err);
-                        } else if (response.statusCode !== 200) {
-                            console.warn(response);
-                            reject(Error('Amplitude status ' + response.statusCode + ': "' + body + '"'));
-                        } else {
-                            console.log('Export successful...');
-                            resolve();
-                        }
-                    });
-                });
-            };
-
-            const mapEvent = (body: any) => {
-                let event = body as { id: string, name: string, args: any, uid?: number, tid?: string, did: string, platform: 'Android'|'iOS'|'WEB', isProd: boolean };
-                return {
-                    user_id: event.uid,
-                    device_id: event.did,
-                    event_type: event.name,
-                    event_properties: event.args,
-                    insert_id: event.id,
-                    platform: event.platform
+            await inTx(createEmptyContext(), async (ctx) => {
+                const mapEvent = (body: any) => {
+                    let event = body as { id: string, name: string, args: any, uid?: number, tid?: string, did: string, platform: 'Android'|'iOS'|'WEB', isProd: boolean };
+                    return {
+                        user_id: event.uid,
+                        device_id: event.did,
+                        event_type: event.name,
+                        event_properties: event.args,
+                        insert_id: event.id,
+                        platform: event.platform
+                    };
                 };
-            };
 
-            let eventsProd = items.filter(i => i.body.isProd === true).map(i => mapEvent(i.body));
-            let eventsTest = items.filter(i => i.body.isProd === false).map(i => mapEvent(i.body));
+                const addUserProps = async (event: any) => {
+                    if (event.user_id) {
+                        let profile = await FDB.UserProfile.findById(ctx, event.user_id);
+                        if (profile) {
+                            let date = new Date(profile.createdAt);
 
-            await saveEvents(eventsProd, true);
-            await saveEvents(eventsTest, false);
+                            let start = new Date(date.getFullYear(), 0, 0);
+                            let diff = ((date as any) - (start as any)) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
+                            let oneDay = 1000 * 60 * 60 * 24;
+                            let day = Math.floor(diff / oneDay);
+
+                            let firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+                            let pastDaysOfYear = ((date as any) - (firstDayOfYear as any)) / 86400000;
+                            let week = Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+
+                            let month = date.getUTCMonth() + 1;
+                            let year = date.getFullYear();
+
+                            event.cohort_day = day;
+                            event.cohort_week = week;
+                            event.cohort_month = month;
+                            event.cohort_year = year;
+                        }
+                    }
+
+                    return event;
+                };
+
+                let eventsProd = await Promise.all(items.filter(i => i.body.isProd === true).map(i => addUserProps(mapEvent(i.body))));
+                let eventsTest = await Promise.all(items.filter(i => i.body.isProd === false).map(i => addUserProps(mapEvent(i.body))));
+
+                await saveEvents(eventsProd, true);
+                await saveEvents(eventsTest, false);
+            });
         });
     }
 }
