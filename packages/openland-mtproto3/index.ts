feec5d15c976a79535856cd1ba6320589100cb5a
altox <xeroxaltox@gmail.com>
2019-05-23 17:52:11 +0300

    wip(server): some work on ws server

--- packages/openland-mtproto3/index.ts
+++ packages/openland-mtproto3/index.ts
@@ -3,11 +3,13 @@ import WebSocket = require('ws');
 import * as http from 'http';
 import * as https from 'https';
 import { isAsyncIterator, isSubscriptionQuery } from './utils';
+import { delay } from '../openland-utils/timer';
 
 interface FuckApolloServerParams {
     server?: http.Server | https.Server;
     path: string;
     executableSchema: GraphQLSchema;
+
     onAuth(payload: any, req: http.IncomingMessage): Promise<any>;
     context(params: any): Promise<any>;
     genSessionId(authParams: any): Promise<string>;
@@ -38,18 +40,32 @@ class FuckApolloSession {
 }
 
 async function handleMessage(params: FuckApolloServerParams, socket: WebSocket, req: http.IncomingMessage, session: FuckApolloSession, message: any) {
+
+    const stopOperation = (id: string) => {
+        session.operations[id].destroy();
+        delete session.operations[id];
+    };
+
     if (session.state === 'INIT') {
         // handle auth here
         if (!message.type && message.type !== 'connection_init') {
             socket.close();
             return;
         }
+
         session.setWaitingConnect();
         session.waitAuth = (async () => {
             session.authParams = await params.onAuth(message.payload, req);
-            session.send({ type: 'connection_ack' });
+            session.send({type: 'connection_ack'});
             session.waitAuth = Promise.resolve();
             session.setConnected();
+            // tslint:disable-next-line:no-floating-promises
+            (async () => {
+               while (session.socket.readyState === WebSocket.OPEN && session.state  === 'CONNECTED') {
+                   session.send({type: 'ka'});
+                   await delay(5000);
+               }
+            })();
         })();
 
     } else if (session.state === 'CONNECTED' || session.state === 'WAITING_CONNECT') {
@@ -73,15 +89,17 @@ async function handleMessage(params: FuckApolloServerParams, socket: WebSocket, 
 
                     if (!isAsyncIterator(iterator)) {
                         // handle error
-                        session.send({id: message.id, type: 'data', payload: iterator });
-                        session.send({id: message.id, type: 'complete', payload: null });
+                        session.send({id: message.id, type: 'data', payload: iterator});
+                        session.send({id: message.id, type: 'complete', payload: null});
+                        stopOperation(message.id);
                         return;
                     }
                     for await (let event of iterator) {
                         if (!working) {
+                            session.send({id: message.id, type: 'complete', payload: null});
                             return;
                         }
-                        session.send({id: message.id, type: 'data', payload: event });
+                        session.send({id: message.id, type: 'data', payload: event});
                     }
                 })();
                 session.operations[message.id] = {
@@ -97,19 +115,16 @@ async function handleMessage(params: FuckApolloServerParams, socket: WebSocket, 
                     variableValues: message.payload.variables,
                     contextValue: await params.context(session.authParams)
                 });
-                session.send({ id: message.id, type: 'data', payload: result });
-                session.operations[message.id] = {
-                    destroy: () => 0
-                };
+                session.send({id: message.id, type: 'data', payload: result});
+                session.send({id: message.id, type: 'complete', payload: null});
             }
         } else if (message.type && message.type === 'stop') {
             if (session.operations[message.id]) {
-                session.operations[message.id].destroy();
-                session.send({id: message.id, type: 'complete', payload: null });
+                stopOperation(message.id);
             }
-        } else if (message.type && message.type === 'connection_close') {
+        } else if (message.type && message.type === 'connection_terminate') {
             for (let operationId in session.operations) {
-                session.operations[operationId].destroy();
+                stopOperation(operationId);
             }
         }
     }
@@ -118,20 +133,28 @@ async function handleMessage(params: FuckApolloServerParams, socket: WebSocket, 
 async function handleConnection(params: FuckApolloServerParams, socket: WebSocket, req: http.IncomingMessage) {
     let session = new FuckApolloSession(socket);
 
+    const stopAllOperations = () => {
+        for (let operationId in session.operations) {
+            session.operations[operationId].destroy();
+            delete session.operations[operationId];
+        }
+    };
+
     socket.on('message', async data => {
-        console.log('got data', data.toString());
         await handleMessage(params, socket, req, session, JSON.parse(data.toString()));
     });
     socket.on('close', (code, reason) => {
         console.log('close connection', code, reason);
+        stopAllOperations();
     });
     socket.on('error', (err) => {
         console.log('connection error', err);
+        stopAllOperations();
     });
 }
 
 export async function createFuckApolloWSServer(params: FuckApolloServerParams) {
-    const ws = new WebSocket.Server(params.server ? { server: params.server, path: params.path } : { noServer: true });
+    const ws = new WebSocket.Server(params.server ? {server: params.server, path: params.path} : {noServer: true});
     ws.on('connection', async (socket, req) => {
         await handleConnection(params, socket, req);
     });
