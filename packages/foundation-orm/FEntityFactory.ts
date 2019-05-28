29f8602847201b71adcb174a7e9b14b7d3d76075
Steve Kite <steve@statecraft.one>
2019-05-27 19:38:52 -0700

    chore(fdb): Improve tracing

--- packages/foundation-orm/FEntityFactory.ts
+++ packages/foundation-orm/FEntityFactory.ts
@@ -74,28 +74,28 @@ export abstract class FEntityFactory<T extends FEntity> {
         // Cached
         let cache = FCacheContextContext.get(parent);
         if (cache && !FTransactionContext.get(parent)) {
-            return await tracer.trace(parent, 'findById [cached]', async (ctx) => {
-                let cacheKey = FKeyEncoding.encodeKeyToString([...this.namespace.namespace, ...key]);
-                let cached = cache!.findInCache(cacheKey);
-                if (cached !== undefined) {
-                    return await (cached as Promise<T | null>);
-                }
+            let cacheKey = FKeyEncoding.encodeKeyToString([...this.namespace.namespace, ...key]);
+            let cached = cache!.findInCache(cacheKey);
+            if (cached !== undefined) {
+                return await (cached as Promise<T | null>);
+            }
 
-                let res: Promise<T | null> = (async () => {
-                    let r = await this.namespace.get(ctx, this.connection, key);
+            let res: Promise<T | null> = (async () => {
+                return await tracer.trace(parent, 'FindById:' + this.name, async (ctx) => { 
+                    let r =  await this.namespace.get(ctx, this.connection, key);
                     if (r) {
                         return this.doCreateEntity(ctx, r, false);
                     } else {
                         return null;
                     }
-                })();
-                cache!.putInCache(cacheKey, res);
-                return await res;
-            });
+                });
+            })();
+            cache!.putInCache(cacheKey, res);
+            return await res;
         }
 
         // Uncached
-        return await tracer.trace(parent, 'findById', async (ctx) => {
+        return await tracer.trace(parent, 'FindById:' + this.name, async (ctx) => {
             let res = await this.namespace.get(ctx, this.connection, key);
             if (res) {
                 return this.doCreateEntity(ctx, res, false);
@@ -106,7 +106,7 @@ export abstract class FEntityFactory<T extends FEntity> {
 
     protected async _findFromIndex(parent: Context, key: (string | number)[]) {
         // let res = await this.directory.get(key);
-        return await tracer.trace(parent, 'findFromIndex', async (ctx) => {
+        return await tracer.trace(parent, 'FindById', async (ctx) => {
             let res = await this.namespace.get(ctx, this.connection, key);
             if (res) {
                 return this.doCreateEntity(ctx, res, false);
