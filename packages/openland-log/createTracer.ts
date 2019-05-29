3bdf40de328fd6b7b229034306881f1fcca51812
Steve Kite <steve@statecraft.one>
2019-05-29 15:42:51 -0700

    pefr(server): Use new jaeger-client and use UDP reporting

--- packages/openland-log/createTracer.ts
+++ packages/openland-log/createTracer.ts
@@ -1,23 +1,14 @@
 import { STracer } from './STracer';
 import { OpenTracer } from './src/OpenTracer';
 import { NoOpTracer } from './src/NoOpTracer';
-
-var initTracer = require('jaeger-client').initTracer;
+import { initTracer } from 'jaeger-client';
+// var initTracer = require('jaeger-client').initTracer;
 const enabled = true;
 
 export function createTracer(name: string): STracer {
     var tracer: STracer;
-    if (enabled && (process.env.TRACING_ENDPOINT || process.env.NODE_ENV !== 'production')) {
-        tracer = new OpenTracer(initTracer({
-            serviceName: name,
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            reporter: {
-                collectorEndpoint: process.env.NODE_ENV === 'production' ? process.env.TRACING_ENDPOINT : 'http://localhost:14268/api/traces',
-            }
-        }));
+    if (enabled && (process.env.TRACING || process.env.NODE_ENV !== 'production')) {
+        tracer = new OpenTracer(initTracer({ serviceName: name }, {}));
     } else {
         tracer = new NoOpTracer();
     }
