b557840ce4e2541980d5e831805936ee46ae64e3
Steve Kite <steve@statecraft.one>
2019-05-31 14:14:22 -0700

    perf(*): Some ORM benchmarks

--- packages/openland-benchmarks/index.ts
+++ packages/openland-benchmarks/index.ts
@@ -1,87 +1,190 @@
-// global.Promise = require('bluebird');
-// import '../openland-utils/SafeContext';
+// Register Modules
+require('module-alias/register');
 
 // tslint:disable:no-floating-promises
+import Benchmark from 'benchmark';
+// @ts-ignore
 import * as fdb from 'foundationdb';
+// @ts-ignore
 import { RandomIDFactory } from '../openland-security/RandomIDFactory';
+// @ts-ignore
 import { delay } from '../openland-utils/timer';
+import { AllEntitiesDirect } from 'foundation-orm/tests/testSchema';
+import { FConnection } from 'foundation-orm/FConnection';
+import { NoOpBus } from 'foundation-orm/tests/NoOpBus';
+import { inTx } from 'foundation-orm/inTx';
+import { createEmptyContext } from 'openland-utils/Context';
 
 fdb.setAPIVersion(510);
-(async () => {
-    let db1 = fdb.openSync()
-        // .wi(fdb.encoders.json)
-        .at(fdb.encoders.tuple.pack(['_benchmarking_2']));
-    await db1.clearRange(fdb.encoders.tuple.pack([]));
-    let db = db1.withKeyEncoding(fdb.encoders.tuple)
-        .withValueEncoding(fdb.encoders.json);
-    let random1 = new RandomIDFactory(1);
-    let random2 = random1; // new RandomIDFactory(0);
-    // let index = 0;
-    let start = Date.now();
-    let promises = [];
-    let count = 0;
-    let retry = 0;
-    let txTime = 0;
-    let rawtxtime = 0;
-    for (let i = 0; i < 50000; i++) {
-        promises.push((async () => {
-            let txstart = Date.now();
-            await db.doTn(async (v) => {
-                // index++;
-                let txopstart = Date.now();
-                count++;
-                let random = i % 2 === 1 ? random1 : random2;
-                let id = random.next();
-                // while (true) {
-                //     let existing = await (await v.getRange([id], ['ff'.repeat(id.length / 2)], { limit: 1 })).next();
-                //     if (existing.done) {
-                //         break;
-                //     }
-                //     retry++;
-                //     id = random.next();
-                // }
-                if (await v.get([id])) {
-                    retry++;
-                    id = random.next();
-                }
-                // await v.get([id]);
-                v.set([id], 1);
-                // await v.get([id]);
-                // let res = await v.get([id]);
-                // if (res) {
-                //     v.set([id], res + 1);
-                // } else {
-                //     v.set([id], 1);
-                // }
+let db1 = fdb.openSync()
+    .at(fdb.encoders.tuple.pack(['_benchmarking_2']));
+// @ts-ignore
+let db = db1.withKeyEncoding(fdb.encoders.tuple)
+    .withValueEncoding(fdb.encoders.json);
 
-                // let res2 = await v.get(['counter2!' + i]);
-                // if (res2) {
-                //     v.set(['counter2!' + (i)], res2 + 1);
-                // } else {
-                //     v.set(['counter2!' + (i)], 1);
-                // }
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                await delay(0);
-                rawtxtime += Date.now() - txopstart;
-            }, { causal_read_risky: true });
-            txTime += Date.now() - txstart;
-        })());
+let suite = new Benchmark.Suite('FDB');
+
+function addAsync(name: string, fn: () => Promise<void>) {
+    suite.add(name, {
+        defer: true, fn: (d: any) => {
+            (async () => {
+                try {
+                    await fn();
+                } finally {
+                    d.resolve();
+                }
+            })();
+        }
+    });
+}
+
+// addAsync('random-id-factory', async () => {
+//     let random1 = new RandomIDFactory(1);
+//     for (let i = 0; i < 100; i++) {
+//         random1.next();
+//     }
+// });
+addAsync('tx-1', async () => {
+    for (let i = 0; i < 1; i++) {
+        await db1.doTn(async (tx) => {
+            await tx.get('key1-' + i);
+            tx.set('key1-' + i, 'key2');
+        });
     }
-    for (let p of promises) {
-        await p;
+});
+
+addAsync('tx-10', async () => {
+    let p: any[] = [];
+    for (let i = 0; i < 10; i++) {
+        let p2 = db1.doTn(async (tx) => {
+            await tx.get('key2-' + i);
+            tx.set('key2-' + i, 'key2');
+        });
+        p.push(p2);
     }
-    let delta = (Date.now() - start);
-    console.log('Incremented in ' + delta + ' ms (' + count + '), speed: ' + (count / (delta / 1000)) + ' ops/sec');
-    console.log('retry: ' + retry);
-    console.log('tx retry probability: ' + retry / 500000);
-    console.log('txtime: ' + (txTime / count) + ' ms');
-    console.log('rawtxtime: ' + (rawtxtime / count) + ' ms');
+    await Promise.all(p);
+});
+
+addAsync('tx-100', async () => {
+    let p: any[] = [];
+    for (let i = 0; i < 100; i++) {
+        let p2 = db1.doTn(async (tx) => {
+            await tx.get('key3-' + i);
+            tx.set('key3-' + i, 'key2');
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+addAsync('tx-1000', async () => {
+    let p: any[] = [];
+    for (let i = 0; i < 1000; i++) {
+        let p2 = db1.doTn(async (tx) => {
+            await tx.get('key3-' + i);
+            tx.set('key3-' + i, 'key2');
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+addAsync('tx-1000-routine', async () => {
+    let p: any[] = [];
+    for (let i = 0; i < 1000; i++) {
+        let p2 = db1.doTn(async (tx) => {
+            await (async () => {
+                await tx.get('key4-' + i);
+            })();
+            tx.set('key4-' + i, 'key2');
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+addAsync('tx-10000', async () => {
+    let p: any[] = [];
+    for (let i = 0; i < 10000; i++) {
+        let p2 = db1.doTn(async (tx) => {
+            await tx.get('key5-' + i);
+            tx.set('key5-' + i, 'key2');
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+addAsync('tx-tuple-10000', async () => {
+    let p: any[] = [];
+    for (let i = 0; i < 10000; i++) {
+        let p2 = db.doTn(async (tx) => {
+            await tx.get(['entity_test', 'something', i]);
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+addAsync('orm-1000', async () => {
+    let entities = new AllEntitiesDirect(new FConnection(db1 as any, NoOpBus, false));
+    let p: any[] = [];
+    for (let i = 0; i < 1000; i++) {
+        let p2 = inTx(createEmptyContext(), async (ctx) => {
+            await entities.SimpleEntity.findById(ctx, i);
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+addAsync('orm-10000', async function orm10000() {
+    let entities = new AllEntitiesDirect(new FConnection(db1 as any, NoOpBus, false));
+    let p: any[] = [];
+    for (let i = 0; i < 10000; i++) {
+        let p2 = inTx(createEmptyContext(), async (ctx) => {
+            await entities.SimpleEntity.findById(ctx, i);
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+addAsync('orm-10000-empty', async function orm10000Empty() {
+    let entities = new AllEntitiesDirect(new FConnection(db1 as any, NoOpBus, false));
+    let p: any[] = [];
+    for (let i = 0; i < 10000; i++) {
+        let p2 = inTx(createEmptyContext(), async (ctx) => {
+            await entities.SimpleEntity.findById(ctx, 100000 + i);
+        });
+        p.push(p2);
+    }
+    await Promise.all(p);
+});
+
+suite.on('cycle', function (event: any) {
+    console.log(String(event.target));
+});
+
+(async () => {
+    await db1.clearRange(fdb.encoders.tuple.pack([]));
+    let entities = new AllEntitiesDirect(new FConnection(db1 as any, NoOpBus, false));
+    // let keySize = 4096;
+    let p: any[] = [];
+    console.log('Prepare');
+    for (let i = 0; i < 10000; i++) {
+        let p2 = inTx(createEmptyContext(), async (ctx) => {
+            await entities.SimpleEntity.create(ctx, i, { data: 'data-0' });
+            // await tx.get('key-read-' + i);
+        });
+        p.push(p2);
+
+        let p3 = db.doTn(async (tn) => {
+            tn.set(['entity_test', 'something', i], { hello: 'world' });
+        });
+        p.push(p3);
+    }
+    await Promise.all(p);
+    console.log('Starting');
+    suite.run({ async: false });
 })();
