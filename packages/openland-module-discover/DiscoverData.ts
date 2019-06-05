0492dcf6a5c6a2836e81ca72c2c2df210f88b485
kor_ka <gleb.putintsev@openland.com>
2019-06-05 13:51:37 +0300

    chore(server): update DiscoverData

--- packages/openland-module-discover/DiscoverData.ts
+++ packages/openland-module-discover/DiscoverData.ts
@@ -3,71 +3,71 @@ import { IDs } from 'openland-module-api/IDs';
 export type Tag = { id: string, title: string, score: number };
 export type TagGroup = { id: string, title?: string, subtitle?: string, tags: Tag[], score: number };
 
-const _data = `Na me,Link,Role,Founder_sub_1,Founder_sub_2, Engineer_sub,all triggers
-Score: 4 (in t he end of re commendations list),,,,,,
-Founder Chats,https ://openland.com/mail/p /ZYx4d9K6kjIZ5jo6r69zc4AX3v,Founder,,,,"Founder, "
-Founders · Random ,https://openland.com/mail/3 Ym4RrOAbxIMAa43Qv1WFDymz4,Founder,,,,"Founder, "
-Venture Capita l,https://openlan d.com/directory/r/nqoZQV6zYQCmnRv19evPSMJPaE,Investor,,,,"Investor, "
-Accelerat ors,https://open land.com/directory/r/qljZr9WbgLiL6YK0gDO6faQVYV,Investor,,,,"Investor, "
-Product C hats,https:// openland.com/mail/rAb139w0Mzc4XrgvdxvEH5DYRO,Product manager,,,,"Product manager, "
-Openland Tech ,https://open land.com/mail/p/LOLqoerbADtq4xDP0dBzuwJwx3,Engineer,,,,"Engineer, "
-Engineer Cha ts,https://openl and.com/mail/p/VywdDrg3byuRx0dqmyaRfrR7Pb,Engineer,,,,"Engineer, "
-Engineer s · Random,https://openland.com/mail/p/Y96dY7aOP9UAMLbZpoAJHdqdKg,Engineer,,,,"Engineer, "
+const _data = `Name,Link,Role,Founder_sub_1,Founder_sub_2,Engineer_sub,all triggers
+Score: 4 (in the end of recommendations list),,,,,,
+Founder Chats,https://openland.com/mail/p/ZYx4d9K6kjIZ5jo6r69zc4AX3v,Founder,,,,"Founder, "
+Founders · Random,https://openland.com/mail/3Ym4RrOAbxIMAa43Qv1WFDymz4,Founder,,,,"Founder, "
+Venture Capital,https://openland.com/directory/r/nqoZQV6zYQCmnRv19evPSMJPaE,Investor,,,,"Investor, "
+Accelerators,https://openland.com/directory/r/qljZr9WbgLiL6YK0gDO6faQVYV,Investor,,,,"Investor, "
+Product Chats,https://openland.com/mail/rAb139w0Mzc4XrgvdxvEH5DYRO,Product manager,,,,"Product manager, "
+Openland Tech,https://openland.com/mail/p/LOLqoerbADtq4xDP0dBzuwJwx3,Engineer,,,,"Engineer, "
+Engineer Chats,https://openland.com/mail/p/VywdDrg3byuRx0dqmyaRfrR7Pb,Engineer,,,,"Engineer, "
+Engineers · Random,https://openland.com/mail/p/Y96dY7aOP9UAMLbZpoAJHdqdKg,Engineer,,,,"Engineer, "
 Community Managers 😎,https://openland.com/mail/p/Rgq6MV7Q5gCb6r53E1koT7BMBZ,Community manager,,,,"Community manager, "
 Openland News,https://openland.com/mail/p/EQvPJ1LamRtJJ9ppVxDDs30Jzw,"Community manager,Designer,Engineer,Founder,Investor,Other roles,Product manager,Recruiter",,,,"Founder, Investor, Product manager, Engineer, Designer, Recruiter, Other roles, Community manager, "
 ,,,,,,
 Score: 2,,,,,,
-Proptech,https://openland.com/mail/p/EQvPJ1LaODS1WAAx65wVI3m55l,Founder,Proptech,,,"Founder, "
-The Future of Messaging,https://openland.com/mail/p/ZYx4d9K6VmhljdO6qm5pi7ORMB,Founder,Messaging,,,"Founder, "
-Fintech Founders,https://openland.com/directory/r/nqoZQV6zYXIWAk34Qak1UMMeeZ,Founder,Fintech,,,"Founder, "
-Marketing Tech Founders,https://openland.com/directory/r/wW4975KQkLixVAznByLlTO1Mna,Founder,Marketing Tech,,,"Founder, "
-HR Tech Founders,https://openland.com/directory/r/0DW7dl3rzJFvjn5m0vELuxA3Xq,Founder,HR Tech,,,"Founder, "
-Media Founders,https://openland.com/directory/r/9KkDvyowQmCRx7MpJ6LghDvXkA,Founder,Media,,,"Founder, "
-Healthcare Founders,https://openland.com/directory/r/b5RYKeLkwWixOd6BO94OFyqxDP,Founder,Healthcare,,,"Founder, "
-Social Apps Founders,https://openland.com/directory/r/mJvq41O57dsbEXWqDy9yCzdAEr,Founder,Social apps,,,"Founder, "
-Dev Tools Founders,https://openland.com/directory/r/b5RYKeLkwgtPE7q3r5kAhvowlW,Founder,Dev Tools,,,"Founder, "
-Local Tech Founders,https://openland.com/directory/r/jZVjLe3a7YfEKvb6PAY5CXWBdo,Founder,Local Tech,,,"Founder, "
-EdTech Founders,https://openland.com/directory/r/vmZR69a4k0FoVWZXZk7zHBakbn,Founder,EdTech,,,"Founder, "
-Ecommerce Founders,https://openland.com/directory/r/g065jdJYwku3WXVk5RweFlDmaD,Founder,Ecommerce,,,"Founder, "
-Productivity Tech Founders,https://openland.com/directory/r/vmZR69a4k0FP1knWjYyWsBP95J,Founder,Productivity Tech,,,"Founder, "
-Transportation Founders,https://openland.com/directory/r/b5RYKeLkwWimlnLrrWxdtYqZQe,Founder,Transportation,,,"Founder, "
-Security Founders,https://openland.com/directory/r/b5RYKeLkw5FK3BOWoRk1ulYo5v,Founder,Security,,,"Founder, "
-AI Founders,https://openland.com/mail/Om49WwAP7BspmarAvko0fWPj1R,Founder,AI,,,"Founder, "
-AR/VR Founders,https://openland.com/directory/r/dB6k5PZDyoUdYvlJkRmeCekQe5,Founder,AR / VR,,,"Founder, "
-Crypto Founders,https://openland.com/directory/r/zoqLwdzrE5CMJpgkRrmZTWeReK,Founder,Cryptocurrencies,,,"Founder, "
-SaaS Founders,https://openland.com/mail/mJvq41O57dsqWMbzL5okUzJgEP,Founder,SaaS,,,"Founder, "
-Marketplace Founders,https://openland.com/mail/dB6k5PZDyoUOgOzWr73yi7VjDm,Founder,Marketplace,,,"Founder, "
-Hardware Founders,https://openland.com/directory/r/BPV0ZljY7PtKZp3X9LQaUaenWX,Founder,Hardware,,,"Founder, "
-Mental Health Founders,https://openland.com/directory/r/qljZr9Wb7VFwq4epebo4Uaqk1w,Founder,Mental Health,,,"Founder, "
-Parenting Tech,https://openland.com/directory/r/Ko0zOxqjenu5mve3QD7VIWQnOe,Founder,Parenting Tech,,,"Founder, "
-Travel Tech Founders,https://openland.com/directory/r/zoqLwdzrErcQq33MPbPRfQZZpZ,Founder,Travel Tech,,,"Founder, "
-Gaming Founders,https://openland.com/directory/r/av6pa90nyvTVVKrzPkD4ij6dkx,Founder,Gaming,,,"Founder, "
-Food Tech Founders,https://openland.com/directory/r/jZVjLe3a7pUBmBRDxPWjHoP7bw,Founder,Food Tech,,,"Founder, "
-Ag Tech Founders,https://openland.com/directory/r/9KkDvyowQRImMglYKJAOc40JQ6,Founder,AgTech,,,"Founder, "
-Biotech Founders,https://openland.com/directory/r/wW4975KQkDf74YmLABoEsMq3WR,Founder,Biotech,,,"Founder, "
-Fashion Tech Founders,https://openland.com/directory/r/orzRJa7oMecnnPDexZLzTpjXd0,Founder,Fashion Tech,,,"Founder, "
-Urbantech Founders,https://openland.com/mail/p/jZVjLe3a7ZCLDmZOPb9otP5jxM,Founder,Urbantech,,,"Founder, "
-Legal Tech,https://openland.com/directory/r/ZYx4d9K6kPFZArMYBpxkS0Brdp,Founder,Legal Tech,,,"Founder, "
-Hard Tech Founders,https://openland.com/directory/r/7Vd4aLWmZRFPPBAnl5x9fPJPor,Founder,Hard Tech,,,"Founder, "
-Nonprofit Founders,https://openland.com/directory/r/M6Pl7R30AmCvZZ31pXzdidVb33,Founder,Nonprofit,,,"Founder, "
-MusicTech Founders,https://openland.com/directory/r/Ko0zOxqjeyUagOeQlWEZT49JmO,Founder,Music Tech,,,"Founder, "
+Proptech,https://openland.com/mail/p/EQvPJ1LaODS1WAAx65wVI3m55l,,Proptech,,,
+The Future of Messaging,https://openland.com/mail/p/ZYx4d9K6VmhljdO6qm5pi7ORMB,,Messaging,,,
+Fintech Founders,https://openland.com/directory/r/nqoZQV6zYXIWAk34Qak1UMMeeZ,,Fintech,,,
+Marketing Tech Founders,https://openland.com/directory/r/wW4975KQkLixVAznByLlTO1Mna,,Marketing Tech,,,
+HR Tech Founders,https://openland.com/directory/r/0DW7dl3rzJFvjn5m0vELuxA3Xq,,HR Tech,,,
+Media Founders,https://openland.com/directory/r/9KkDvyowQmCRx7MpJ6LghDvXkA,,Media,,,
+Healthcare Founders,https://openland.com/directory/r/b5RYKeLkwWixOd6BO94OFyqxDP,,Healthcare,,,
+Social Apps Founders,https://openland.com/directory/r/mJvq41O57dsbEXWqDy9yCzdAEr,,Social apps,,,
+Dev Tools Founders,https://openland.com/directory/r/b5RYKeLkwgtPE7q3r5kAhvowlW,,Dev Tools,,,
+Local Tech Founders,https://openland.com/directory/r/jZVjLe3a7YfEKvb6PAY5CXWBdo,,Local Tech,,,
+EdTech Founders,https://openland.com/directory/r/vmZR69a4k0FoVWZXZk7zHBakbn,,EdTech,,,
+Ecommerce Founders,https://openland.com/directory/r/g065jdJYwku3WXVk5RweFlDmaD,,Ecommerce,,,
+Productivity Tech Founders,https://openland.com/directory/r/vmZR69a4k0FP1knWjYyWsBP95J,,Productivity Tech,,,
+Transportation Founders,https://openland.com/directory/r/b5RYKeLkwWimlnLrrWxdtYqZQe,,Transportation,,,
+Security Founders,https://openland.com/directory/r/b5RYKeLkw5FK3BOWoRk1ulYo5v,,Security,,,
+AI Founders,https://openland.com/mail/Om49WwAP7BspmarAvko0fWPj1R,,AI,,,
+AR/VR Founders,https://openland.com/directory/r/dB6k5PZDyoUdYvlJkRmeCekQe5,,AR / VR,,,
+Crypto Founders,https://openland.com/directory/r/zoqLwdzrE5CMJpgkRrmZTWeReK,,Cryptocurrencies,,,
+SaaS Founders,https://openland.com/mail/mJvq41O57dsqWMbzL5okUzJgEP,,SaaS,,,
+Marketplace Founders,https://openland.com/mail/dB6k5PZDyoUOgOzWr73yi7VjDm,,Marketplace,,,
+Hardware Founders,https://openland.com/directory/r/BPV0ZljY7PtKZp3X9LQaUaenWX,,Hardware,,,
+Mental Health Founders,https://openland.com/directory/r/qljZr9Wb7VFwq4epebo4Uaqk1w,,Mental Health,,,
+Parenting Tech,https://openland.com/directory/r/Ko0zOxqjenu5mve3QD7VIWQnOe,,Parenting Tech,,,
+Travel Tech Founders,https://openland.com/directory/r/zoqLwdzrErcQq33MPbPRfQZZpZ,,Travel Tech,,,
+Gaming Founders,https://openland.com/directory/r/av6pa90nyvTVVKrzPkD4ij6dkx,,Gaming,,,
+Food Tech Founders,https://openland.com/directory/r/jZVjLe3a7pUBmBRDxPWjHoP7bw,,Food Tech,,,
+Ag Tech Founders,https://openland.com/directory/r/9KkDvyowQRImMglYKJAOc40JQ6,,AgTech,,,
+Biotech Founders,https://openland.com/directory/r/wW4975KQkDf74YmLABoEsMq3WR,,Biotech,,,
+Fashion Tech Founders,https://openland.com/directory/r/orzRJa7oMecnnPDexZLzTpjXd0,,Fashion Tech,,,
+Urbantech Founders,https://openland.com/mail/p/jZVjLe3a7ZCLDmZOPb9otP5jxM,,Urbantech,,,
+Legal Tech,https://openland.com/directory/r/ZYx4d9K6kPFZArMYBpxkS0Brdp,,Legal Tech,,,
+Hard Tech Founders,https://openland.com/directory/r/7Vd4aLWmZRFPPBAnl5x9fPJPor,,Hard Tech,,,
+Nonprofit Founders,https://openland.com/directory/r/M6Pl7R30AmCvZZ31pXzdidVb33,,Nonprofit,,,
+MusicTech Founders,https://openland.com/directory/r/Ko0zOxqjeyUagOeQlWEZT49JmO,,Music Tech,,,
 ,,,,,,
 Score: 1,,,,,,
-Next Chapter,https://openland.com/mail/p/BPV0ZljY7qcAPAw709vpurpOaK,Founder,,Next role / co-founder search,,"Founder, "
-Market Research,https://openland.com/mail/p/Om49WwAP7Wfjezv0dBAPt4kLvb,Founder,,Market exploration,,"Founder, "
-CTOs,https://openland.com/directory/r/lQKjZMAv71tMWAXRlwRlF6dWWv,Founder,,Technology development,,"Founder, "
-Product Launch,https://openland.com/directory/r/vmZR69a4k0FoqJEJDykRIyeZ3q,Founder,,Product launch,,"Founder, "
-Product Feedback,https://next.openland.com/directory/r/D4KeQl0V7RH0V07lvyOvClDe1a,Founder,,Product launch,,"Founder, "
-Fundraising Help,https://openland.com/mail/p/4dmAE76OeWfR4qn9kdQKcZaQKy,Founder,,Fundraising,,"Founder, "
-Pitch Deck Review,https://openland.com/directory/r/5Xmd1J76LRujqV4zjjwrIDWBwK,Founder,,Fundraising,,"Founder, "
-Fundraising Tactics,https://openland.com/directory/r/jZVjLe3a4pfxmD6yOO9YUXljM7,Founder,,Fundraising,,"Founder, "
-Growth Chats,https://openland.com/mail/p/VywdDrg3AJfkdXYkMznpibbnWJ,Founder,,Growth,,"Founder, "
-B2B Sales,https://openland.com/mail/Ko0zOxqje5TRbYgjvA6xu4jAjV,Founder,,Sales,,"Founder, "
-Customer Leads,https://openland.com/directory/r/xwQxobvJ7BfdVopQvoBYsOkmPR,Founder,,Sales,,"Founder, "
-Recruiting Help,https://openland.com/directory/r/Rgq6MV7Q59TLP5lKdyXqc3MEjm,Founder,,Recruiting,,"Founder, "
-Services for Startups,https://openland.com/directory/r/Rgq6MV7QAPUK7XoJ5jV5I4xnBm,Founder,,Selling to startups,,"Founder, "
-Startup Operations,https://openland.com/mail/p/jZVjLe3a7giOmDPEgxebsKROWk,Founder,,Operations,,"Founder, "
-Founders · Ask for Intros,https://openland.com/mail/5Xmd1J76LRu0ZxBoYxvZswQeWz,Founder,,Networking,,"Founder, "
+Next Chapter,https://openland.com/mail/p/BPV0ZljY7qcAPAw709vpurpOaK,,,Next role / co-founder search,,
+Market Research,https://openland.com/mail/p/Om49WwAP7Wfjezv0dBAPt4kLvb,,,Market exploration,,
+CTOs,https://openland.com/directory/r/lQKjZMAv71tMWAXRlwRlF6dWWv,,,Technology development,,
+Product Launch,https://openland.com/directory/r/vmZR69a4k0FoqJEJDykRIyeZ3q,,,Product launch,,
+Product Feedback,https://next.openland.com/directory/r/D4KeQl0V7RH0V07lvyOvClDe1a,,,Product launch,,
+Fundraising Help,https://openland.com/mail/p/4dmAE76OeWfR4qn9kdQKcZaQKy,,,Fundraising,,
+Pitch Deck Review,https://openland.com/directory/r/5Xmd1J76LRujqV4zjjwrIDWBwK,,,Fundraising,,
+Fundraising Tactics,https://openland.com/directory/r/jZVjLe3a4pfxmD6yOO9YUXljM7,,,Fundraising,,
+Growth Chats,https://openland.com/mail/p/VywdDrg3AJfkdXYkMznpibbnWJ,,,Growth,,
+B2B Sales,https://openland.com/mail/Ko0zOxqje5TRbYgjvA6xu4jAjV,,,Sales,,
+Customer Leads,https://openland.com/directory/r/xwQxobvJ7BfdVopQvoBYsOkmPR,,,Sales,,
+Recruiting Help,https://openland.com/directory/r/Rgq6MV7Q59TLP5lKdyXqc3MEjm,,,Recruiting,,
+Services for Startups,https://openland.com/directory/r/Rgq6MV7QAPUK7XoJ5jV5I4xnBm,,,Selling to startups,,
+Startup Operations,https://openland.com/mail/p/jZVjLe3a7giOmDPEgxebsKROWk,,,Operations,,
+Founders · Ask for Intros,https://openland.com/mail/5Xmd1J76LRu0ZxBoYxvZswQeWz,,,Networking,,
 ,,,,,,
 Score: 3,,,,,,
 JS Jobs in the Valley,https://openland.com/mail/7Vd4aLWmOMuLBK7AQr4ZSmVQdp,Engineer,,,JS,"Engineer, "
