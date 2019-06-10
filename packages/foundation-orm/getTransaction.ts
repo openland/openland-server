00405bdc977b724d7b89bec040c137bd3bbfd082
altox <xeroxaltox@gmail.com>
2019-06-10 19:55:34 +0300

    wip(server): update schema

--- packages/foundation-orm/getTransaction.ts
+++ packages/foundation-orm/getTransaction.ts
@@ -4,7 +4,7 @@ import { FTransactionContext, FTransactionReadOnlyContext } from './utils/contex
 import { FTransactionReadOnly } from './tx/FTransactionReadOnly';
 import { createLogger } from 'openland-log/createLogger';
 
-const log = createLogger('ephermal');
+const log = createLogger('ephermal', false);
 export function getTransaction(ctx: Context): FTransaction {
     let tx = FTransactionContext.get(ctx);
     if (tx) {
