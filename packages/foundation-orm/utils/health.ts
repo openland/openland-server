5cb995b4e10362e0c32bc7f0d37da62c55cecf87
Steve Korshakov <steve@korshakov.com>
2019-06-13 16:44:31 -0700

    fix(*): Fix build

--- packages/foundation-orm/utils/health.ts
+++ packages/foundation-orm/utils/health.ts
@@ -6,18 +6,18 @@ import { Context } from '@openland/context';
 export async function checkIndexConsistency<T extends FEntity>(parent: Context, entity: FEntityFactory<T>, indexKey: (string | number)[], extractRawId: (value: any) => (string | number)[]) {
     // Find index inconsistency
     let duplicatesCount = 0;
-    await inTx(parent, async (ctx) => {
-        let data = await entity.namespace.keySpace.range(ctx, indexKey);
+    // await inTx(parent, async (ctx) => {
+    //     let data = await entity.namespace.keySpace.range(ctx, indexKey);
 
-        for (let item of data) {
-            let rawId = extractRawId(item.value);
-            let actual = await entity.namespace.keySpace.get(ctx, rawId);
+    //     for (let item of data) {
+    //         let rawId = extractRawId(item.value);
+    //         let actual = await entity.namespace.keySpace.get(ctx, rawId);
 
-            if (JSON.stringify(actual) !== JSON.stringify(item.value)) {
-                duplicatesCount++;
-            }
-        }
-    });
+    //         if (JSON.stringify(actual) !== JSON.stringify(item.value)) {
+    //             duplicatesCount++;
+    //         }
+    //     }
+    // });
 
     return duplicatesCount;
 }
@@ -25,19 +25,19 @@ export async function checkIndexConsistency<T extends FEntity>(parent: Context, 
 export async function fixIndexConsistency<T extends FEntity>(parent: Context, entity: FEntityFactory<T>, indexKey: (string | number)[], extractRawId: (value: any) => (string | number)[], getData: (ctx: Context) => Promise<T[]>) {
     // Remove duplicates from index
     let duplicatesCount = 0;
-    await inTx(parent, async (ctx) => {
-        let data = await entity.namespace.keySpace.range(ctx, indexKey);
+    // await inTx(parent, async (ctx) => {
+    //     let data = await entity.namespace.keySpace.range(ctx, indexKey);
 
-        for (let item of data) {
-            let rawId = extractRawId(item.value);
-            let actual = await entity.namespace.keySpace.get(ctx, rawId);
+    //     for (let item of data) {
+    //         let rawId = extractRawId(item.value);
+    //         let actual = await entity.namespace.keySpace.get(ctx, rawId);
 
-            if (JSON.stringify(actual) !== JSON.stringify(item.value)) {
-                duplicatesCount++;
-                entity.namespace.keySpace.delete(ctx, item.key);
-            }
-        }
-    });
+    //         if (JSON.stringify(actual) !== JSON.stringify(item.value)) {
+    //             duplicatesCount++;
+    //             entity.namespace.keySpace.delete(ctx, item.key);
+    //         }
+    //     }
+    // });
 
     // Reindex all
     await inTx(parent, async (ctx) => {
