d14bd79e524cc846bb4e11d298c7f95a048fbef8
Steve Korshakov <steve@korshakov.com>
2019-06-17 21:18:47 -0700

    ref(fdb): Remove some allKeys usages

--- packages/foundation-orm/FAtomicBooleanFactory.ts
+++ packages/foundation-orm/FAtomicBooleanFactory.ts
@@ -1,4 +1,3 @@
-import { Subspace } from '@openland/foundationdb';
 import { FDirectory } from './FDirectory';
 import { FKeyEncoding } from './utils/FKeyEncoding';
 import { FAtomicBoolean } from './FAtomicBoolean';
@@ -9,11 +8,9 @@ export class FAtomicBooleanFactory {
 
     readonly layer: EntityLayer;
     readonly directory: FDirectory;
-    readonly keySpace: Subspace;
 
     constructor(name: string, layer: EntityLayer) {
         this.layer = layer;
-        this.keySpace = layer.db.allKeys;
         this.directory = layer.directory.getDirectory(['atomic', name]);
     }
 
--- packages/foundation-orm/FAtomicIntegerFactory.ts
+++ packages/foundation-orm/FAtomicIntegerFactory.ts
@@ -2,18 +2,15 @@ import { FDirectory } from './FDirectory';
 import { FKeyEncoding } from './utils/FKeyEncoding';
 import { FAtomicInteger } from './FAtomicInteger';
 import { EntityLayer } from './EntityLayer';
-import { Subspace } from '@openland/foundationdb';
 import { Tuple } from '@openland/foundationdb/lib/encoding';
 
 export class FAtomicIntegerFactory {
 
     readonly layer: EntityLayer;
     readonly directory: FDirectory;
-    readonly keySpace: Subspace;
 
     constructor(name: string, layer: EntityLayer) {
         this.layer = layer;
-        this.keySpace = layer.db.allKeys;
         this.directory = layer.directory.getDirectory(['atomic', name]);
     }
 
