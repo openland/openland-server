4d74ce0d9089f1171b1a2fbfcf1c1d1e8c408b7c
altox <xeroxaltox@gmail.com>
2019-06-19 19:04:07 +0300

    fix(build)

--- packages/openland-module-messaging/Messaging.container.test.ts
+++ packages/openland-module-messaging/Messaging.container.test.ts
@@ -4,10 +4,12 @@ import { TypingsModule } from 'openland-module-typings/TypingsModule';
 import { TypingsModuleMock } from 'openland-module-typings/TypingsModule.mock';
 import { DraftsModule } from 'openland-module-drafts/DraftsModule';
 import { loadCommentsModule } from 'openland-module-comments/CommentsModule.container';
+import { loadNotificationCenterModule } from '../openland-module-notification-center/NotificationCenterModule.container';
 
 export function loadMessagingTestModule() {
     loadMessagingModule();
     loadCommentsModule();
+    loadNotificationCenterModule();
     container.bind(TypingsModule).to(TypingsModuleMock as any).inSingletonScope();
     container.bind(DraftsModule).toSelf().inSingletonScope();
 }
--- packages/openland-module-notification-center/NotificationCenterModule.container.ts
+++ packages/openland-module-notification-center/NotificationCenterModule.container.ts
@@ -2,9 +2,11 @@ import { container } from '../openland-modules/Modules.container';
 import { NotificationCenterRepository } from './repositories/NotificationCenterRepository';
 import { NotificationCenterMediator } from './mediators/NotificationCenterMediator';
 import { NeedDeliveryRepository } from './repositories/NeedDeliveryRepository';
+import { NotificationCenterModule } from './NotificationCenterModule';
 
 export function loadNotificationCenterModule() {
-    container.bind('NotificationCenterRepository').to(NotificationCenterRepository);
-    container.bind('NotificationCenterMediator').to(NotificationCenterMediator);
-    container.bind('NeedDeliveryRepository').to(NeedDeliveryRepository);
+    container.bind('NeedDeliveryRepository').to(NeedDeliveryRepository).inSingletonScope();
+    container.bind('NotificationCenterRepository').to(NotificationCenterRepository).inSingletonScope();
+    container.bind('NotificationCenterMediator').to(NotificationCenterMediator).inSingletonScope();
+    container.bind(NotificationCenterModule).toSelf().inSingletonScope();
 }
