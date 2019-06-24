e03caf7eeebc8de1ebc639e30826a823423e80a0
Steve Korshakov <steve@korshakov.com>
2019-06-23 20:13:39 -0700

    wip(fdb): Working on integration of new compiler

---
+++ packages/openland-module-db/store.schema.ts
@@ -0,0 +1,32 @@
+import { declareSchema, atomicInt, primaryKey, atomicBool } from '@openland/foundationdb-compiler';
+
+export default declareSchema(() => {
+    atomicInt('UserCounter', () => {
+        primaryKey('uid', 'number');
+    });
+    atomicInt('UserMessagesSentCounter', () => {
+        primaryKey('uid', 'number');
+    });
+    atomicInt('UserMessagesReceivedCounter', () => {
+        primaryKey('uid', 'number');
+    });
+    atomicInt('UserMessagesChatsCounter', () => {
+        primaryKey('uid', 'number');
+    });
+    atomicInt('UserMessagesDirectChatsCounter', () => {
+        primaryKey('uid', 'number');
+    });
+
+    atomicInt('UserDialogCounter', () => {
+        primaryKey('uid', 'number');
+        primaryKey('cid', 'number');
+    });
+    atomicBool('UserDialogHaveMention', () => {
+        primaryKey('uid', 'number');
+        primaryKey('cid', 'number');
+    });
+
+    atomicInt('NotificationCenterCounter', () => {
+        primaryKey('ncid', 'number');
+    });
+});
