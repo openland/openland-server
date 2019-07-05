7f4590780409bbd0d2e4b0744ef7c5e36cc8227d
Steve Korshakov <steve@korshakov.com>
2019-07-05 20:11:22 +0300

    fix(fdb): Fix invalid firebase push tokens

--- packages/openland-server/migrations.ts
+++ packages/openland-server/migrations.ts
@@ -64,4 +64,24 @@ migrations.push({
     }
 });
 
+migrations.push({
+    key: '103-remove-invalid-firebase-tokens',
+    migration: async (parent) => {
+        let subspaces = [
+            Store.PushFirebase.descriptor.subspace,
+            ...Store.PushFirebase.descriptor.secondaryIndexes.map((v) => v.subspace)
+        ];
+        for (let s of subspaces) {
+            await inTx(parent, async (ctx) => {
+                let all = await s.range(ctx, []);
+                for (let a of all) {
+                    if (typeof a.value.tid === 'number') {
+                        s.clear(ctx, a.key);
+                    }
+                }
+            });
+        }
+    }
+});
+
 export default migrations;
