3450b5004b18090c24d1626de945fafd43d66653
Steve Korshakov <steve@korshakov.com>
2019-07-05 16:03:02 +0300

    fix(fdb): Support missing metadata values

--- packages/openland-module-db/store.spec.ts
+++ packages/openland-module-db/store.spec.ts
@@ -13,5 +13,7 @@ describe('Store', () => {
     it('should load environment variable', async () => {
         let data = JSON.parse('{\"name\":\"api-error-reporting-bot-id\",\"value\":\"2121\",\"createdAt\":1552053786059,\"updatedAt\":1552053786059,\"_version\":1}');
         (Store.EnvironmentVariable as any)._codec.decode(data);
+
+        (Store.Online as any)._codec.decode('{\"uid\":1087,\"lastSeen\":1558983105850,\"createdAt\":1540523817105,\"updatedAt\":1540523817105,\"active\":true,\"activeExpires\":1558983105850}');
     });
 });
