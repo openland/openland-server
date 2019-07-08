bcf1aa32fa401d7dcfe988857e8a0203cac35beb
altox <xeroxaltox@gmail.com>
2019-07-08 14:22:00 +0300

    wip(server): basic admin interface

--- packages/openland-module-super/startAdminInterface.ts
+++ packages/openland-module-super/startAdminInterface.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import compression from 'compression';
-// import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import { Modules } from 'openland-modules/Modules';
 import * as http from 'http';
 import { createNamedContext } from '@openland/context';
 import { createLogger } from '@openland/log';
-// import { createGraphQLAdminSchema } from 'openland-module-db/tools/GraphEndpoint';
+import { createGraphQLAdminSchema } from 'openland-module-db/tools/GraphEndpoint';
 
 const rootCtx = createNamedContext('admin');
 const logger = createLogger('admin');
@@ -27,20 +27,20 @@ export async function startAdminInterface() {
     app.get('/', (req, res) => res.send('Welcome to Closedland API!'));
     app.get('/stats', async (req, res) => res.json(await Modules.Super.calculateStats(rootCtx)));
 
-    // const Server = new ApolloServer({
-    //     schema: await createGraphQLAdminSchema(),
-    //     introspection: true,
-    //     playground: {
-    //         endpoint: 'https://db.openland.io/graphql',
-    //         settings: {
-    //             'request.credentials': 'include'
-    //         } as any
-    //     }
-    // });
+    const Server = new ApolloServer({
+        schema: await createGraphQLAdminSchema(),
+        introspection: true,
+        playground: {
+            endpoint: 'https://db.openland.io/graphql',
+            settings: {
+                'request.credentials': 'include'
+            } as any
+        }
+    });
 
     const httpServer = http.createServer(app);
-    // Server.applyMiddleware({ app, path: '/graphql' });
-    // Server.applyMiddleware({ app, path: '/api' });
-    // Server.installSubscriptionHandlers(httpServer);
+    Server.applyMiddleware({ app, path: '/graphql' });
+    Server.applyMiddleware({ app, path: '/api' });
+    Server.installSubscriptionHandlers(httpServer);
     httpServer.listen(8319);
 }
