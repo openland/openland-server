22d0ed6cd526c9bb6e62aa8fd0bd30a8732ab5b3
altox <xeroxaltox@gmail.com>
2019-07-17 19:27:58 +0300

    wip(server): disable tracing on prod

--- packages/openland-log/createTracer.ts
+++ packages/openland-log/createTracer.ts
@@ -3,10 +3,13 @@ import { OpenTracer } from './src/OpenTracer';
 import { NoOpTracer } from './src/NoOpTracer';
 import { initTracer } from 'jaeger-client';
 
+const isProd = process.env.APP_ENVIRONMENT === 'production';
+
 export function createTracer(name: string): STracer {
     var tracer: STracer;
-    if (process.env.TRACING) {
-        tracer = new OpenTracer(initTracer({ serviceName: name, sampler: { type: 'probabilistic', param: 0.1 } }, {}));
+    if (!isProd) {
+        tracer = new OpenTracer(initTracer({serviceName: name, sampler: {type: 'const', param: 1}}, {}));
+        // tracer = new OpenTracer(initTracer({serviceName: name, sampler: {type: 'probabilistic', param: 0.1}}, {}));
     } else {
         tracer = new NoOpTracer();
     }
