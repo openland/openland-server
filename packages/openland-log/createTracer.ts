dfa79013c5c112b2384b57e6363bb592c3ed64b0
altox <xeroxaltox@gmail.com>
2019-07-18 16:47:19 +0300

    wip(server): enable back tracing

--- packages/openland-log/createTracer.ts
+++ packages/openland-log/createTracer.ts
@@ -3,15 +3,19 @@ import { OpenTracer } from './src/OpenTracer';
 import { NoOpTracer } from './src/NoOpTracer';
 import { initTracer } from 'jaeger-client';
 
-const isProd = process.env.APP_ENVIRONMENT === 'production';
+// const isProd = process.env.APP_ENVIRONMENT === 'production';
 
 export function createTracer(name: string): STracer {
     var tracer: STracer;
-    if (!isProd) {
-        tracer = new OpenTracer(initTracer({serviceName: name, sampler: {type: 'const', param: 1}}, {}));
-        // tracer = new OpenTracer(initTracer({serviceName: name, sampler: {type: 'probabilistic', param: 0.1}}, {}));
+    if (process.env.TRACING) {
+        tracer = new OpenTracer(initTracer({ serviceName: name, sampler: { type: 'probabilistic', param: 0.1 } }, {}));
     } else {
         tracer = new NoOpTracer();
     }
+    // if (!isProd) {
+    //     tracer = new OpenTracer(initTracer({serviceName: name, sampler: {type: 'const', param: 1}}, {}));
+    // } else {
+    //     tracer = new NoOpTracer();
+    // }
     return tracer;
 }
