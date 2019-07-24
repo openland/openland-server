80548ff5d9ec2fc5fd185e4cfa99b33c51ad4ed7
altox <xeroxaltox@gmail.com>
2019-07-24 18:22:33 +0300

    wip(server): enable gql tracing

--- packages/openland-graphql/gqlTracer.ts
+++ packages/openland-graphql/gqlTracer.ts
@@ -92,14 +92,15 @@ class GQLTracer {
 
 export const gqlTraceNamespace = createContextNamespace<GQLTracer | null>('gql-trace', null);
 
-const isProd = process.env.APP_ENVIRONMENT === 'production';
+// const isProd = process.env.APP_ENVIRONMENT === 'production';
 
 export function withGqlTrace(parent: Context, name: string): Context {
-    if (!isProd) {
-        return gqlTraceNamespace.set(parent, new GQLTracer(name));
-    } else {
-        return parent;
-    }
+    return gqlTraceNamespace.set(parent, new GQLTracer(name));
+    // if (!isProd) {
+    //     return gqlTraceNamespace.set(parent, new GQLTracer(name));
+    // } else {
+    //     return parent;
+    // }
 }
 
 export const GqlQueryIdNamespace = createContextNamespace<string | null>('gql-query-id', null);
