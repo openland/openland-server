361f1bada1e2aa6576d1b5afb8172d20a4587272
altox <xeroxaltox@gmail.com>
2019-07-30 18:24:16 +0300

    wip(server): fix build

--- packages/openland-server/nodeJSTracing.ts
+++ packages/openland-server/nodeJSTracing.ts
@@ -2,17 +2,20 @@ import { createHyperlogger } from '../openland-module-hyperlog/createHyperlogEve
 import { createNamedContext } from '@openland/context';
 import { createLogger } from '@openland/log';
 
-const lagLogger = createHyperlogger<{ lag: number }>('event_loop_lag');
+const lagLogger = createHyperlogger<{ lag_ns: number, lag_ms: number }>('event_loop_lag');
 const ctx = createNamedContext('nodejs-tracing');
 const log = createLogger('event_loop_lag');
 
-const hrTime = () => (process.hrtime as any).bigint() as bigint;
+function hrTime() {
+    let t = process.hrtime();
+    return (t[0] * 1e3) + (t[1] / 1e6);
+}
 
-function measureEventLoopLag(): Promise<bigint> {
+function measureEventLoopLag(): Promise<number> {
     return new Promise(resolve => {
         let start = hrTime();
         setTimeout(() => {
-            resolve(hrTime() - start);
+            resolve((hrTime() - start) * 1000000);
         }, 0);
     });
 }
@@ -20,7 +23,7 @@ function measureEventLoopLag(): Promise<bigint> {
 export function setupNodeJSTracing() {
     setInterval(async () => {
         let lag = await measureEventLoopLag();
-        await lagLogger.event(ctx, { lag: Number(lag) });
-        log.log(ctx, 'lag in ms:', Number(lag / 1000000n), 'lag in ns:', Number(lag));
+        await lagLogger.event(ctx, { lag_ns: lag, lag_ms: lag / 1000000 });
+        log.log(ctx, 'lag in ms:', lag / 1000000, 'lag in ns:', lag);
     }, 1000);
 }
