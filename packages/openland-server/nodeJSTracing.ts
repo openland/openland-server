afcc46966661fcaeb454b0882053c015870502fc
altox <xeroxaltox@gmail.com>
2019-08-01 18:54:07 +0300

    wip(server): remove log

--- packages/openland-server/nodeJSTracing.ts
+++ packages/openland-server/nodeJSTracing.ts
@@ -1,10 +1,8 @@
 import { createHyperlogger } from '../openland-module-hyperlog/createHyperlogEvent';
 import { createNamedContext } from '@openland/context';
-import { createLogger } from '@openland/log';
 
 const lagLogger = createHyperlogger<{ lag_ns: number, lag_ms: number }>('event_loop_lag');
 const ctx = createNamedContext('nodejs-tracing');
-const log = createLogger('event_loop_lag');
 
 function hrTime() {
     let t = process.hrtime();
@@ -24,6 +22,5 @@ export function setupNodeJSTracing() {
     setInterval(async () => {
         let lag = await measureEventLoopLag();
         await lagLogger.event(ctx, { lag_ns: lag, lag_ms: lag / 1000000 });
-        log.log(ctx, 'lag in ms:', lag / 1000000, 'lag in ns:', lag);
     }, 1000);
 }
