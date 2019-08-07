f75434afe81de33710f17b578f4b893f5daa0657
altox <xeroxaltox@gmail.com>
2019-08-07 17:03:35 +0300

    wip(server): optimize push & email workers

--- packages/openland-module-messaging/workers/EmailNotificationWorker.ts
+++ packages/openland-module-messaging/workers/EmailNotificationWorker.ts
@@ -7,6 +7,8 @@ import { hasMention } from '../resolvers/ModernMessage.resolver';
 import { createLogger } from '@openland/log';
 import { singletonWorker } from '@openland/foundationdb-singleton';
 import { delay } from '@openland/foundationdb/lib/utils';
+import { Context } from '@openland/context';
+import { batch } from '../../openland-utils/batch';
 
 const Delays = {
     '15min': 15 * 60 * 1000,
@@ -17,129 +19,132 @@ const Delays = {
 
 const log = createLogger('email');
 
+const handleUser = async (ctx: Context, uid: number) => {
+    const needNotificationDelivery = Modules.Messaging.needNotificationDelivery;
+    let now = Date.now();
+    let ustate = await Modules.Messaging.getUserMessagingState(ctx, uid);
+    let state = await Modules.Messaging.getUserNotificationState(ctx, uid);
+    let lastSeen = await Modules.Presence.getLastSeen(ctx, uid);
+    let isActive = await Modules.Presence.isActive(ctx, uid);
+    let tag = 'email_notifications ' + uid;
+
+    // Ignore active users
+    if (isActive) {
+        return;
+    }
+
+    // Ignore never online
+    if (lastSeen === 'never_online') {
+        needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
+        return;
+    }
+
+    // Ignore recently online users
+    if (lastSeen === 'online' || (lastSeen > now - 5 * 60 * 1000)) {
+        return;
+    }
+
+    // Ignore never opened apps
+    if (state.readSeq === null) {
+        needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
+        return;
+    }
+
+    // Ignore read updates
+    if (state.readSeq === ustate.seq) {
+        needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
+        return;
+    }
+
+    // Ignore already processed updates
+    if (state.lastEmailSeq === ustate.seq) {
+        return;
+    }
+
+    let settings = await Modules.Users.getUserSettings(ctx, uid);
+
+    if (settings.emailFrequency === 'never') {
+        return;
+    }
+
+    // Read email timeouts
+    let delta = Delays[settings.emailFrequency];
+
+    // Do not send emails more than one in an hour
+    if (state.lastEmailNotification !== null && state.lastEmailNotification > now - delta) {
+        return;
+    }
+
+    // Fetch pending updates
+    let remainingUpdates = (await Store.UserDialogEvent.user.query(ctx, uid, { after: Math.max(state.lastEmailSeq ? state.lastEmailSeq : 0, state.readSeq) })).items;
+    let messages = remainingUpdates.filter((v) => v.kind === 'message_received');
+
+    let hasNonMuted = false;
+    let msgs: Message[] = [];
+
+    for (let m of messages) {
+        let message = await Store.Message.findById(ctx, m.mid!);
+        if (!message) {
+            continue;
+        }
+
+        if (message.uid === uid) {
+            continue;
+        }
+
+        // disable email notificaitons for channels
+        let conversation = (await Store.Conversation.findById(ctx, message.cid))!;
+        if (conversation.kind === 'room') {
+            if ((await Store.ConversationRoom.findById(ctx, message.cid))!.kind === 'public') {
+                continue;
+            }
+        }
+
+        // Ignore service messages for big rooms
+        if (message.isService) {
+            if (await Modules.Messaging.roomMembersCount(ctx, message.cid) >= 50) {
+                continue;
+            }
+        }
+
+        let userMentioned = hasMention(message, uid);
+
+        let conversationSettings = await Modules.Messaging.getRoomSettings(ctx, uid, conversation.id);
+        if (conversationSettings.mute && !userMentioned) {
+            continue;
+        }
+
+        if (!message.isMuted) {
+            hasNonMuted = true;
+        }
+
+        msgs.push(message);
+    }
+
+    // Send email notification if there are some
+    if (hasNonMuted) {
+        log.log(ctx, tag, 'new_email_notification');
+        await Emails.sendUnreadMessages(ctx, uid, msgs);
+        state.lastEmailNotification = Date.now();
+    }
+
+    // Save state
+    state.lastEmailSeq = ustate.seq;
+    needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
+};
+
 export function startEmailNotificationWorker() {
     singletonWorker({ name: 'email_notifications', delay: 15000, startDelay: 3000, db: Store.storage.db }, async (parent) => {
-        let needNotificationDelivery = Modules.Messaging.needNotificationDelivery;
-        let unreadUsers = await inTx(parent, async (ctx) => await needNotificationDelivery.findAllUsersWithNotifications(ctx, 'email'));
+        let unreadUsers = await inTx(parent, async (ctx) => await Modules.Messaging.needNotificationDelivery.findAllUsersWithNotifications(ctx, 'email'));
         if (unreadUsers.length > 0) {
             log.debug(parent, 'unread users: ' + unreadUsers.length);
         } else {
             await delay(5000);
             return;
         }
-        let now = Date.now();
-        for (let uid of unreadUsers) {
-            await inTx(parent, async (ctx) => {
-                let ustate = await Modules.Messaging.getUserMessagingState(ctx, uid);
-                let state = await Modules.Messaging.getUserNotificationState(ctx, uid);
-                let lastSeen = await Modules.Presence.getLastSeen(ctx, uid);
-                let isActive = await Modules.Presence.isActive(ctx, uid);
-                let tag = 'email_notifications ' + uid;
-
-                // Ignore active users
-                if (isActive) {
-                    return;
-                }
-
-                // Ignore never online
-                if (lastSeen === 'never_online') {
-                    needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
-                    return;
-                }
-
-                // Ignore recently online users
-                if (lastSeen === 'online' || (lastSeen > now - 5 * 60 * 1000)) {
-                    return;
-                }
-
-                // Ignore never opened apps
-                if (state.readSeq === null) {
-                    needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
-                    return;
-                }
-
-                // Ignore read updates
-                if (state.readSeq === ustate.seq) {
-                    needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
-                    return;
-                }
-
-                // Ignore already processed updates
-                if (state.lastEmailSeq === ustate.seq) {
-                    return;
-                }
-
-                let settings = await Modules.Users.getUserSettings(ctx, uid);
-
-                if (settings.emailFrequency === 'never') {
-                    return;
-                }
-
-                // Read email timeouts
-                let delta = Delays[settings.emailFrequency];
-
-                // Do not send emails more than one in an hour
-                if (state.lastEmailNotification !== null && state.lastEmailNotification > now - delta) {
-                    return;
-                }
-
-                // Fetch pending updates
-                let remainingUpdates = (await Store.UserDialogEvent.user.query(ctx, uid, { after: Math.max(state.lastEmailSeq ? state.lastEmailSeq : 0, state.readSeq) })).items;
-                let messages = remainingUpdates.filter((v) => v.kind === 'message_received');
-
-                let hasNonMuted = false;
-                let msgs: Message[] = [];
-
-                for (let m of messages) {
-                    let message = await Store.Message.findById(ctx, m.mid!);
-                    if (!message) {
-                        continue;
-                    }
-
-                    if (message.uid === uid) {
-                        continue;
-                    }
-
-                    // disable email notificaitons for channels
-                    let conversation = (await Store.Conversation.findById(ctx, message.cid))!;
-                    if (conversation.kind === 'room') {
-                        if ((await Store.ConversationRoom.findById(ctx, message.cid))!.kind === 'public') {
-                            continue;
-                        }
-                    }
-
-                    // Ignore service messages for big rooms
-                    if (message.isService) {
-                        if (await Modules.Messaging.roomMembersCount(ctx, message.cid) >= 50) {
-                            continue;
-                        }
-                    }
-
-                    let userMentioned = hasMention(message, uid);
-
-                    let conversationSettings = await Modules.Messaging.getRoomSettings(ctx, uid, conversation.id);
-                    if (conversationSettings.mute && !userMentioned) {
-                        continue;
-                    }
-
-                    if (!message.isMuted) {
-                        hasNonMuted = true;
-                    }
-
-                    msgs.push(message);
-                }
-
-                // Send email notification if there are some
-                if (hasNonMuted) {
-                    log.log(ctx, tag, 'new_email_notification');
-                    await Emails.sendUnreadMessages(ctx, uid, msgs);
-                    state.lastEmailNotification = Date.now();
-                }
-
-                // Save state
-                state.lastEmailSeq = ustate.seq;
-                needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'email', uid);
-            });
-        }
+        let batches = batch(unreadUsers, 10);
+        await Promise.all(batches.map(b => inTx(parent, async (c) => {
+            await Promise.all(b.map(uid => handleUser(c, uid)));
+        })));
     });
 }
--- packages/openland-module-messaging/workers/PushNotificationWorker.ts
+++ packages/openland-module-messaging/workers/PushNotificationWorker.ts
@@ -105,6 +105,186 @@ export const shouldUpdateUserSeq = (ctx: Context, user: {
     return false;
 };
 
+const handleUser = async (_ctx: Context, uid: number) => {
+    let ctx = withLogPath(_ctx, 'user ' + uid);
+
+    // Loading user's settings and state
+
+    let ustate = await Modules.Messaging.getUserMessagingState(ctx, uid);
+    let settings = await Modules.Users.getUserSettings(ctx, uid);
+    let state = await Modules.Messaging.getUserNotificationState(ctx, uid);
+    let lastSeen = await Modules.Presence.getLastSeen(ctx, uid);
+    let isActive = await Modules.Presence.isActive(ctx, uid);
+
+    const user = {
+        uid,
+        lastSeen,
+        isActive,
+        notificationsDelay: settings.notificationsDelay,
+        notificationsReadSeq: state.readSeq,
+        userStateSeq: ustate.seq,
+        lastPushSeq: state.lastPushSeq,
+        mobileNotifications: settings.mobileNotifications,
+        desktopNotifications: settings.desktopNotifications,
+    };
+
+    if (shouldIgnoreUser(ctx, user)) {
+        await Modules.Push.sendCounterPush(ctx, uid);
+
+        if (shouldResetNotificationDelivery(ctx, user)) {
+            Modules.Messaging.needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'push', uid);
+        }
+        if (shouldUpdateUserSeq(ctx, user)) {
+            state.lastPushSeq = ustate.seq;
+        }
+        return;
+    }
+
+    // Scanning updates
+    let afterSec = Math.max(state.lastEmailSeq ? state.lastEmailSeq : 0, state.readSeq!, state.lastPushSeq || 0);
+
+    let remainingUpdates = (await Store.UserDialogEvent.user.query(ctx, uid, {after: afterSec})).items;
+    let messages = remainingUpdates.filter((v) => v.kind === 'message_received');
+
+    let unreadCounter: number | undefined = undefined;
+
+    // Handling unread messages
+    let hasMessage = false;
+    for (let m of messages) {
+        if (m.seq <= afterSec) {
+            continue;
+        }
+
+        let messageId = m.mid!;
+        let message = await Store.Message.findById(ctx, messageId);
+        if (!message) {
+            continue;
+        }
+        let senderId = message.uid!;
+
+        // Ignore current user
+        if (senderId === uid) {
+            continue;
+        }
+
+        let sender = await Modules.Users.profileById(ctx, senderId);
+        let receiver = await Modules.Users.profileById(ctx, uid);
+        let conversation = await Store.Conversation.findById(ctx, message.cid);
+
+        if (!sender) {
+            continue;
+        }
+
+        if (!receiver) {
+            continue;
+        }
+
+        if (!conversation) {
+            continue;
+        }
+
+        // Ignore service messages for big rooms
+        if (message.isService) {
+            if (await Modules.Messaging.roomMembersCount(ctx, message.cid) >= 50) {
+                continue;
+            }
+        }
+
+        let userMentioned = hasMention(message, uid);
+
+        let sendDesktop = settings.desktopNotifications !== 'none';
+        let sendMobile = settings.mobileNotifications !== 'none';
+
+        // Filter non-private if only direct messages enabled
+        if (settings.desktopNotifications === 'direct') {
+            if (conversation.kind !== 'private') {
+                sendDesktop = false;
+            }
+        }
+        if (settings.mobileNotifications === 'direct') {
+            if (conversation.kind !== 'private') {
+                sendMobile = false;
+            }
+        }
+
+        let conversationSettings = await Modules.Messaging.getRoomSettings(ctx, uid, conversation.id);
+        if (conversationSettings.mute && !userMentioned) {
+            continue;
+        }
+
+        if (userMentioned) {
+            sendMobile = true;
+            sendDesktop = true;
+        }
+
+        if (!sendMobile && !sendDesktop) {
+            continue;
+        }
+
+        let chatTitle = await Modules.Messaging.room.resolveConversationTitle(ctx, conversation.id, uid);
+
+        if (chatTitle.startsWith('@')) {
+            chatTitle = chatTitle.slice(1);
+        }
+
+        hasMessage = true;
+        let senderName = [sender.firstName, sender.lastName].filter((v) => !!v).join(' ');
+
+        let pushTitle = Texts.Notifications.GROUP_PUSH_TITLE({senderName, chatTitle});
+
+        if (conversation.kind === 'private') {
+            pushTitle = chatTitle;
+        }
+
+        if (message.isService) {
+            pushTitle = chatTitle;
+        }
+
+        let pushBody = await fetchMessageFallback(message);
+
+        if (unreadCounter === undefined) {
+            unreadCounter = await Modules.Messaging.fetchUserGlobalCounter(ctx, uid);
+        }
+
+        let push = {
+            uid: uid,
+            title: pushTitle,
+            body: pushBody,
+            picture: sender.picture ? buildBaseImageUrl(sender.picture!!) : null,
+            counter: unreadCounter!,
+            conversationId: conversation.id,
+            mobile: sendMobile,
+            desktop: sendDesktop,
+            mobileAlert: (settings.mobileAlert !== undefined && settings.mobileAlert !== null) ? settings.mobileAlert : true,
+            mobileIncludeText: (settings.mobileIncludeText !== undefined && settings.mobileIncludeText !== null) ? settings.mobileIncludeText : true,
+            silent: null,
+        };
+
+        if (sendMobile) {
+            Modules.Hooks.onMobilePushSent(ctx, uid);
+        }
+        if (sendDesktop) {
+            Modules.Hooks.onDesktopPushSent(ctx, uid);
+        }
+
+        log.debug(ctx, 'new_push', JSON.stringify(push));
+        await Modules.Push.pushWork(ctx, push);
+        // workDone = true;
+    }
+
+    // Save state
+    if (hasMessage) {
+        state.lastPushNotification = Date.now();
+    } else {
+        await Modules.Push.sendCounterPush(ctx, uid);
+    }
+
+    log.debug(ctx, 'updated ' + state.lastPushSeq + '->' + ustate.seq);
+
+    state.lastPushSeq = ustate.seq;
+    Modules.Messaging.needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'push', uid);
+};
+
 export function startPushNotificationWorker() {
     singletonWorker({
         name: 'push_notifications',
@@ -122,190 +302,8 @@ export function startPushNotificationWorker() {
         log.log(parent, 'found', unreadUsers.length, 'users');
 
         let batches = batch(unreadUsers, 10);
-        // let workDone = false;
-        for (let b of batches) {
-            await inTx(parent, async (c) => {
-                for (let uid of b) {
-
-                    let ctx = withLogPath(c, 'user ' + uid);
-
-                    // Loading user's settings and state
-
-                    let ustate = await Modules.Messaging.getUserMessagingState(ctx, uid);
-                    let settings = await Modules.Users.getUserSettings(ctx, uid);
-                    let state = await Modules.Messaging.getUserNotificationState(ctx, uid);
-                    let lastSeen = await Modules.Presence.getLastSeen(ctx, uid);
-                    let isActive = await Modules.Presence.isActive(ctx, uid);
-
-                    const user = {
-                        uid,
-                        lastSeen,
-                        isActive,
-                        notificationsDelay: settings.notificationsDelay,
-                        notificationsReadSeq: state.readSeq,
-                        userStateSeq: ustate.seq,
-                        lastPushSeq: state.lastPushSeq,
-                        mobileNotifications: settings.mobileNotifications,
-                        desktopNotifications: settings.desktopNotifications,
-                    };
-
-                    if (shouldIgnoreUser(ctx, user)) {
-                        await Modules.Push.sendCounterPush(ctx, uid);
-
-                        if (shouldResetNotificationDelivery(ctx, user)) {
-                            Modules.Messaging.needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'push', uid);
-                        }
-                        if (shouldUpdateUserSeq(ctx, user)) {
-                            state.lastPushSeq = ustate.seq;
-                        }
-                        return;
-                    }
-
-                    // Scanning updates
-                    let afterSec = Math.max(state.lastEmailSeq ? state.lastEmailSeq : 0, state.readSeq!, state.lastPushSeq || 0);
-
-                    let remainingUpdates = (await Store.UserDialogEvent.user.query(ctx, uid, {after: afterSec})).items;
-                    let messages = remainingUpdates.filter((v) => v.kind === 'message_received');
-
-                    let unreadCounter: number | undefined = undefined;
-
-                    // Handling unread messages
-                    let hasMessage = false;
-                    for (let m of messages) {
-                        if (m.seq <= afterSec) {
-                            continue;
-                        }
-
-                        let messageId = m.mid!;
-                        let message = await Store.Message.findById(ctx, messageId);
-                        if (!message) {
-                            continue;
-                        }
-                        let senderId = message.uid!;
-
-                        // Ignore current user
-                        if (senderId === uid) {
-                            continue;
-                        }
-
-                        let sender = await Modules.Users.profileById(ctx, senderId);
-                        let receiver = await Modules.Users.profileById(ctx, uid);
-                        let conversation = await Store.Conversation.findById(ctx, message.cid);
-
-                        if (!sender) {
-                            continue;
-                        }
-
-                        if (!receiver) {
-                            continue;
-                        }
-
-                        if (!conversation) {
-                            continue;
-                        }
-
-                        // Ignore service messages for big rooms
-                        if (message.isService) {
-                            if (await Modules.Messaging.roomMembersCount(ctx, message.cid) >= 50) {
-                                continue;
-                            }
-                        }
-
-                        let userMentioned = hasMention(message, uid);
-
-                        let sendDesktop = settings.desktopNotifications !== 'none';
-                        let sendMobile = settings.mobileNotifications !== 'none';
-
-                        // Filter non-private if only direct messages enabled
-                        if (settings.desktopNotifications === 'direct') {
-                            if (conversation.kind !== 'private') {
-                                sendDesktop = false;
-                            }
-                        }
-                        if (settings.mobileNotifications === 'direct') {
-                            if (conversation.kind !== 'private') {
-                                sendMobile = false;
-                            }
-                        }
-
-                        let conversationSettings = await Modules.Messaging.getRoomSettings(ctx, uid, conversation.id);
-                        if (conversationSettings.mute && !userMentioned) {
-                            continue;
-                        }
-
-                        if (userMentioned) {
-                            sendMobile = true;
-                            sendDesktop = true;
-                        }
-
-                        if (!sendMobile && !sendDesktop) {
-                            continue;
-                        }
-
-                        let chatTitle = await Modules.Messaging.room.resolveConversationTitle(ctx, conversation.id, uid);
-
-                        if (chatTitle.startsWith('@')) {
-                            chatTitle = chatTitle.slice(1);
-                        }
-
-                        hasMessage = true;
-                        let senderName = [sender.firstName, sender.lastName].filter((v) => !!v).join(' ');
-
-                        let pushTitle = Texts.Notifications.GROUP_PUSH_TITLE({senderName, chatTitle});
-
-                        if (conversation.kind === 'private') {
-                            pushTitle = chatTitle;
-                        }
-
-                        if (message.isService) {
-                            pushTitle = chatTitle;
-                        }
-
-                        let pushBody = await fetchMessageFallback(message);
-
-                        if (unreadCounter === undefined) {
-                            unreadCounter = await Modules.Messaging.fetchUserGlobalCounter(ctx, uid);
-                        }
-
-                        let push = {
-                            uid: uid,
-                            title: pushTitle,
-                            body: pushBody,
-                            picture: sender.picture ? buildBaseImageUrl(sender.picture!!) : null,
-                            counter: unreadCounter!,
-                            conversationId: conversation.id,
-                            mobile: sendMobile,
-                            desktop: sendDesktop,
-                            mobileAlert: (settings.mobileAlert !== undefined && settings.mobileAlert !== null) ? settings.mobileAlert : true,
-                            mobileIncludeText: (settings.mobileIncludeText !== undefined && settings.mobileIncludeText !== null) ? settings.mobileIncludeText : true,
-                            silent: null,
-                        };
-
-                        if (sendMobile) {
-                            Modules.Hooks.onMobilePushSent(ctx, uid);
-                        }
-                        if (sendDesktop) {
-                            Modules.Hooks.onDesktopPushSent(ctx, uid);
-                        }
-
-                        log.debug(ctx, 'new_push', JSON.stringify(push));
-                        await Modules.Push.pushWork(ctx, push);
-                        // workDone = true;
-                    }
-
-                    // Save state
-                    if (hasMessage) {
-                        state.lastPushNotification = Date.now();
-                    } else {
-                        await Modules.Push.sendCounterPush(ctx, uid);
-                    }
-
-                    log.debug(ctx, 'updated ' + state.lastPushSeq + '->' + ustate.seq);
-
-                    state.lastPushSeq = ustate.seq;
-                    Modules.Messaging.needNotificationDelivery.resetNeedNotificationDelivery(ctx, 'push', uid);
-                }
-            });
-        }
+        await Promise.all(batches.map(b => inTx(parent, async (c) => {
+            await Promise.all(b.map(uid => handleUser(c, uid)));
+        })));
     });
 }
