525ae286bdf4e4b6971f56cb401217bd0c6927fb
Steve Korshakov <steve@korshakov.com>
2019-08-24 12:01:31 -0700

    fix(video): Trying to fix video service endpoint

--- packages/openland-module-video/generateVideo.ts
+++ packages/openland-module-video/generateVideo.ts
@@ -1,6 +1,6 @@
 import fetch, { Request } from 'node-fetch';
 export async function generateVideo(name: string, args: any) {
-    let res = await fetch(new Request('http://openland-videos-service/create', {
+    let res = await fetch(new Request('http://openland-videos-service.default.svc/create', {
         method: 'POST',
         headers: ['Content-Type: application/json'],
         body: JSON.stringify({ name, arguments: args })
