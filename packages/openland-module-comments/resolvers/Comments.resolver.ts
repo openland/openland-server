ae1368cc3dc3231d4c9f4ea041f5e9e69b6711d3
altox <xeroxaltox@gmail.com>
2019-09-18 19:29:23 +0300

    wip(comments): universal subscribeToComments & unsubscribeFromComments mutations

--- packages/openland-module-comments/resolvers/Comments.resolver.ts
+++ packages/openland-module-comments/resolvers/Comments.resolver.ts
@@ -76,127 +76,6 @@ export default {
     },
 
     Mutation: {
-        addMessageComment: withUser(async (ctx, args, uid) => {
-            let messageId = IDs.ConversationMessage.parse(args.messageId);
-            let replyToComment = args.replyComment ? IDs.Comment.parse(args.replyComment) : null;
-
-            let spans: CommentSpan[] = [];
-
-            //
-            // Mentions
-            //
-            if (args.mentions) {
-                let mentions: CommentSpan[] = [];
-
-                for (let mention of args.mentions) {
-                    if (mention.userId) {
-                        mentions.push({
-                            type: 'user_mention',
-                            offset: mention.offset,
-                            length: mention.length,
-                            user: IDs.User.parse(mention.userId!)
-                        });
-                    } else if (mention.chatId) {
-                        mentions.push({
-                            type: 'room_mention',
-                            offset: mention.offset,
-                            length: mention.length,
-                            room: IDs.Conversation.parse(mention.chatId!)
-                        });
-                    } else if (mention.userIds) {
-                        mentions.push({
-                            type: 'multi_user_mention',
-                            offset: mention.offset,
-                            length: mention.length,
-                            users: mention.userIds.map(id => IDs.User.parse(id))
-                        });
-                    } else if (mention.all) {
-                        mentions.push({
-                            type: 'all_mention',
-                            offset: mention.offset,
-                            length: mention.length,
-                        });
-                    }
-                }
-
-                spans.push(...mentions);
-            }
-
-            //
-            // File attachments
-            //
-            let attachments: MessageAttachmentFileInput[] = [];
-            if (args.fileAttachments) {
-                for (let fileInput of args.fileAttachments) {
-                    let fileMetadata = await Modules.Media.saveFile(ctx, fileInput.fileId);
-                    let filePreview: string | null = null;
-
-                    if (fileMetadata.isImage) {
-                        filePreview = await Modules.Media.fetchLowResPreview(ctx, fileInput.fileId);
-                    }
-
-                    attachments.push({
-                        type: 'file_attachment',
-                        fileId: fileInput.fileId,
-                        fileMetadata: fileMetadata || null,
-                        filePreview: filePreview || null
-                    });
-                }
-            }
-
-            //
-            //  Spans
-            //
-            if (args.spans) {
-                for (let span of args.spans) {
-                    if (span.type === 'Bold') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'bold_text' });
-                    } else if (span.type === 'Italic') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'italic_text' });
-                    } else if (span.type === 'InlineCode') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'inline_code_text' });
-                    } else if (span.type === 'CodeBlock') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'code_block_text' });
-                    } else if (span.type === 'Irony') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'irony_text' });
-                    } else if (span.type === 'Insane') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'insane_text' });
-                    } else if (span.type === 'Loud') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'loud_text' });
-                    } else if (span.type === 'Rotating') {
-                        spans.push({ offset: span.offset, length: span.length, type: 'rotating_text' });
-                    }
-                }
-            }
-
-            await Modules.Comments.addMessageComment(ctx, messageId, uid, {
-                message: args.message,
-                replyToComment,
-                attachments,
-                spans
-            });
-            return true;
-        }),
-        betaAddMessageComment: withUser(async (ctx, args, uid) => {
-            let messageId = IDs.ConversationMessage.parse(args.messageId);
-            let replyToComment = args.replyComment ? IDs.Comment.parse(args.replyComment) : null;
-
-            return await Modules.Comments.addMessageComment(ctx, messageId, uid, {
-                ...(await resolveRichMessageCreation(ctx, args)),
-                replyToComment,
-                repeatKey: args.repeatKey,
-            });
-        }),
-        betaAddFeedComment: withUser(async (ctx, args, uid) => {
-            let itemId = IDs.FeedItem.parse(args.feedItemId);
-            let replyToComment = args.replyComment ? IDs.Comment.parse(args.replyComment) : null;
-
-            return await Modules.Comments.addFeedItemComment(ctx, itemId, uid, {
-                ...(await resolveRichMessageCreation(ctx, args)),
-                replyToComment,
-                repeatKey: args.repeatKey,
-            });
-        }),
         betaAddComment: withUser(async (ctx, args, uid) => {
             let id = IdsFactory.resolve(args.peerId);
             let peerId: number | null;
@@ -368,6 +247,167 @@ export default {
             return true;
         }),
 
+        subscribeToComments: withUser(async (ctx, args, uid) => {
+            let id = IdsFactory.resolve(args.peerId);
+            let peerId: number | null;
+            let peerType: 'message' | 'feed_item' | null;
+
+            if (id.type === IDs.ConversationMessage) {
+                peerId = id.id as number;
+                peerType = 'message';
+            } else if (id.type === IDs.FeedItem) {
+                peerId = id.id as number;
+                peerType = 'feed_item';
+            } else {
+                throw new UserError('Unknown peer');
+            }
+
+            await Modules.Comments.subscribeToComments(ctx, peerType, peerId, uid, args.type.toLowerCase() as any);
+            return true;
+        }),
+        unsubscribeFromComments: withUser(async (ctx, args, uid) => {
+            let id = IdsFactory.resolve(args.peerId);
+            let peerId: number | null;
+            let peerType: 'message' | 'feed_item' | null;
+
+            if (id.type === IDs.ConversationMessage) {
+                peerId = id.id as number;
+                peerType = 'message';
+            } else if (id.type === IDs.FeedItem) {
+                peerId = id.id as number;
+                peerType = 'feed_item';
+            } else {
+                throw new UserError('Unknown peer');
+            }
+
+            await Modules.Comments.unsubscribeFromComments(ctx, peerType, peerId, uid);
+            return true;
+        }),
+
+        //
+        //  Deprecated
+        //
+        addMessageComment: withUser(async (ctx, args, uid) => {
+            let messageId = IDs.ConversationMessage.parse(args.messageId);
+            let replyToComment = args.replyComment ? IDs.Comment.parse(args.replyComment) : null;
+
+            let spans: CommentSpan[] = [];
+
+            //
+            // Mentions
+            //
+            if (args.mentions) {
+                let mentions: CommentSpan[] = [];
+
+                for (let mention of args.mentions) {
+                    if (mention.userId) {
+                        mentions.push({
+                            type: 'user_mention',
+                            offset: mention.offset,
+                            length: mention.length,
+                            user: IDs.User.parse(mention.userId!)
+                        });
+                    } else if (mention.chatId) {
+                        mentions.push({
+                            type: 'room_mention',
+                            offset: mention.offset,
+                            length: mention.length,
+                            room: IDs.Conversation.parse(mention.chatId!)
+                        });
+                    } else if (mention.userIds) {
+                        mentions.push({
+                            type: 'multi_user_mention',
+                            offset: mention.offset,
+                            length: mention.length,
+                            users: mention.userIds.map(id => IDs.User.parse(id))
+                        });
+                    } else if (mention.all) {
+                        mentions.push({
+                            type: 'all_mention',
+                            offset: mention.offset,
+                            length: mention.length,
+                        });
+                    }
+                }
+
+                spans.push(...mentions);
+            }
+
+            //
+            // File attachments
+            //
+            let attachments: MessageAttachmentFileInput[] = [];
+            if (args.fileAttachments) {
+                for (let fileInput of args.fileAttachments) {
+                    let fileMetadata = await Modules.Media.saveFile(ctx, fileInput.fileId);
+                    let filePreview: string | null = null;
+
+                    if (fileMetadata.isImage) {
+                        filePreview = await Modules.Media.fetchLowResPreview(ctx, fileInput.fileId);
+                    }
+
+                    attachments.push({
+                        type: 'file_attachment',
+                        fileId: fileInput.fileId,
+                        fileMetadata: fileMetadata || null,
+                        filePreview: filePreview || null
+                    });
+                }
+            }
+
+            //
+            //  Spans
+            //
+            if (args.spans) {
+                for (let span of args.spans) {
+                    if (span.type === 'Bold') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'bold_text' });
+                    } else if (span.type === 'Italic') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'italic_text' });
+                    } else if (span.type === 'InlineCode') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'inline_code_text' });
+                    } else if (span.type === 'CodeBlock') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'code_block_text' });
+                    } else if (span.type === 'Irony') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'irony_text' });
+                    } else if (span.type === 'Insane') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'insane_text' });
+                    } else if (span.type === 'Loud') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'loud_text' });
+                    } else if (span.type === 'Rotating') {
+                        spans.push({ offset: span.offset, length: span.length, type: 'rotating_text' });
+                    }
+                }
+            }
+
+            await Modules.Comments.addMessageComment(ctx, messageId, uid, {
+                message: args.message,
+                replyToComment,
+                attachments,
+                spans
+            });
+            return true;
+        }),
+        betaAddMessageComment: withUser(async (ctx, args, uid) => {
+            let messageId = IDs.ConversationMessage.parse(args.messageId);
+            let replyToComment = args.replyComment ? IDs.Comment.parse(args.replyComment) : null;
+
+            return await Modules.Comments.addMessageComment(ctx, messageId, uid, {
+                ...(await resolveRichMessageCreation(ctx, args)),
+                replyToComment,
+                repeatKey: args.repeatKey,
+            });
+        }),
+        betaAddFeedComment: withUser(async (ctx, args, uid) => {
+            let itemId = IDs.FeedItem.parse(args.feedItemId);
+            let replyToComment = args.replyComment ? IDs.Comment.parse(args.replyComment) : null;
+
+            return await Modules.Comments.addFeedItemComment(ctx, itemId, uid, {
+                ...(await resolveRichMessageCreation(ctx, args)),
+                replyToComment,
+                repeatKey: args.repeatKey,
+            });
+        }),
         subscribeMessageComments: withUser(async (ctx, args, uid) => {
             let messageId = IDs.ConversationMessage.parse(args.messageId);
             await Modules.Comments.subscribeToComments(ctx, 'message', messageId, uid, args.type.toLowerCase() as any);
