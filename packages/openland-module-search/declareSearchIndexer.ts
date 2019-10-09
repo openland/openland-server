6abe07f60c2a1465f4ba3fbf683092ab3ec28c01
altox <xeroxaltox@gmail.com>
2019-10-09 18:52:14 +0300

    wip(server): reduce logs

--- packages/openland-module-search/declareSearchIndexer.ts
+++ packages/openland-module-search/declareSearchIndexer.ts
@@ -90,7 +90,7 @@ export class SearchIndexer<T, P extends SearchIndexerProperties> {
                         body: converted,
                     });
                     if (res.errors) {
-                        log.warn(ctx, 'Elastic error', JSON.stringify(res), JSON.stringify(converted));
+                        log.warn(ctx, 'Elastic error', JSON.stringify(res));
                         throw new Error('Error during indexing (' + this.name + ')');
                     }
                 } catch (e) {
