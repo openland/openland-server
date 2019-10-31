0a3e300fe8f283a706719ada9b6fa54fe018c940
kor_ka <gleb.putintsev@openland.com>
2019-10-31 11:38:37 +0300

    chore(server): debug google auth audience

--- packages/openland-module-auth/providers/google.ts
+++ packages/openland-module-auth/providers/google.ts
@@ -4,6 +4,9 @@ import express from 'express';
 import { Modules } from '../../openland-modules/Modules';
 import { createNamedContext } from '@openland/context';
 import { OAuth2Client } from 'google-auth-library';
+import { createLogger } from '@openland/log';
+
+const logger = createLogger('google-auth');
 
 const rootCtx = createNamedContext('auth-email');
 var client: OAuth2Client;
@@ -23,6 +26,11 @@ const sendError = (response: express.Response, error: ErrorsEnum) => {
     response.json({ ok: false, errorCode: error, errorText: Errors[error] });
 };
 
+// temp just for debug
+function decodeBase64StringUtf8(base64: string): string {
+    return Buffer.from(base64, 'base64').toString('utf-8');
+}
+
 export async function getAccessToken(req: express.Request, response: express.Response) {
     let {
         idToken
@@ -38,14 +46,21 @@ export async function getAccessToken(req: express.Request, response: express.Res
         client = new OAuth2Client(clientIdWeb);
     }
 
+    const audience = [
+        clientIdWeb,
+        await Modules.Super.getEnvVar<string>(rootCtx, 'auth-google-android-client-id-debug') || '',
+        await Modules.Super.getEnvVar<string>(rootCtx, 'auth-google-android-client-id') || '',
+        // TODO: add ios client_id
+    ];
+
+    // debug aud
+    const segments = idToken!.split('.');
+    logger.debug(rootCtx, 'payload', decodeBase64StringUtf8(segments[1]));
+    logger.debug(rootCtx, 'audience', audience);
+
     const ticket = await client.verifyIdToken({
         idToken,
-        audience: [
-            clientIdWeb,
-            await Modules.Super.getEnvVar<string>(rootCtx, 'auth-google-android-client-id-debug') || '',
-            await Modules.Super.getEnvVar<string>(rootCtx, 'auth-google-android-client-id') || '',
-            // TODO: add ios client_id
-        ],
+        audience
     });
     const payload = ticket.getPayload();
 
