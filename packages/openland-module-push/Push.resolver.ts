55eaa1c7e13aebc791b5bf2d6dd73fa7da2daa49
mrmld <die.marmelade@yandex.ru>
2019-11-14 16:47:30 +0300

    wip(server): debug data push android

--- packages/openland-module-push/Push.resolver.ts
+++ packages/openland-module-push/Push.resolver.ts
@@ -3,6 +3,8 @@ import { PushConfig } from './PushConfig';
 import { AppContext } from 'openland-modules/AppContext';
 import { GQLResolver } from '../openland-module-api/schema/SchemaSpec';
 import { createLogger } from '@openland/log';
+import { withPermission } from '../openland-module-api/Resolvers';
+import { IDs } from '../openland-module-api/IDs';
 
 const pushLog = createLogger('push');
 
@@ -45,6 +47,23 @@ export default {
                 return 'ok';
             }
             throw Error('Unknown type: ' + args.type);
-        }
+        },
+        debugSendAndroidDataPush: withPermission('super-admin',  async (ctx, args) => {
+            let uid = IDs.User.parse(args.uid);
+            let androidTokens = await Modules.Push.repository.getUserAndroidPushTokens(ctx, uid);
+            for (let token of androidTokens) {
+                await Modules.Push.androidWorker.pushWork(ctx, {
+                    uid: uid,
+                    tokenId: token.id,
+                    collapseKey: 'test',
+                    data: {
+                        ['title']: 'Test data push',
+                        ['message']: args.message,
+                        ['soundName']: 'default',
+                    },
+                });
+            }
+            return true;
+        })
     }
 } as GQLResolver;
