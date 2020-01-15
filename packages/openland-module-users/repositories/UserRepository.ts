1c6765dbc9ec421909310490fa959a20448cbea8
mrmld <die.marmelade@yandex.ru>
2020-01-15 13:37:14 +0300

    wip(server): disable waitlisting

--- packages/openland-module-users/repositories/UserRepository.ts
+++ packages/openland-module-users/repositories/UserRepository.ts
@@ -39,7 +39,7 @@ export class UserRepository {
                 authId: authId,
                 email: email.toLowerCase(),
                 isBot: false,
-                status: 'pending',
+                status: 'activated',
                 invitedBy: null,
                 isSuperBot: null,
                 botOwner: null,
