80c74969f9e4b2ae36b72cd72f0ec886daa4cb80
altox <xeroxaltox@gmail.com>
2020-02-14 00:19:43 +0300

    wip(server): fix GeoPowerup resolver

--- packages/openland-module-geo-powerup/GeoPowerup.resolver.ts
+++ packages/openland-module-geo-powerup/GeoPowerup.resolver.ts
@@ -11,6 +11,7 @@ import { AuthContext } from '../openland-module-auth/AuthContext';
 import { EventBus } from '../openland-module-pubsub/EventBus';
 import { PowerupChatUserSettings } from '../openland-module-powerups/PowerupsRepository';
 import { combineAsyncIterators } from '../openland-utils/combineAsyncIterators';
+import { isDefined } from '../openland-utils/misc';
 
 const getGeoPowerupId = (ctx: Context) => Modules.Super.getEnvVar<number>(ctx, 'geo-powerup-id');
 const getPermissionRequestInfo = (ctx: Context, appId: number, cid: number, uid: number): PermissionRequestInfo => {
@@ -24,7 +25,7 @@ const getPermissionRequestInfo = (ctx: Context, appId: number, cid: number, uid:
     };
 };
 
-export default {
+export const Resolver: GQLResolver = {
     Query: {
         chatLocations: withActivatedUser(async (ctx, args, uid) => {
             let cid = IDs.Conversation.parse(args.id);
@@ -49,7 +50,7 @@ export default {
             members = members.filter(a => membersSettings[a].enabled);
             let memberLocations = await Promise.all(members.map(a => Modules.Geo.getUserGeo(ctx, a, getPermissionRequestInfo(ctx, pid!, cid, a))));
 
-            return memberLocations.filter(a => !!a);
+            return memberLocations.filter(isDefined);
         })
     },
     Subscription: {
@@ -90,4 +91,4 @@ export default {
             },
         },
     },
-} as GQLResolver;
+};
