6434eed64c773a3d9acc02744238414e6c9ba321
altox <xeroxaltox@gmail.com>
2020-02-14 03:54:28 +0300

    wip(server): fix Videos.resolver.ts

--- packages/openland-module-video/Videos.resolver.ts
+++ packages/openland-module-video/Videos.resolver.ts
@@ -2,10 +2,10 @@ import { GQLResolver } from '../openland-module-api/schema/SchemaSpec';
 import { withUser } from 'openland-module-api/Resolvers';
 import { generateVideo } from './generateVideo';
 
-export default {
+export const Resolver: GQLResolver = {
     Mutation: {
         alphaRenderVideo: withUser(async (ctx, args, uid) => {
             return await generateVideo(args.name, {});
         })
     }
-} as GQLResolver;
+};
