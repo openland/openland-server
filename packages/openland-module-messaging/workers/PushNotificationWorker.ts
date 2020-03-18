fdcdac0a4b3478c0731120ecf25abb87df236f1d
altox <xeroxaltox@gmail.com>
2020-03-18 18:38:03 +0300

    fix(server): log push error

--- packages/openland-module-messaging/workers/PushNotificationWorker.ts
+++ packages/openland-module-messaging/workers/PushNotificationWorker.ts
@@ -238,9 +238,13 @@ export function startPushNotificationWorker() {
         let batches = batch(unreadUsers, 10);
 
         for (let b of batches) {
-            await inTx(rootCtx, async ctx => {
-                await Promise.all(b.map(uid => handleUser(ctx, uid)));
-            });
+            try {
+                await inTx(rootCtx, async ctx => {
+                    await Promise.all(b.map(uid => handleUser(ctx, uid)));
+                });
+            } catch (e) {
+                log.log(rootCtx, 'push_error', e);
+            }
         }
     });
 }
