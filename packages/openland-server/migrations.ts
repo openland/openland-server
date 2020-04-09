bf57d4250ab571bfbacd7589aef258bc8532acf7
kor_ka <gleb@openland.com>
2020-04-09 22:07:56 +0300

    fix(calls): fix conferences

--- packages/openland-server/migrations.ts
+++ packages/openland-server/migrations.ts
@@ -510,4 +510,24 @@ migrations.push({
     }
 });
 
+migrations.push({
+    key: '122-fix-conference',
+    migration: async (parent) => {
+        let data = await inTx(parent, ctx => Store.ConferenceRoom.findAll(ctx));
+        for (let cursor = 0; cursor < data.length; cursor += 100) {
+            let batch = data.slice(cursor, cursor + 100);
+            await inTx(parent, async ctx => {
+                for (let key of batch) {
+                    let item = (await Store.ConferenceRoom.findById(ctx, key.id))!;
+                    if (item) {
+                        item.strategy = 'mash';
+                        item.kind = 'conference';
+                        await item.flush(ctx);
+                    }
+                }
+            });
+        }
+    }
+});
+
 export default migrations;
