12b0d9281113a2531127050d2aba4c74444fabb1
altox <xeroxaltox@gmail.com>
2020-04-12 02:31:53 +0300

    wip(server): disable concurrent pool for a while

--- packages/openland-module-api/buildConcurrencyPool.ts
+++ packages/openland-module-api/buildConcurrencyPool.ts
@@ -1,18 +1,19 @@
 import { AppContext } from 'openland-modules/AppContext';
-import { ConcurrencyPool, UnboundedConcurrencyPool, BoundedConcurrencyPoool } from 'openland-utils/ConcurrencyPool';
+import { ConcurrencyPool, UnboundedConcurrencyPool } from 'openland-utils/ConcurrencyPool';
 
-let pools = new Map<String, ConcurrencyPool>();
+// let pools = new Map<String, ConcurrencyPool>();
 
 export function buildConcurrencyPool(ctx: AppContext): ConcurrencyPool {
-    if (ctx.auth.tid) {
-        let tid = ctx.auth.tid;
-        if (!pools.has(tid)) {
-            let res = new BoundedConcurrencyPoool(30);
-            pools.set(tid, res);
-            return res;
-        }
-        return pools.get(tid)!;
-    } else {
-        return UnboundedConcurrencyPool;
-    }
+    return UnboundedConcurrencyPool;
+    // if (ctx.auth.tid) {
+    //     let tid = ctx.auth.tid;
+    //     if (!pools.has(tid)) {
+    //         let res = new BoundedConcurrencyPoool(30);
+    //         pools.set(tid, res);
+    //         return res;
+    //     }
+    //     return pools.get(tid)!;
+    // } else {
+    //     return UnboundedConcurrencyPool;
+    // }
 }
