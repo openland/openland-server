7e98444dc525b409cb9d8a622d994685cf232657
altox <xeroxaltox@gmail.com>
2020-04-12 02:46:47 +0300

    wip(server): enable back concurrent pool

--- packages/openland-module-api/buildConcurrencyPool.ts
+++ packages/openland-module-api/buildConcurrencyPool.ts
@@ -1,19 +1,18 @@
 import { AppContext } from 'openland-modules/AppContext';
-import { ConcurrencyPool, UnboundedConcurrencyPool } from 'openland-utils/ConcurrencyPool';
+import { ConcurrencyPool, UnboundedConcurrencyPool, BoundedConcurrencyPoool } from 'openland-utils/ConcurrencyPool';
 
-// let pools = new Map<String, ConcurrencyPool>();
+let pools = new Map<String, ConcurrencyPool>();
 
 export function buildConcurrencyPool(ctx: AppContext): ConcurrencyPool {
-    return UnboundedConcurrencyPool;
-    // if (ctx.auth.tid) {
-    //     let tid = ctx.auth.tid;
-    //     if (!pools.has(tid)) {
-    //         let res = new BoundedConcurrencyPoool(30);
-    //         pools.set(tid, res);
-    //         return res;
-    //     }
-    //     return pools.get(tid)!;
-    // } else {
-    //     return UnboundedConcurrencyPool;
-    // }
+    if (ctx.auth.tid) {
+        let tid = ctx.auth.tid;
+        if (!pools.has(tid)) {
+            let res = new BoundedConcurrencyPoool(30);
+            pools.set(tid, res);
+            return res;
+        }
+        return pools.get(tid)!;
+    } else {
+        return UnboundedConcurrencyPool;
+    }
 }
