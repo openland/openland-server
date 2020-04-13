0ed505eb16afc7f9a35cb60b0233b7804fd78ba1
Steve Korshakov <steve@korshakov.com>
2020-04-12 19:31:22 -0700

    feat: Add mediakitchen and nats

--- packages/openland-module-calls/CallsModule.ts
+++ packages/openland-module-calls/CallsModule.ts
@@ -3,6 +3,8 @@ import { lazyInject } from 'openland-modules/Modules.container';
 import { CallRepository } from './repositories/CallRepository';
 import { serverRoleEnabled } from 'openland-utils/serverRoleEnabled';
 import { startCallReaper } from './worker/startCallReaper';
+import { connectToCluster, Cluster } from 'mediakitchen';
+import { Client } from 'ts-nats';
 
 @injectable()
 export class CallsModule {
@@ -10,7 +12,14 @@ export class CallsModule {
     @lazyInject(CallRepository)
     repo!: CallRepository;
 
+    @lazyInject('NATS')
+    nats!: Client;
+
+    cluster!: Cluster;
+
     start = async () => {
+        this.cluster = await connectToCluster({ nc: this.nats });
+        
         if (serverRoleEnabled('workers')) {
             startCallReaper();
         }
