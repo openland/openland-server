58f41799918a0833dd96881ed7e4462c497f46fc
Steve Korshakov <steve@korshakov.com>
2020-05-10 10:33:09 -0700

    wip(metrics): Remove amplitude indexing, add clickhouse to dev deploy

--- packages/openland-module-hyperlog/HyperlogModule.ts
+++ packages/openland-module-hyperlog/HyperlogModule.ts
@@ -1,14 +1,12 @@
 import { declareHyperlogIndexer } from './workers/declareHyperlogIndexer';
 import { serverRoleEnabled } from 'openland-utils/serverRoleEnabled';
 import { injectable } from 'inversify';
-import { declareBatchAmplitudeIndexer } from './workers/declareBatchAmplitudeIndexer';
 
 @injectable()
 export class HyperlogModule {
     start = () => {
         if (serverRoleEnabled('admin')) {
             declareHyperlogIndexer();
-            declareBatchAmplitudeIndexer();
         }
     }
 }
