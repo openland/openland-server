6809b8feabfb92108aa1c6f68bcbc5c5c7ca6b8d
Steve Korshakov <steve@korshakov.com>
2020-05-10 21:12:38 -0700

    perf(mon): Faster hyperlog export

--- packages/openland-module-hyperlog/clickhouse/startExporters.ts
+++ packages/openland-module-hyperlog/clickhouse/startExporters.ts
@@ -6,7 +6,7 @@ import { backoff } from 'openland-utils/timer';
 import { createClient } from './migrations';
 
 function startSimpleExport(client: DatabaseClient) {
-    updateReader('ch-exporter-reader', 3, Store.HyperLog.created.stream(), async (src, first, ctx) => {
+    updateReader('ch-exporter-reader', 3, Store.HyperLog.created.stream({ batchSize: 5000 }), async (src, first, ctx) => {
         let presences = src.filter((v) => v.type === 'presence' && v.body.online === true);
         if (presences.length > 0) {
             await client.insert(ctx, 'presences', ['time', 'eid', 'uid', 'platform'], src.map((v) => [Math.round(v.date / 1000), v.id, v.body.uid, v.body.platform]));
