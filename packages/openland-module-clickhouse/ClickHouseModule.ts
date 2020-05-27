3d3cd6c075b28cbf8069bc53542b0bf8b4bb9335
mrmld <die.marmelade@yandex.ru>
2020-05-27 22:26:26 +0300

    wip(clickhouse): remove test table

--- packages/openland-module-clickhouse/ClickHouseModule.ts
+++ packages/openland-module-clickhouse/ClickHouseModule.ts
@@ -2,10 +2,6 @@ import { injectable } from 'inversify';
 import { startExporters } from './startExporters';
 import { createNamedContext } from '@openland/context';
 import { serverRoleEnabled } from '../openland-utils/serverRoleEnabled';
-import { Table } from './schema/Table';
-import { backoff } from '../openland-utils/timer';
-import { createClient } from './migrations';
-import { boolean, integer, schema, string } from './schema';
 
 let ctx = createNamedContext('clickhouse');
 @injectable()
@@ -13,19 +9,6 @@ export class ClickHouseModule {
     start = async () => {
         if (serverRoleEnabled('admin')) {
             startExporters(ctx);
-            let client = await backoff(ctx, () => createClient(ctx));
-            const testTable = new Table(client, 'test2', schema({
-                id: integer(),
-                lol: string(),
-                kek: integer(),
-                flex: boolean(),
-            }), {
-                id: 'id',
-                orderBy: 'id',
-                partition: 'id'
-            });
-
-            await testTable.init(ctx);
         }
     }
 }
