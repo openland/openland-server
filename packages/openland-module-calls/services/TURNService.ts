4ae4ebf2a79845a45a842ac69675d9250767aaf7
mrmld <die.marmelade@yandex.ru>
2020-05-29 00:54:43 +0300

    wip(server): turn autodiscover

--- packages/openland-module-calls/services/TURNService.ts
+++ packages/openland-module-calls/services/TURNService.ts
@@ -1,37 +1,48 @@
-import { Config } from 'openland-config/Config';
-import Twilio from 'twilio';
+import { Modules } from '../../openland-modules/Modules';
+// import { Config } from 'openland-config/Config';
+// import Twilio from 'twilio';
 
-let twillioApi = Twilio(Config.twillio.sid, Config.twillio.token);
-let iceServers: any | undefined = undefined;
-let iceServerExpire: number | undefined = undefined;
+// let twillioApi = Twilio(Config.twillio.sid, Config.twillio.token);
+// let iceServers: any | undefined = undefined;
+// let iceServerExpire: number | undefined = undefined;
 
 export async function resolveTurnServices() {
-    if (twillioApi && Config.environment === 'production') {
-        let now = Date.now();
-        if (iceServers) {
-            if (now < iceServerExpire!) {
-                return iceServers;
-            }
-        }
-        let res = await twillioApi.tokens.create();
-        let iceServersRaw = res.iceServers as any as [{ url: string, username?: string, credential?: string }];
-        iceServers = iceServersRaw.map((v) => ({
-            urls: [v.url],
-            username: v.username,
-            credential: v.credential
-        }));
-        iceServerExpire = now + ((parseInt(res.ttl, 10)) * 1000 / 2);
-        return iceServers;
-    }
-    return [{
-        ip: '35.228.24.228',
-        urls: ['turn:35.228.24.228:3478'],
+    // if (twillioApi && Config.environment === 'production') {
+    //     let now = Date.now();
+    //     if (iceServers) {
+    //         if (now < iceServerExpire!) {
+    //             return iceServers;
+    //         }
+    //     }
+    //     let res = await twillioApi.tokens.create();
+    //     let iceServersRaw = res.iceServers as any as [{ url: string, username?: string, credential?: string }];
+    //     iceServers = iceServersRaw.map((v) => ({
+    //         urls: [v.url],
+    //         username: v.username,
+    //         credential: v.credential
+    //     }));
+    //     iceServerExpire = now + ((parseInt(res.ttl, 10)) * 1000 / 2);
+    //     return iceServers;
+    // }
+
+    let kitchenIceServers = Modules.Calls.mediaKitchen.cluster.workers.map(a => ({
+        ip: a.appData.ip,
+        urls: ['turn:' + a.appData.ip + ':3478'],
         username: 'user',
         credential: 'emFsdXBhCg',
-    }, {
-        ip: '35.245.151.83',
-        urls: ['turn:35.245.151.83:3478'],
-        username: 'user',
-        credential: 'cGl6ZGFyaWsK',
-    }];
+    }));
+
+    return [
+        ...kitchenIceServers, {
+            ip: '35.228.24.228',
+            urls: ['turn:35.228.24.228:3478'],
+            username: 'user',
+            credential: 'emFsdXBhCg',
+        }, {
+            ip: '35.245.151.83',
+            urls: ['turn:35.245.151.83:3478'],
+            username: 'user',
+            credential: 'cGl6ZGFyaWsK',
+        }
+    ];
 }
