4393cf9e02aff1c57fdd816f2b1c5ec3ab473868
Steve Korshakov <steve@korshakov.com>
2020-06-05 15:35:47 -0700

    fix(calls): Fail safe kitchen worker

--- packages/openland-module-calls/worker/declareRouterDeleteWorker.ts
+++ packages/openland-module-calls/worker/declareRouterDeleteWorker.ts
@@ -5,42 +5,47 @@ import { MediaKitchenService } from '../kitchen/MediaKitchenService';
 
 export function declareRouterDeleteWorker(service: MediaKitchenService, repo: MediaKitchenRepository) {
     repo.
-    routerDeleteQueue.addWorker(async (args, parent) => {
-        let router = await inTx(parent, async (ctx) => {
-            let r = await Store.KitchenRouter.findById(ctx, args.id);
-            if (!r) {
-                throw Error('Unable to find router');
-            }
+        routerDeleteQueue.addWorker(async (args, parent) => {
+            let router = await inTx(parent, async (ctx) => {
+                let r = await Store.KitchenRouter.findById(ctx, args.id);
+                if (!r) {
+                    throw Error('Unable to find router');
+                }
 
-            // Fast exit if no workers was assigned
-            if (!r.workerId) {
-                r.state = 'deleted';
-            }
-            return r;
-        });
-        if (router.state === 'deleted') {
-            return;
-        }
-
-        // Doing "creating" with an existing retry key to get instance
-        let rawRouter = await service.getOrCreateRouter(router.workerId!, router.id);
-        if (rawRouter.closed) {
-            return;
-        }
-
-        // Closing router
-        await rawRouter.close();
-
-        await inTx(parent, async (ctx) => {
-            let r = await Store.KitchenRouter.findById(ctx, args.id);
-            if (!r) {
-                throw Error('Unable to find router');
-            }
-            if (r.state !== 'deleting') {
+                // Fast exit if no workers was assigned
+                if (!r.workerId) {
+                    r.state = 'deleted';
+                }
+                return r;
+            });
+            if (router.state === 'deleted') {
                 return;
             }
-            r.state = 'deleted';
-            await repo.onRouterRemoved(ctx, r.id);
+
+            // Doing "creating" with an existing retry key to get instance
+            let worker = await Store.KitchenWorker.findById(parent, router.workerId!);
+            if (worker) {
+                if (!worker.deleted) {
+                    if (service.cluster.workers.find((v) => v.id === router.workerId!)) {
+                        let rawRouter = await service.getOrCreateRouter(router.workerId!, router.id);
+                        if (!rawRouter.closed) {
+                            // Closing router
+                            await rawRouter.close();
+                        }
+                    }
+                }
+            }
+
+            await inTx(parent, async (ctx) => {
+                let r = await Store.KitchenRouter.findById(ctx, args.id);
+                if (!r) {
+                    throw Error('Unable to find router');
+                }
+                if (r.state !== 'deleting') {
+                    return;
+                }
+                r.state = 'deleted';
+                await repo.onRouterRemoved(ctx, r.id);
+            });
         });
-    });
 }
