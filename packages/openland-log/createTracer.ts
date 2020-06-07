23fc0485f98c7b077ab1c6e180bd01e840551eb7
Steve Korshakov <steve@korshakov.com>
2020-06-07 11:29:10 -0700

    fix(apm): Hotfix for APM

--- packages/openland-log/createTracer.ts
+++ packages/openland-log/createTracer.ts
@@ -1,4 +1,5 @@
-import APM from 'elastic-apm-node';
+import { apm } from './apm';
+// import APM from 'elastic-apm-node';
 import { Config } from 'openland-config/Config';
 import { STracer } from './STracer';
 import { OpenTracer } from './src/OpenTracer';
@@ -7,12 +8,12 @@ const Tracer = require('elastic-apm-node-opentracing');
 
 export function createTracer(name: string): STracer {
     if (Config.environment === 'production' && Config.enableTracing && Config.apm) {
-        const apm = APM.start({
-            serviceName: name,
-            serverUrl: Config.apm.endpoint,
-            active: true,
-            instrument: false
-        });
+        // const apm = APM.start({
+        //     serviceName: name,
+        //     serverUrl: Config.apm.endpoint,
+        //     active: true,
+        //     instrument: false
+        // });
         return new OpenTracer(new Tracer(apm));
     } else {
         return new NoOpTracer();
