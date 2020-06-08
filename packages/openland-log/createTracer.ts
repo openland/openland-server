f2a5c83a9fd5ab5c158ae9afc8b0fd8817570361
Steve Korshakov <steve@korshakov.com>
2020-06-08 00:01:48 -0700

    debug(mon): Remove manual span logging, add env variables logging

--- packages/openland-log/createTracer.ts
+++ packages/openland-log/createTracer.ts
@@ -11,11 +11,19 @@ const ctx = createNamedContext('reporter');
 
 export function createTracer(name: string): STracer {
     if (Config.environment === 'production' && Config.enableTracing) {
+        logger.log(ctx, 'Declared tracer ' + name + ' ' + JSON.stringify({
+            endpoint: process.env.JAEGER_ENDPOINT || null,
+            agentHost: process.env.JAEGER_AGENT_HOST || null,
+            agentPort: process.env.JAEGER_AGENT_PORT || null
+        }));
         return new OpenTracer(initTracer({
             serviceName: name,
             sampler: { type: 'const', param: 1 },
             reporter: {
-                logSpans: true
+                logSpans: false,
+                collectorEndpoint: process.env.JAEGER_ENDPOINT ? process.env.JAEGER_ENDPOINT : undefined,
+                agentHost: process.env.JAEGER_AGENT_HOST ? process.env.JAEGER_AGENT_HOST : undefined,
+                agentPort: process.env.JAEGER_AGENT_PORT ? parseInt(process.env.JAEGER_AGENT_PORT, 10) : undefined
             }
         }, {
             logger: {
