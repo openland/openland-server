1ece8556e86e8b1f3bbb3852e6e430f5fb6a0a94
Steve Korshakov <steve@korshakov.com>
2020-06-08 22:02:20 -0700

    feat(mon): Add total users metric

--- packages/openland-module-monitoring/Metrics.ts
+++ packages/openland-module-monitoring/Metrics.ts
@@ -4,25 +4,31 @@ import { MetricFactory } from './MetricFactory';
 export const Factory = new MetricFactory();
 
 export const Metrics = {
-    // Distributed gauges
+
+    // SpaceX
     WebSocketConnections: Factory.createMachineGauge('connections', 'Active WebSocket connections'),
     WebSocketPacketsIn: Factory.createFrequencyGauge('ws_in_hz', 'WebSocket incoming packets frequency'),
     WebSocketPacketsOut: Factory.createFrequencyGauge('ws_out_hz', 'WebSocket outgoing packets frequency'),
-
-    GQLRequestTime: Factory.createGauge('gql_request_time', 'Time of GraphQL request resolving', 'median'),
-    GQLRequests: Factory.createMachineGauge('gql_requests', 'Number of parallel graphql requests'),
-
-    // SpaceX
     SpaceXSessions: Factory.createMachineGauge('spacex_sessions', 'Number of active SpaceX sessions'),
     SpaceXSessionsAuthenticated: Factory.createMachineGauge('spacex_sessions_authenticated', 'Number of active authenticated SpaceX sessions'),
     SpaceXSessionsAnonymous: Factory.createMachineGauge('spacex_sessions_anonymous', 'Number of active authenticated SpaceX sessions'),
     SpaceXOperations: Factory.createMachineGauge('spacex_ops', 'Number of active SpaceX operations'),
     SpaceXOperationsFrequence: Factory.createFrequencyGauge('spacex_ops_hz', 'Frequency of SpaceX operations'),
+    SpaceXOperationTime: Factory.createGauge('spacex_req_duration', 'Duration of SpaceX operation'),
     SpaceXSubscriptions: Factory.createMachineGauge('spacex_sub', 'Number of active SpaceX subscriptions'),
     SpaceXSubscriptionEvents: Factory.createFrequencyGauge('spacex_sub_hz', 'Frequency of SpaceX subscription events'),
 
+    // GQL: Obsolete
+    GQLRequestTime: Factory.createGauge('gql_request_time', 'Time of GraphQL request resolving', 'median'),
+    GQLRequests: Factory.createMachineGauge('gql_requests', 'Number of parallel graphql requests'),
+
     // Tracing
     TracingFrequence: Factory.createFrequencyGauge('tracing_span_hz', 'Tracing spans generation frequence'),
+
+    // Basics
+    Users: Factory.createPersistedGauge('users_count', 'Total openland users', async (ctx) => {
+        return (await Store.User.findAllKeys(ctx)).length;
+    }),
 
     // Calls
     CallWorkers: Factory.createPersistedGauge('calls_workers', 'Number of active workers', async (ctx) => {
