3640cb7e626d5272ca08a7e41ba6d07ef38a081b
mrmld <die.marmelade@yandex.ru>
2020-06-15 19:51:00 +0300

    wip(server): disable elastic hyperlog

--- packages/openland-module-hyperlog/workers/declareHyperlogIndexer.ts
+++ packages/openland-module-hyperlog/workers/declareHyperlogIndexer.ts
@@ -1,31 +1,28 @@
-import { Store } from 'openland-module-db/FDB';
-import { declareSearchIndexer } from 'openland-module-search/declareSearchIndexer';
-
 export function declareHyperlogIndexer() {
-    declareSearchIndexer({
-        name: 'hyperlog',
-        version: 1,
-        index: 'hyperlog',
-        stream: Store.HyperLog.created.stream({ batchSize: 5000 }),
-        includedClusters: ['default']
-    }).withProperties({
-        type: {
-            type: 'keyword'
-        },
-        date: {
-            type: 'date'
-        }
-    }).start(async (item) => {
-        let { tid, ...other } = item.body;
-        return {
-            id: item.id!!,
-            doc: {
-                type: item.type,
-                date: item.date,
-                body: {
-                    ...other
-                }
-            }
-        };
-    });
+    // declareSearchIndexer({
+    //     name: 'hyperlog',
+    //     version: 1,
+    //     index: 'hyperlog',
+    //     stream: Store.HyperLog.created.stream({ batchSize: 5000 }),
+    //     includedClusters: ['default']
+    // }).withProperties({
+    //     type: {
+    //         type: 'keyword'
+    //     },
+    //     date: {
+    //         type: 'date'
+    //     }
+    // }).start(async (item) => {
+    //     let { tid, ...other } = item.body;
+    //     return {
+    //         id: item.id!!,
+    //         doc: {
+    //             type: item.type,
+    //             date: item.date,
+    //             body: {
+    //                 ...other
+    //             }
+    //         }
+    //     };
+    // });
 }
