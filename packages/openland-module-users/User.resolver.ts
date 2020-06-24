6918b628cff33c0f9d69403a784cb673900f8291
altox <xeroxaltox@gmail.com>
2020-06-24 19:44:52 +0300

    wip(server)

--- packages/openland-module-users/User.resolver.ts
+++ packages/openland-module-users/User.resolver.ts
@@ -104,45 +104,46 @@ export const Resolver: GQLResolver = {
         photo: withProfile((ctx, src, profile) => profile && profile.picture ? buildBaseImageUrl(profile.picture) : null, true),
         photoRef: withProfile((ctx, src, profile) => profile && profile.picture, true),
 
-        email: withUser(async (ctx, src, authorized) => {
-            if (!authorized) {
-                return null;
-            }
-            if (!src.email) {
-                return null;
-            }
-
-            if (ctx.auth.uid === src.id) {
-                return src.email;
-            }
-
-            let settings = await Modules.Users.getUserSettings(ctx, src.id);
-            if (!settings.privacy || settings.privacy.whoCanSeeEmail === 'everyone') {
-                return src.email;
-            } else {
-                return null;
-            }
-        }, true),
-        phone: withUser(async (ctx, src, authorized) => {
-            if (!authorized) {
-                return null;
-            }
-            if (!src.phone) {
-                return null;
-            }
-
-            if (ctx.auth.uid === src.id) {
-                return src.phone;
-            }
-
-            let settings = await Modules.Users.getUserSettings(ctx, src.id);
-            if (!settings.privacy || settings.privacy.whoCanSeePhone === 'everyone') {
-                return src.phone;
-            } else {
-                return null;
-            }
-        }, true),
-        about: withProfile((ctx, src, profile) => profile ? profile.about : null, true),
+        // email: withUser(async (ctx, src, authorized) => {
+        //     if (!authorized) {
+        //         return null;
+        //     }
+        //     if (!src.email) {
+        //         return null;
+        //     }
+        //
+        //     if (ctx.auth.uid === src.id) {
+        //         return src.email;
+        //     }
+        //
+        //     let settings = await Modules.Users.getUserSettings(ctx, src.id);
+        //     if (!settings.privacy || settings.privacy.whoCanSeeEmail === 'everyone') {
+        //         return src.email;
+        //     } else {
+        //         return null;
+        //     }
+        // }, true),
+        // phone: withUser(async (ctx, src, authorized) => {
+        //     if (!authorized) {
+        //         return null;
+        //     }
+        //     if (!src.phone) {
+        //         return null;
+        //     }
+        //
+        //     if (ctx.auth.uid === src.id) {
+        //         return src.phone;
+        //     }
+        //
+        //     let settings = await Modules.Users.getUserSettings(ctx, src.id);
+        //     if (!settings.privacy || settings.privacy.whoCanSeePhone === 'everyone') {
+        //         return src.phone;
+        //     } else {
+        //         return null;
+        //     }
+        // }, true),
+        email: withProfile((ctx, src, profile) => profile ? profile.email : null, true),
+        phone: withProfile((ctx, src, profile) => profile ? profile.phone : null, true),
         website: withProfile((ctx, src, profile) => profile ? profile.website : null),
         linkedin: withProfile((ctx, src, profile) => profile && profile.linkedin),
         instagram: withProfile((ctx, src, profile) => profile && profile.instagram),
