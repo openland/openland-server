088a829b987724b3aa883f477a027f86e9a5fb3b
mrmld <die.marmelade@yandex.ru>
2020-07-22 14:58:44 +0300

    wip(server): slice message in logs to 512 symbols

--- packages/openland-server/logs.ts
+++ packages/openland-server/logs.ts
@@ -37,8 +37,8 @@ export const logger = isProduction ? getPino() : winston.createLogger({
 function formatMessage(ctx: Context, name: string, message: string) {
     if (isProduction) {
         let v = LogPathContext.get(ctx);
-        if (message.length > 2048) {
-            message = message.slice(2048) + '...';
+        if (message.length > 512) {
+            message = message.slice(512) + '...';
         }
         return ContextName.get(ctx) + ' | ' + [...v, name].join(' ➾ ') + ': ' + message;
     } else {
