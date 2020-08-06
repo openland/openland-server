de9449ab2bc207db3b7eb5cb81f77c13a82f6270
steve <steve@openland.com>
2020-08-06 06:33:52 +0000

    perf(*): Increase perfomance limits per user

--- packages/openland-server/concurrency.ts
+++ packages/openland-server/concurrency.ts
@@ -6,20 +6,20 @@ export const Concurrency = {
     // Operation rate limiting. Unique for each connection.
     Operation: new SimpleFactory(() => {
         return new TokenBucket({
-            maxTokens: 20,
+            maxTokens: 64,
             refillDelay: 100,
-            refillAmount: 5
+            refillAmount: 10
         });
     }),
 
     // Parallel operation execution pool. Unique for each connection.
     Execution: new SimpleFactory(() => {
-        return new BoundedConcurrencyPool(64) as ConcurrencyPool;
+        return new BoundedConcurrencyPool(128) as ConcurrencyPool;
     }),
 
     // FoundationDB operation pool. Unique per authentication token.
     FDB: new SimpleFactory(() => {
-        return new BoundedConcurrencyPool(30) as ConcurrencyPool;
+        return new BoundedConcurrencyPool(128) as ConcurrencyPool;
     }),
     
     // Should be avoided
