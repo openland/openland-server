f316e732e4969bbf6c6f83a793b48094c99d660f
steve <steve@openland.com>
2020-08-15 01:17:22 +0000

    fix(groups): Fix group service startup

--- packages/openland-module-events/groups/GroupService.ts
+++ packages/openland-module-events/groups/GroupService.ts
@@ -1,16 +1,35 @@
+import { AsyncLock } from 'openland-utils/timer';
 import { Metrics } from 'openland-module-monitoring/Metrics';
+import { PresenceGroupService } from 'openland-module-presences/routing/PresenceGroupService';
+
 export class GroupService {
     readonly cid: number;
+    readonly lock = new AsyncLock();
+    readonly presencesService: PresenceGroupService;
 
     constructor(cid: number) {
         this.cid = cid;
+
+        this.presencesService = new PresenceGroupService(this);
+
+        // tslint:disable-next-line:no-floating-promises
+        this.lock.inLock(this.start);
     }
 
-    async start() {
+    private async start() {
         Metrics.GroupActiveServices.inc();
+
+        // Start services
+        await this.presencesService.start();
     }
 
     async stop() {
-        Metrics.GroupActiveServices.dec();
+        await this.lock.inLock(async () => {
+
+            // Stop services
+            await this.presencesService.stop();
+
+            Metrics.GroupActiveServices.dec();
+        });
     }
 }
