24afc0c1da1c1a7ed5a4dd8d52830ab14367d5bf
steve <steve@openland.com>
2020-08-22 00:30:39 +0000

    wip(events): Working on event definitions, add SeqRepository

--- packages/openland-module-events/Definitions.ts
+++ packages/openland-module-events/Definitions.ts
@@ -1,3 +1,4 @@
+import { encoders } from '@openland/foundationdb';
 import {
     UpdateChatRead,
     UpdateChatMessage,
@@ -7,6 +8,10 @@ import {
     UpdateChatLostAccess
 } from 'openland-module-db/store';
 
+//
+// All available events 
+//
+
 export type Event =
     | UpdateChatRead
     | UpdateChatMessage
@@ -15,3 +20,21 @@ export type Event =
     | UpdateChatGotAccess
     | UpdateChatLostAccess
     ;
+
+//
+// Repeat Key Resolving
+//
+
+const REPEAT_READ = 0;
+const REPEAT_UPDATED = 1;
+const REPEAT_ACCESS = 2;
+export function resolveRepeatKey(event: Event): Buffer | undefined {
+    if (event instanceof UpdateChatRead) {
+        return encoders.tuple.pack([REPEAT_READ, event.cid]);
+    } else if (event instanceof UpdateChatMessageUpdated) {
+        return encoders.tuple.pack([REPEAT_UPDATED, event.cid, event.mid]);
+    } else if (event instanceof UpdateChatGotAccess || event instanceof UpdateChatLostAccess) {
+        return encoders.tuple.pack([REPEAT_ACCESS, event.cid]);
+    }
+    return undefined;
+}
