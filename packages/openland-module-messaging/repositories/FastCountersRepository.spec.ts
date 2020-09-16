2522520f12f2728e91ee9685a2c1ead19f39f360
altox <xeroxaltox@gmail.com>
2020-09-16 20:38:01 +0300

    wip(server): fix tests

--- packages/openland-module-messaging/repositories/FastCountersRepository.spec.ts
+++ packages/openland-module-messaging/repositories/FastCountersRepository.spec.ts
@@ -6,6 +6,7 @@ import { FastCountersRepository } from './FastCountersRepository';
 import { createNamedContext } from '@openland/context';
 import { inTx } from '@openland/foundationdb';
 import { Store } from '../../openland-module-db/FDB';
+import { UserReadSeqsDirectory } from './UserReadSeqsDirectory';
 
 let parentCtx = createNamedContext('test');
 
@@ -13,6 +14,7 @@ describe('FastCountersRepository', () => {
     beforeAll(async () => {
         await testEnvironmentStart('fat-counters-repo');
         container.bind('FastCountersRepository').to(FastCountersRepository).inSingletonScope();
+        container.bind('UserReadSeqsDirectory').to(UserReadSeqsDirectory).inSingletonScope();
         container.bind(UsersModule).toSelf().inSingletonScope();
         loadUsersModule();
     });
@@ -22,8 +24,9 @@ describe('FastCountersRepository', () => {
 
     it('counter should be 0 for new chat', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         await inTx(parentCtx, async ctx => {
-            await repo.onAddDialog(ctx, 1, 1);
+            await readSeqsRepo.onAddDialog(ctx, 1, 1);
 
             let counters = await repo.fetchUserCounters(ctx, 1);
             expect(counters.length).toBe(1);
@@ -34,28 +37,31 @@ describe('FastCountersRepository', () => {
 
     it('should handle dialog deletion', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         await inTx(parentCtx, async ctx => {
-            await repo.onAddDialog(ctx, 2, 1);
+            await readSeqsRepo.onAddDialog(ctx, 2, 1);
 
             let counters = await repo.fetchUserCounters(ctx, 2);
             expect(counters.length).toBe(1);
             expect(counters[0].haveMention).toBe(false);
             expect(counters[0].unreadCounter).toBe(0);
 
-            await repo.onRemoveDialog(ctx, 2, 1);
-
-            counters = await repo.fetchUserCounters(ctx, 2);
+            await readSeqsRepo.onRemoveDialog(ctx, 2, 1);
+        });
+        await inTx(parentCtx, async ctx => {
+            let counters = await repo.fetchUserCounters(ctx, 2);
             expect(counters.length).toBe(0);
         });
     });
 
     it('should calc unreads', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         await inTx(parentCtx, async ctx => {
             let uid = 3;
             let cid = 2;
 
-            await repo.onAddDialog(ctx, uid, cid);
+            await readSeqsRepo.onAddDialog(ctx, uid, cid);
             await Store.ConversationLastSeq.byId(cid).set(ctx, 100);
 
             let counters = await repo.fetchUserCounters(ctx, uid);
@@ -67,11 +73,12 @@ describe('FastCountersRepository', () => {
 
     it('should handle message read', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         await inTx(parentCtx, async ctx => {
             let uid = 4;
             let cid = 3;
 
-            await repo.onAddDialog(ctx, uid, cid);
+            await readSeqsRepo.onAddDialog(ctx, uid, cid);
             await Store.ConversationLastSeq.byId(cid).set(ctx, 100);
 
             let counters = await repo.fetchUserCounters(ctx, uid);
@@ -79,9 +86,10 @@ describe('FastCountersRepository', () => {
             expect(counters[0].haveMention).toBe(false);
             expect(counters[0].unreadCounter).toBe(100);
 
-            repo.onMessageRead(ctx, uid, cid, 100);
-
-            counters = await repo.fetchUserCounters(ctx, uid);
+            readSeqsRepo.updateReadSeq(ctx, uid, cid, 100);
+        });
+        await inTx(parentCtx, async ctx => {
+            let counters = await repo.fetchUserCounters(ctx, 4);
             expect(counters.length).toBe(1);
             expect(counters[0].haveMention).toBe(false);
             expect(counters[0].unreadCounter).toBe(0);
@@ -90,11 +98,12 @@ describe('FastCountersRepository', () => {
 
     it('should handle deleted messages', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         let uid = 5;
         let cid = 4;
 
         await inTx(parentCtx, async ctx => {
-            await repo.onAddDialog(ctx, uid, cid);
+            await readSeqsRepo.onAddDialog(ctx, uid, cid);
             await Store.ConversationLastSeq.byId(cid).set(ctx, 100);
 
             await repo.onMessageDeleted(ctx, cid, 1, [], []);
@@ -119,12 +128,13 @@ describe('FastCountersRepository', () => {
     });
 
     it('should handle user mention', async () => {
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
         await inTx(parentCtx, async ctx => {
             let uid = 6;
             let cid = 5;
 
-            await repo.onAddDialog(ctx, uid, cid);
+            await readSeqsRepo.onAddDialog(ctx, uid, cid);
             await Store.ConversationLastSeq.byId(cid).set(ctx, 100);
 
             await repo.onMessageCreated(ctx, 10, cid, 1, [uid], []);
@@ -138,11 +148,12 @@ describe('FastCountersRepository', () => {
 
     it('should handle deletion of mention', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         await inTx(parentCtx, async ctx => {
             let uid = 7;
             let cid = 6;
 
-            await repo.onAddDialog(ctx, uid, cid);
+            await readSeqsRepo.onAddDialog(ctx, uid, cid);
             await Store.ConversationLastSeq.byId(cid).set(ctx, 100);
 
             await repo.onMessageCreated(ctx, 10, cid, 1, [uid], []);
@@ -163,11 +174,12 @@ describe('FastCountersRepository', () => {
 
     it('should handle deletion of mention after message edit', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         await inTx(parentCtx, async ctx => {
             let uid = 8;
             let cid = 7;
 
-            await repo.onAddDialog(ctx, uid, cid);
+            await readSeqsRepo.onAddDialog(ctx, uid, cid);
             await Store.ConversationLastSeq.byId(cid).set(ctx, 100);
 
             await repo.onMessageCreated(ctx, 10, cid, 1, [uid], []);
@@ -188,11 +200,12 @@ describe('FastCountersRepository', () => {
 
     it('should handle creation of mention after message edit', async () => {
         let repo = container.get<FastCountersRepository>('FastCountersRepository');
+        let readSeqsRepo = container.get<UserReadSeqsDirectory>('UserReadSeqsDirectory');
         await inTx(parentCtx, async ctx => {
             let uid = 9;
             let cid = 8;
 
-            await repo.onAddDialog(ctx, uid, cid);
+            await readSeqsRepo.onAddDialog(ctx, uid, cid);
             await Store.ConversationLastSeq.byId(cid).set(ctx, 100);
 
             await repo.onMessageCreated(ctx, 10, cid, 1, [], []);
