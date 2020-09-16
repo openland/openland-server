d95fdafa3ea99145faf9428f4f441cc9a27d30ce
altox <xeroxaltox@gmail.com>
2020-09-16 18:01:18 +0300

    wip(server): fix build

--- packages/openland-module-messaging/MessagingModule.ts
+++ packages/openland-module-messaging/MessagingModule.ts
@@ -23,7 +23,7 @@ import { SocialImageRepository } from './repositories/SocialImageRepository';
 import { DonationsMediator } from './mediators/DonationsMediator';
 import { Modules } from '../openland-modules/Modules';
 import { CounterProvider } from './counters/CounterProvider';
-import { NewCounterProvider } from './counters/NewCounterProvider';
+import { ExperimentalCountersProvider } from './counters/ExperimentalCountersProvider';
 
 export const USE_NEW_COUNTERS = true;
 
@@ -36,7 +36,7 @@ export class MessagingModule {
     readonly fixer: FixerRepository;
     readonly needNotificationDelivery: NeedNotificationDeliveryRepository;
     readonly delivery: DeliveryMediator;
-    readonly counters: CounterProvider = new NewCounterProvider();
+    readonly counters: CounterProvider = new ExperimentalCountersProvider();
     private readonly messaging: MessagingMediator;
     private readonly augmentation: AugmentationMediator;
     private readonly userState: UserStateRepository;
