a26c3c587581de0e39ea7a45ef545a552838d407
steve <steve@openland.com>
2020-09-24 08:10:08 +0000

    wip(events): Working on new events

---
+++ packages/openland-module-events/repo/internal/Locations.ts
@@ -0,0 +1,37 @@
+import { encoders } from '@openland/foundationdb';
+
+const FEED_STREAM = 0; // Event Stream
+const FEED_LATEST = 1; // Latest versionstamp
+const FEED_SEQ = 2; // Latest seq
+
+const SUBSCRIBER_SUBSCRIPTIONS = 0;
+const SUBSCRIBER_SUBSCRIPTIONS_VERSION = 3;
+const SUBSCRIBER_DIRECT = 1;
+const SUBSCRIBER_DIRECT_REV = 3;
+const SUBSCRIBER_DIRECT_UPDATES = 4;
+const SUBSCRIBER_DIRECT_LATEST = 5;
+const SUBSCRIBER_ASYNC = 2;
+const SUBSCRIBER_SUBSCRIPTIONS_CHANGES = 6;
+
+export const Locations = {
+    feedSeq: (feed: Buffer) => encoders.tuple.pack([feed, FEED_SEQ]),
+    feedLatest: (feed: Buffer) => encoders.tuple.pack([feed, FEED_LATEST]),
+    feedStream: (feed: Buffer) => encoders.tuple.pack([feed, FEED_STREAM]),
+
+    subscriber: {
+        subscription: (subscriber: Buffer, feed: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_SUBSCRIPTIONS, feed]),
+        subscriptionVersion: (subscriber: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_SUBSCRIPTIONS_VERSION]),
+
+        direct: (subscriber: Buffer, feed: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_DIRECT, feed]),
+        directAll: (subscriber: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_DIRECT]),
+        directReverse: (subscriber: Buffer, feed: Buffer) => encoders.tuple.pack([feed, SUBSCRIBER_DIRECT_REV, subscriber]),
+        directReverseAll: (feed: Buffer) => encoders.tuple.pack([feed, SUBSCRIBER_DIRECT_REV]),
+        async: (subscriber: Buffer, feed: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_ASYNC, feed]),
+        asyncAll: (subscriber: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_ASYNC]),
+
+        directUpdates: (subscriber: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_DIRECT_UPDATES]),
+        directLatest: (feed: Buffer) => encoders.tuple.pack([SUBSCRIBER_DIRECT_LATEST, feed]), // Change location?
+
+        subscriptionChanges: (subscriber: Buffer) => encoders.tuple.pack([subscriber, SUBSCRIBER_SUBSCRIPTIONS_CHANGES])
+    }
+};
---
+++ packages/openland-module-events/repo/internal/SeqRepository.spec.ts
@@ -0,0 +1,50 @@
+import { SeqRepository } from './SeqRepository';
+import { createNamedContext } from '@openland/context';
+import { Database } from '@openland/foundationdb';
+
+const ZERO = Buffer.alloc(0);
+
+describe('SeqRepository', () => {
+    it('posting allocate seq', async () => {
+        let root = createNamedContext('test');
+        let db = await Database.openTest({ name: 'event-storage-posting', layers: [] });
+        let repo = new SeqRepository(db.allKeys);
+
+        // Initial state
+        let seq = await repo.getCurrentSeqSnapshot(root, ZERO);
+        expect(seq).toBe(0);
+
+        // Allocate
+        seq = await repo.allocateSeq(root, ZERO);
+        expect(seq).toBe(1);
+        seq = await repo.getCurrentSeqSnapshot(root, ZERO);
+        expect(seq).toBe(1);
+
+        // Should not allocate since user never was online
+        let now = Math.floor(Date.now() / 1000);
+        expect(await repo.isOnline(root, ZERO, now + 4)).toBe(false);
+        // let seq2 = await repo.allocateSeqIfOnline(root, ZERO, now);
+        // expect(seq2).toBeNull();
+        seq = await repo.getCurrentSeqSnapshot(root, ZERO);
+        expect(seq).toBe(1);
+
+        // Should refresh online status
+        expect(await repo.refreshOnline(root, ZERO, now + 5)).toBeNull();
+        expect(await repo.refreshOnline(root, ZERO, now + 6)).toBe(now + 5);
+
+        // Should be online
+        expect(await repo.isOnline(root, ZERO, now + 4)).toBe(true);
+
+        // Allocate since user is online
+        seq = await repo.allocateSeq(root, ZERO);
+        expect(seq).toBe(2);
+        seq = await repo.getCurrentSeqSnapshot(root, ZERO);
+        expect(seq).toBe(2);
+
+        // Should not allocate for expired
+        // seq2 = await repo.allocateSeqIfOnline(root, ZERO, now + 6000);
+        // expect(seq2).toBeNull();
+        seq = await repo.getCurrentSeqSnapshot(root, ZERO);
+        expect(seq).toBe(2);
+    });
+});
