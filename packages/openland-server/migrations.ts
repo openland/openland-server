bf236bd0adff25106488a482c8d0039736ea291f
Steve Kite <steve@Steves-Mac-mini.local>
2020-12-11 18:25:02 +0300

    debug(*): Add error logging

--- packages/openland-server/migrations.ts
+++ packages/openland-server/migrations.ts
@@ -1109,14 +1109,19 @@ migrations.push({
     migration: async (parent) => {
         let index = 0;
         await Store.ConversationPrivate.iterateAllItems(parent, 10000, async (ctx, items) => {
-            logger.log(ctx, 'Iteration' + index);
-            for (let i of items) {
-                let mute1 = (await Modules.Messaging.getRoomSettings(ctx, i.uid1, i.id)).mute;
-                let mute2 = (await Modules.Messaging.getRoomSettings(ctx, i.uid2, i.id)).mute;
-                let seq1 = await Modules.Messaging.messaging.userReadSeqs.getUserReadSeqForChat(ctx, i.uid1, i.id);
-                let seq2 = await Modules.Messaging.messaging.userReadSeqs.getUserReadSeqForChat(ctx, i.uid2, i.id);
-                await Modules.Messaging.messaging.counters.subscribe(ctx, { cid: i.id, uid: i.uid1, seq: seq1, muted: mute1 });
-                await Modules.Messaging.messaging.counters.subscribe(ctx, { cid: i.id, uid: i.uid2, seq: seq2, muted: mute2 });
+            try {
+                logger.log(ctx, 'Iteration ' + index);
+                for (let i of items) {
+                    let mute1 = (await Modules.Messaging.getRoomSettings(ctx, i.uid1, i.id)).mute;
+                    let mute2 = (await Modules.Messaging.getRoomSettings(ctx, i.uid2, i.id)).mute;
+                    let seq1 = await Modules.Messaging.messaging.userReadSeqs.getUserReadSeqForChat(ctx, i.uid1, i.id);
+                    let seq2 = await Modules.Messaging.messaging.userReadSeqs.getUserReadSeqForChat(ctx, i.uid2, i.id);
+                    await Modules.Messaging.messaging.counters.subscribe(ctx, { cid: i.id, uid: i.uid1, seq: seq1, muted: mute1 });
+                    await Modules.Messaging.messaging.counters.subscribe(ctx, { cid: i.id, uid: i.uid2, seq: seq2, muted: mute2 });
+                }
+            } catch (e) {
+                logger.warn(ctx, e);
+                throw e;
             }
             index++;
         });
