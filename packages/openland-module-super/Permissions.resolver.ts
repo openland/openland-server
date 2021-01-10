a0e6570a141c73df045e5c3eabc6313774ce8450
steve <steve@korshakov.com>
2021-01-10 04:21:29 +0300

    fix(*): Fix build

--- packages/openland-module-super/Permissions.resolver.ts
+++ packages/openland-module-super/Permissions.resolver.ts
@@ -1,8 +1,8 @@
-import { withPermission } from 'openland-module-api/Resolvers';
+// import { withPermission } from 'openland-module-api/Resolvers';
 import { Modules } from '../openland-modules/Modules';
-import { Store } from '../openland-module-db/FDB';
+// import { Store } from '../openland-module-db/FDB';
 import { GQLResolver } from '../openland-module-api/schema/SchemaSpec';
-import { isDefined } from '../openland-utils/misc';
+// import { isDefined } from '../openland-utils/misc';
 
 export const Resolver: GQLResolver = {
     Query: {
@@ -11,14 +11,14 @@ export const Resolver: GQLResolver = {
                 roles: Array.from(await Modules.Super.resolvePermissions(ctx, { uid: ctx.auth.uid, oid: ctx.auth.oid }))
             };
         },
-        users: withPermission('super-admin', async (ctx, args) => {
-            let {uids} = await Modules.Users.searchForUsers(ctx, args.query, { limit: 10 });
+        // users: withPermission('super-admin', async (ctx, args) => {
+        //     let {uids} = await Modules.Users.searchForUsers(ctx, args.query, { limit: 10 });
 
-            if (uids.length === 0) {
-                return [];
-            }
+        //     if (uids.length === 0) {
+        //         return [];
+        //     }
 
-            return (await Promise.all(uids.map((v) => Store.User.findById(ctx, v)))).filter(isDefined);
-        }),
+        //     return (await Promise.all(uids.map((v) => Store.User.findById(ctx, v)))).filter(isDefined);
+        // }),
     },
 };
