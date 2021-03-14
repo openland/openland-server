9f9493f9b4377c2f1c530b6e9afe1cb0b7f9e6f5
steve <steve@korshakov.com>
2021-03-14 19:53:20 +0300

    chore(search): Enable search tracing

--- packages/openland-module-search/SearchModule.ts
+++ packages/openland-module-search/SearchModule.ts
@@ -2,14 +2,16 @@ import { ElasticService } from './services/ElasticService';
 import { injectable } from 'inversify';
 import * as ES from 'elasticsearch';
 import { Context } from '@openland/context';
+import { createTracer } from 'openland-log/createTracer';
 
+const tracer = createTracer('elastic');
 @injectable()
 export class SearchModule {
 
     readonly elastic = new ElasticService();
 
     search<T>(ctx: Context, params: ES.SearchParams): Promise<ES.SearchResponse<T>> {
-        return this.elastic.client.search(params);
+        return tracer.trace(ctx, 'search', () => this.elastic.client.search(params));
     }
 
     start = async () => {
