a421a088a2706c527b1e58b643a427daed3386b4
altox <xeroxaltox@gmail.com>
2021-03-19 17:12:24 +0300

    wip(server): counters resolve time metrics

--- packages/openland-module-messaging/counters/PrecalculatedCounterProvider.ts
+++ packages/openland-module-messaging/counters/PrecalculatedCounterProvider.ts
@@ -3,27 +3,33 @@ import { Context } from '@openland/context';
 import { injectable } from 'inversify';
 import { Modules } from 'openland-modules/Modules';
 import { createTracer } from 'openland-log/createTracer';
+import { Metrics } from '../../openland-module-monitoring/Metrics';
 
 const tracer = createTracer('counters');
 @injectable()
 export class PrecalculatedCounterProvider implements CounterProvider {
 
     async fetchUserGlobalCounter(parent: Context, uid: number) {
-        return await tracer.trace(parent, 'global_counter', async (ctx) => {
-            let settings = await Modules.Users.getUserSettings(ctx, uid);
-            if (settings.globalCounterType === 'unread_messages') {
-                return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, false, 'all');
-            } else if (settings.globalCounterType === 'unread_messages_no_muted') {
-                return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, true, 'all');
-            } else if (settings.globalCounterType === 'unread_chats') {
-                return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, false, 'distinct');
-            } else if (settings.globalCounterType === 'unread_chats_no_muted') {
-                return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, true, 'distinct');
-            }
+        let start = Date.now();
+        try {
+            return await tracer.trace(parent, 'global_counter', async (ctx) => {
+                let settings = await Modules.Users.getUserSettings(ctx, uid);
+                if (settings.globalCounterType === 'unread_messages') {
+                    return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, false, 'all');
+                } else if (settings.globalCounterType === 'unread_messages_no_muted') {
+                    return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, true, 'all');
+                } else if (settings.globalCounterType === 'unread_chats') {
+                    return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, false, 'distinct');
+                } else if (settings.globalCounterType === 'unread_chats_no_muted') {
+                    return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, true, 'distinct');
+                }
 
-            // Default counter
-            return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, false, 'all');
-        });
+                // Default counter
+                return await Modules.Messaging.messaging.counters.getGlobalCounter(ctx, uid, false, 'all');
+            });
+        } finally {
+            Metrics.GlobalCounterResolveTime.report(Date.now() - start);
+        }
     }
 
     async fetchUserUnreadMessagesCount(parent: Context, uid: number) {
