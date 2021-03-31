4f6a53a3d6ece1a4391219b0d50e777217c0ad40
altox <xeroxaltox@gmail.com>
2021-04-01 00:31:59 +0300

    wip(server): disable clickhouse on dev build for now

--- packages/openland-module-clickhouse/ClickHouseModule.ts
+++ packages/openland-module-clickhouse/ClickHouseModule.ts
@@ -2,12 +2,13 @@ import { injectable } from 'inversify';
 import { startExporters } from './startExporters';
 import { createNamedContext } from '@openland/context';
 import { serverRoleEnabled } from '../openland-utils/serverRoleEnabled';
+import { Config } from '../openland-config/Config';
 
 let ctx = createNamedContext('clickhouse');
 @injectable()
 export class ClickHouseModule {
     start = async () => {
-        if (serverRoleEnabled('admin')) {
+        if (serverRoleEnabled('admin') && Config.environment !== 'debug') {
             startExporters(ctx);
         }
     }
