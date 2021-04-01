3a5265801e5c19cacc9ec446719e5e305c7a1776
steve <steve@korshakov.com>
2021-04-01 14:13:53 +0300

    perf(auth): Track auth performance

--- packages/openland-utils/sms/SmscSmsService.ts
+++ packages/openland-utils/sms/SmscSmsService.ts
@@ -2,16 +2,20 @@ import { ISmsService } from './ISmsService';
 import { Context } from '@openland/context';
 import { Events } from '../../openland-module-hyperlog/Events';
 import fetch from 'node-fetch';
+import { createTracer } from 'openland-log/createTracer';
 
 const USER = 'altox';
 const PASSWORD = '8eVnYtVUcqcZaLC2pyFa';
+const trace = createTracer('smsc');
 
 export class SmscSmsService implements ISmsService {
-    async sendSms(ctx: Context, to: string, body: string) {
-        await fetch(encodeURI(`https://smsc.ru/sys/send.php?login=${USER}&psw=${PASSWORD}&phones=${to}&mes=${body}`), {
-            method: 'POST'
+    async sendSms(parent: Context, to: string, body: string) {
+        return await trace.trace(parent, 'send-sms', async (ctx) => {
+            await fetch(encodeURI(`https://smsc.ru/sys/send.php?login=${USER}&psw=${PASSWORD}&phones=${to}&mes=${body}`), {
+                method: 'POST'
+            });
+            Events.SmsSentEvent.event(ctx, { phone: to });
+            return true;
         });
-        Events.SmsSentEvent.event(ctx, { phone: to });
-        return true;
     }
 }
