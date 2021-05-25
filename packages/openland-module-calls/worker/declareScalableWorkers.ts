75315d0223ae834eb61fba6c8bf06c56c5c6370e
Steve Kite <steve@korshakov.com>
2021-05-25 12:05:14 +0000

    fix(*): Fix build

--- packages/openland-module-calls/worker/declareScalableWorkers.ts
+++ packages/openland-module-calls/worker/declareScalableWorkers.ts
@@ -5,7 +5,7 @@ export function declareScalableWorkers(repo: CallRepository) {
     repo.schedulerScalable.sessionWorker.addWorker(10, async (ctx: Context, items) => {
         await repo.schedulerScalable.mediator.onSessionJob(ctx, items[0].cid, items);
     });
-    repo.schedulerScalable.shardWorker.addWorker(10, async (ctx: Context, items) => {
+    repo.schedulerScalable.mediator.repoShard.shardWorker.addWorker(10, async (ctx: Context, items) => {
         await repo.schedulerScalable.mediator.onShardJob(ctx, items[0].cid, items[0].session, items[0].shard, items);
     });
 }
