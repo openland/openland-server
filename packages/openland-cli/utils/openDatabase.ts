e6b4268fe44fac55d775773e320633b4bf99a169
Steve Kite <steve@korshakov.com>
2021-05-31 13:58:52 +0000

    fix(*): Fix build

--- packages/openland-cli/utils/openDatabase.ts
+++ packages/openland-cli/utils/openDatabase.ts
@@ -2,13 +2,14 @@ import { BusLayer, NoOpBus } from '@openland/foundationdb-bus';
 import { Database } from '@openland/foundationdb';
 import { openStore } from 'openland-module-db/store';
 import { EntityStorage } from '@openland/foundationdb-entity';
+import { Context } from '@openland/context';
 
-export async function openDatabase() {
+export async function openDatabase(ctx: Context) {
     let db = await Database.open({
         layers: [
             new BusLayer(new NoOpBus())
         ]
     });
     let storage = new EntityStorage(db);
-    return await openStore(storage);
+    return await openStore(ctx, storage);
 }
