87a86e9db66525da04fed5b8c3e07bbc3c309544
Steve Kite <steve@korshakov.com>
2021-07-09 13:58:23 +0000

    chore: more fixer logging

--- packages/openland-server/fixer.ts
+++ packages/openland-server/fixer.ts
@@ -1,13 +1,18 @@
 import { Context } from '@openland/context';
 import { singletonWorker } from '@openland/foundationdb-singleton';
+import { createLogger } from '@openland/log';
 import { IDs } from 'openland-module-api/IDs';
 import { Store } from 'openland-module-db/FDB';
 import { Modules } from 'openland-modules/Modules';
 import { serverRoleEnabled } from 'openland-utils/serverRoleEnabled';
 
+const logger = createLogger('fixer');
+
 async function fixWalletLock(parent: Context) {
+    logger.log(parent, 'Fixing Wallet Locks');
     await Store.Wallet.iterateAllItems(parent, 100, async (ctx, items) => {
         for (let item of items) {
+            logger.log(parent, 'Fixing Wallet lock for ' + item.uid);
             await Modules.Wallet.updateIsLocked(ctx, item.uid);
         }
     });
@@ -19,8 +24,10 @@ async function cleanupSubscriptions(parent: Context) {
 
     // Collect to cancel
     let toCancel = new Set<string>();
+    logger.log(parent, 'Fixing subscriptions');
     await Store.Wallet.iterateAllItems(parent, 10, async (ctx, items) => {
         for (let item of items) {
+            logger.log(parent, 'Fixing subscriptions for ' + item.uid);
             let subscriptions = await Store.WalletSubscription.user.findAll(ctx, item.uid);
             for (let sub of subscriptions) {
                 if (sub.state === 'expired') {
@@ -41,6 +48,7 @@ async function cleanupSubscriptions(parent: Context) {
 
     // Pending cancel
     for (let s of toCancel) {
+        logger.log(parent, 'Canceling subscription ' + s);
         await Modules.Wallet.cancelSubscription(parent, s);
     }
 }
@@ -49,11 +57,15 @@ export function startFixer() {
     if (serverRoleEnabled('workers')) {
         singletonWorker({ db: Store.storage.db, name: 'fixer', delay: 15000 }, async (parent) => {
 
+            logger.log(parent, 'Start Fixer');
+
             // Fix wallet locks
             await fixWalletLock(parent);
 
             // Remove subscriptions
             await cleanupSubscriptions(parent);
+
+            logger.log(parent, 'End Fixer');
         });
     }
 }
